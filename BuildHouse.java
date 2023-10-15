6d8baee817e039c2938cef64f3f2146175033244
S <s>
2023-10-15 10:02:50 -0800

    {"types":"AR","runs":5}

---
+++ BuildHouse.java
@@ -0,0 +1,24 @@
+import acm.graphics.*;
+import acm.program.*;
+
+public class BuildHouse extends GraphicsProgram {
+	
+	private static final double HOUSE_HEIGHT = 200;
+	private static final double HOUSE_WIDTH = 200;
+	
+	public void run (){
+		drawHouse();
+	}
+
+	private void drawHouse() {
+    	drawWall();
+//		drawRoof();
+//		drawDoor();
+//		drawWindow();
+	}
+
+	private void drawWall() {
+		GRect wall = new GRect (HOUSE_HEIGHT, HOUSE_WIDTH);
+		add(wall);
+	}
+}
