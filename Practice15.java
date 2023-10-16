07026c08db746948bee19d9ecaa2484e7afda406
S <s>
2023-10-16 05:27:23 -0800

    {"types":"C","runs":1}

--- Practice15.java
+++ Practice15.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Practice15 {
-
+public class Practice15  extends ConsoleProgram{
+	public void run(){
+		println ("write numbers and get their middle");
+		int x1 =readInt ("enter n1");
+		int x2 =readInt ("enter n2");
+		double middle = (x1+x2)/2;
+		
+		println(middle);
+	}
 }
