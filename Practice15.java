196a041ce79b7417d21499396c33a3e0be701e69
S <s>
2023-10-16 10:18:55 -0800

    {"types":"C","runs":1}

--- Practice15.java
+++ Practice15.java
@@ -2,11 +2,11 @@ import acm.program.ConsoleProgram;
 
 public class Practice15  extends ConsoleProgram{
 	public void run(){
-		println ("write numbers and get their middle");
+		println ("write numbers and get their average");
 		int x1 = readInt ("enter n1=");
 		int x2 = readInt ("enter n2=");
-		double middle = (double) (x1 + x2) / 2;
+		double average = (double) (x1 + x2) / 2;
 		
-		println(middle);
+		println(average);
 	}
 }
--- Practice16.java
+++ Practice16.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class Practice16 {
-
+public class Practice16 extends ConsoleProgram{
+	public void run(){
+		println ("write numbers and get");
+		int a = readInt ("enter a = ");
+		int b = readInt ("enter b = ");
+		int c = readInt ("enteer c = ");
+		double harmonic = (double) 1/(1 / a + 1 / b + 1 / c);
+		print(harmonic);
+	}
 }
