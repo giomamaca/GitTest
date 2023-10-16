a876e3d73b6a5ad15996fe8c1b8e38b8e82216b7
S <s>
2023-10-16 10:19:30 -0800

    {"types":"C","runs":2}

--- Practice16.java
+++ Practice16.java
@@ -6,7 +6,7 @@ public class Practice16 extends ConsoleProgram{
 		int a = readInt ("enter a = ");
 		int b = readInt ("enter b = ");
 		int c = readInt ("enteer c = ");
-		double harmonic = (double) 1/(1 / a + 1 / b + 1 / c);
+		double harmonic = (double) 1/((1 / a) + (1 / b) + (1 / c));
 		print(harmonic);
 	}
 }
