5fd627b66ae6699dcf2fb1aaf7d2fe236a63450d
S <s>
2023-10-16 02:46:08 -0800

    {"types":"C","runs":8}

--- BuildHouse.java
+++ BuildHouse.java
@@ -26,7 +26,7 @@ public class BuildHouse extends GraphicsProgram {
 	private void drawWall() {
 		GRect wall = new GRect (HOUSE_HEIGHT, HOUSE_WIDTH);
 		double x = (getWidth() / 2 + 100);
-		double y = getHeight();
+		double y = getHeight() / 2;
 		
 		add(wall, x, y);
 	}
