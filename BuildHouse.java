276228a1545b5dacbff24280f042489af84faabe
S <s>
2023-10-16 02:55:16 -0800

    {"types":"C","runs":19}

--- BuildHouse.java
+++ BuildHouse.java
@@ -28,7 +28,7 @@ public class BuildHouse extends GraphicsProgram {
 		double x3 = getWidth() / 2 - 100 + HOUSE_HEIGHT /2;
 		double y3 = getHeight() / 2 - 60;
 		double x4 = getWidth() / 2 - 100 + HOUSE_HEIGHT;
-		double y4 = getHeight() / 2 - HOUSE_HEIGHT;
+		double y4 = getHeight() / 2;
 		
 		 GLine line = new GLine (x3, y3 ,x4, y4);
 		 add(line);
