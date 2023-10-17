7613a0588a97fada78112c58250efdca08f6a23b
S <s>
2023-10-17 04:31:57 -0800

    {"types":"C","runs":3}

--- dausgdad.java
+++ dausgdad.java
@@ -3,8 +3,8 @@ import acm.program.ConsoleProgram;
 public class dausgdad extends ConsoleProgram {
 	public void run(){
 		println ("dsasdasda");
-		int x = readInt("enter x");
-		int y = readInt ("enter y");
+		int x = readInt("enter x=");
+		int y = readInt ("enter y=");
 		double average = (double) (x + y) / 2;
 		print(average);
 	}
