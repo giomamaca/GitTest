e60649553d261b5b4326284554f43c1e9f62655b
S <s>
2023-10-16 23:54:22 -0800

    {"types":"C","runs":4}

--- Practice16.java
+++ Practice16.java
@@ -3,10 +3,10 @@ import acm.program.ConsoleProgram;
 public class Practice16 extends ConsoleProgram{
 	public void run(){
 		println ("write numbers and get");
-		int a = 1 / readInt ("enter a = ");
-		int b = 1 / readInt ("enter b = ");
-		int c = 1 / readInt ("enteer c = ");
-		double harmonic = (double) 1/(a + b + c);
+		int a = readInt ("enter a = ");
+		int b = readInt ("enter b = ");
+		int c = readInt ("enteer c = ");
+		double harmonic = (double) (a * b * c )/ (a + b + c);
 		print(harmonic);
 	}
 }
