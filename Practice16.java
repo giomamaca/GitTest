d53af3cbccf7158ad641c3cd7feafa29eeb4ef73
S <s>
2023-10-17 05:27:10 -0800

    {"types":"C","runs":2}

--- Practice16.java
+++ Practice16.java
@@ -3,9 +3,11 @@ import acm.program.ConsoleProgram;
 public class Practice16 extends ConsoleProgram{
 	public void run(){
 		println ("write numbers and get");
-		int a = readInt ("enter a = ");
-		int b = readInt ("enter b = ");
-		int c = readInt ("enteer c = ");;
+		double a = readInt ("enter a = ");
+		double b = readInt ("enter b = ");
+		double c = readInt ("enteer c = ");
+		
+		
 		println(1 / (1/a + 1/b  + 1/c));
 	}
 }
