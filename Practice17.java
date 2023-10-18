f2a082714aa631c77cab8b83c903935fdaeef77c
S <s>
2023-10-18 10:50:33 -0800

    {"types":"C","runs":2}

--- Practice17.java
+++ Practice17.java
@@ -2,6 +2,9 @@ import acm.program.ConsoleProgram;
 
 public class Practice17 extends ConsoleProgram{
 	public void run(){
+		println("enter your number");
+		double a = readDouble("enter number : ");
+		
 		
 	}
 }
