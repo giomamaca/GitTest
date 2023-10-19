5539120879251e09b716d553f8baeb48e3283e82
S <s>
2023-10-19 06:07:46 -0800

    {"types":"C","runs":16}

--- Practice22.java
+++ Practice22.java
@@ -4,13 +4,13 @@ import acm.program.GraphicsProgram;
 public class Practice22 extends GraphicsProgram {
 	private static final double NUM_CELLS = 8;
 	public void run(){
+		double length = math.min(getWidth(), getHeight()) / (double) NUM_CELLS;
 		for(int i = 0 ; i < NUM_CELLS; i++){
 			for(int j = 0; j < NUM_CELLS; j++){
-				double x;
-				double y; 
-				double lenght;
-				double isFilled;
-				drawSquare(x, y, lenght, isFilled);
+				double x = i * length;
+				double y = j * length; 
+				double isFilled = (i + j) % 2 == 1;
+				drawSquare(x, y, length, isFilled);
 			}
 		}
 	}
