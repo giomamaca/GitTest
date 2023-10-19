fd9ebac2dbd0232167d2f4c6eac40c2b77ea6616
S <s>
2023-10-19 04:12:49 -0800

    {"types":"C","runs":1}

--- Practice182.java
+++ Practice182.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class Practice182 {
-
+public class Practice182 extends ConsoleProgram {
+	public void run(){
+		println("enter numbers");
+		double a = readDouble ("enter num1");
+		double b = readDouble ("enter num2");
+		
+		if( a > b){
+			print(a);
+		}else{
+			print(b);
+		}
+	}
 }
