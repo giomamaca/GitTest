0e7e7694a38ccebf0a3be4dd4f92f32c132a75ac
S <s>
2023-10-19 05:35:01 -0800

    {"types":"C","runs":4}

--- Practice20.java
+++ Practice20.java
@@ -4,11 +4,13 @@ public class Practice20 extends ConsoleProgram {
 	public void run(){
 		int n = readInt("enter number:");
 		
-		
+		int sum = 0;
 		for(int i = 0; i < n; i++){
 			int num = readInt("enter number #" + i + ":");
 			
-			println(num);
+//			println(num);
+			sum += num;
 		}
+		print("sum:" + sum);
 	}
 }
