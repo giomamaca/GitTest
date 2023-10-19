1a0a11d99a6cb6eacd85e0648f99974f452827ef
S <s>
2023-10-19 05:48:45 -0800

    {"types":"C","runs":5}

--- Practice21.java
+++ Practice21.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
+import acm.program.*;
 
-public class Practice21 {
-
+public class Practice21 extends ConsoleProgram {
+	private static final int NUM_LINES = 11;
+	public void run(){
+		for(int i = 0; i < NUM_LINES; i++){
+			drawHorisont(i);
+		}
+	}
+	private void drawHorisont(int i) {
+		double y = i * (getHeight() / (NUM_LINES - 1));
+		double x1 = 0;
+		double x2 = getWidth();
+		GLine line = new Gline (x1, y, x2, y);
+	}
 }
