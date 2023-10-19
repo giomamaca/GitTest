955cf82d557f0f99b816c6d9b6b26651c70de351
S <s>
2023-10-19 04:20:13 -0800

    {"types":"C","runs":4}

--- Practice19.java
+++ Practice19.java
@@ -2,6 +2,11 @@ import acm.program.ConsoleProgram;
 
 public class Practice19 extends ConsoleProgram {
 	public void run(){
+		println("enter number");
+		int x = readInt ("number = ");
 		
+		for(int i = 0; i < x; i++){
+			println("hello");
+		}
 	}
 }
