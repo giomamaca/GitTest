bef9ebd14480e224011085300b55d36e4b396bda
S <s>
2023-10-20 11:33:07 -0800

    {"types":"C","runs":48}

--- assignment2P2.java
+++ assignment2P2.java
@@ -8,17 +8,17 @@ public class assignment2P2 extends GraphicsProgram{
 	private static final double R3 = 24;
 	public void run(){
 		GOval largeOval = new GOval (R1, R1);
-		double x = getWidth() / 2 - 36;
-		double y = getHeight() / 2 - 36;
+		double x1 = getWidth() / 2 - 36;
+		double y1 = getHeight() / 2 - 36;
 		largeOval.setFilled(true);
 		largeOval.setColor(Color.RED);
-		add(largeOval, x, y);
+		add(largeOval, x1, y1);
 		
 		GOval medOval = new GOval (R2, R2);
-		double x1 = getWidth() / 2 - 36;
-		double y1 = getHeight() / 2 - 36 ;
+		double x2 = getWidth() / 2 - 36;
+		double y2 = getHeight() / 2 - 36 ;
 		medOval.setFilled(true);
 		medOval.setColor(Color.blue);
-		add(medOval);
+		add(medOval, x2, y2);
 	}
 }
