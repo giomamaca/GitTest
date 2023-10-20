dc7d2aecee7d2d4e9ca95812eb0b29d40e5f5bfc
S <s>
2023-10-20 10:47:33 -0800

    {"types":"C","runs":28}

--- assignment2P2.java
+++ assignment2P2.java
@@ -6,6 +6,6 @@ public class assignment2P2 extends GraphicsProgram{
 	private static final double R2 = 48;
 	private static final double R3 = 24;
 	public void run(){
-		add(new GOval(R1,R2));
+		add(new GOval(R1,R1));
 	}
 }
