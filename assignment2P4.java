d0048d4cb511abe860879c7010c2a8e07d900ae0
S <s>
2023-10-20 10:06:45 -0800

    {"types":"C","runs":7}

--- assignment2P4.java
+++ assignment2P4.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class assignment2P4 {
-
+public class assignment2P4 extends ConsoleProgram{
+	public void run(){
+		int a = readInt ("enter num1 = ");
+		int b = readInt ("enter num2 = ");
+		
+		double c = Math.sqrt(a^2 + b^2);
+		
+	}
 }
