5dab055b565d5dd6c331462c647832f7723fe63f
S <s>
2023-10-20 10:47:26 -0800

    {"types":"C","runs":27}

--- assignment2P2.java
+++ assignment2P2.java
@@ -3,8 +3,9 @@ import acm.program.GraphicsProgram;
 
 public class assignment2P2 extends GraphicsProgram{
 	private static final double R1 = 72;
-	private static final double R2 = 
+	private static final double R2 = 48;
+	private static final double R3 = 24;
 	public void run(){
-		
+		add(new GOval(R1,R2));
 	}
 }
--- assignment2P6.java
+++ assignment2P6.java
@@ -4,11 +4,5 @@ public class assignment2P6 extends ConsoleProgram{
 	public void run(){
 		int a = readInt ("enter number = ");
 		
-		while(a % 2 ==0){
-			if(a % 2 ==0){
-				int c = (a / 2);
-				print(c);
-			}
-		}
 	}
 }
