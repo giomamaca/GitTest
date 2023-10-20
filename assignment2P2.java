a57d9b28de6db24467a730ce2ffa3875d254fe7d
S <s>
2023-10-20 10:05:09 -0800

    {"types":"C","runs":6}

--- assignment2P2.java
+++ assignment2P2.java
@@ -1,4 +1,10 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class assignment2P2 {
-
+public class assignment2P2 extends GraphicsProgram{
+	private static final double R1 = 72;
+	private static final double R2 = 
+	public void run(){
+		
+	}
 }
