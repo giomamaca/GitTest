27437e9091a74af9097abed487d2eaa05a1cb161
S <s>
2023-10-20 10:51:34 -0800

    {"types":"C","runs":32}

--- assignment2P2.java
+++ assignment2P2.java
@@ -7,7 +7,7 @@ public class assignment2P2 extends GraphicsProgram{
 	private static final double R3 = 24;
 	public void run(){
 		GOval largeOval = new GOval (R1, R1);
-		double x = getWidth() / 2 - 72;
+		double x = getWidth() / 2 - 36;
 		double y = getHeight() / 2 - 72;
 		
 		add(largeOval, x, y);
