6e8f36d29ca6749f99197908f512e27f8a506bd8
S <s>
2023-10-20 09:47:29 -0800

    {"types":"C","runs":1}

--- assignment2P1.java
+++ assignment2P1.java
@@ -1,4 +1,13 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class assignment2P1 {
-
+public class assignment2P1 extends GraphicsProgram {
+	private static final double BRICK_WIDTH = 30;
+	private static final double BRICK_HEIGHT = 12;
+	private static final double BRICK_IN_BASE = 14;
+	public void run(){
+		for(int i = 0; i < BRICK_IN_BASE; i++){
+			GRect Brick = new GRect(BRICK_WIDTH, BRICK_HEIGHT);
+		}
+	}
 }
