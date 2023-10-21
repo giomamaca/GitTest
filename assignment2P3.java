91ca613bc0353ac02d6795430634a410e6f6f440
S <s>
2023-10-21 01:54:30 -0800

    {"types":"C","runs":21}

--- assignment2P3.java
+++ assignment2P3.java
@@ -4,15 +4,16 @@ import acm.program.GraphicsProgram;
 public class assignment2P3 extends GraphicsProgram{
 	private static final double RECT_WIDTH = 150;
 	private static final double RECT_HEIGHT = 75;
+	private static final double MIDLINE_WIDTH = 50;
 	public void run(){
 		GRect rect1 = new GRect (RECT_WIDTH, RECT_HEIGHT);
 		double x = getWidth() / 2 - RECT_WIDTH / 2;
 		double y = getHeight() / 5;
 	    add(rect1, x, y);
 	    
-	    GLabel label1 = new GLabel (CENTER);
-	    double xLabel = getWidth() / 2;
-	    double yLabel = getHeight() / 2;
-	    add(label1, xLabel, yLabel);
+	    GRect rect2 = new GRect (RECT_WIDTH, RECT_HEIGHT);
+	    double x1 = getWidth() / 2 - RECT_WIDTH / 2;
+	    double y1 = getHeight() / 5 + MIDLINE_WIDTH;
+	    add(rect2, x1, y1);
 	}
 }
