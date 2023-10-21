45cff4d253856a2326da2139ed6d1b4cd5f59b75
S <s>
2023-10-21 05:26:42 -0800

    {"types":"C","runs":44}

--- Practice19.java
+++ Practice19.java
@@ -3,6 +3,7 @@ import acm.program.ConsoleProgram;
 
 public class Practice19 extends ConsoleProgram {
 	public void run(){
+		
 		GLabel lab = new GLabel ("dasdadad", 100, 110);
 		int a = (int) lab.getWidth();
 		print(lab);
