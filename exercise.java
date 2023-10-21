754dc766f7ebf0d7248d28f38ac2c9c1a99df2c5
S <s>
2023-10-21 04:27:37 -0800

    {"types":"C","runs":23}

--- exercise.java
+++ exercise.java
@@ -3,16 +3,7 @@ import acm.program.GraphicsProgram;
 
 public class exercise extends GraphicsProgram{	
 	public void run(){
-		drawLine();
-	}
-
-	private void drawLine() {
-		double x1 = 100;
-		double y1 = 100;
-		double x2 = 200;
-		double y2 = 100;
-		
-		GLine line = new GLine (x1, y1, x2, y2);
-		add(line);
+		GLabel lab = new GLabel ("dasda", 50, 50);
+		lab.getWidth();
 	}
 }
