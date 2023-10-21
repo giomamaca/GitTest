d7fe7346683774b7e09ebe406d977df9c211433b
S <s>
2023-10-21 03:33:35 -0800

    {"types":"C","runs":9}

--- assignment2P3.java
+++ assignment2P3.java
@@ -18,8 +18,13 @@ public class assignment2P3 extends GraphicsProgram{
 	    add(rect2, x1, y1);
 	    
 	    GRect rect3 = new GRect (RECT_WIDTH, RECT_HEIGHT);
-	    double x2 = getWidth() / 2 - RECT_WIDTH /2 -SPACE_BETWEENREC;
+	    double x2 = getWidth() / 2 - RECT_WIDTH /2 - SPACE_BETWEENREC;
 	    double y2 = getHeight() / 5 + MIDLINE_WIDTH;
 	    add(rect3, x2, y2);
+	    
+	    GRect rect4 = new GRect (RECT_WIDTH, RECT_HEIGHT);
+	    double x2 = getWidth() / 2 - RECT_WIDTH /2 + SPACE_BETWEENREC;
+	    double y3 = getHeight() / 5 + MIDLINE_WIDTH;
+	    add(rect3, x3, y3);
 	}
 }
