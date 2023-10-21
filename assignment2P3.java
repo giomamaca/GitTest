ffef65dd2c1e2da2867b97a25bc6fb8ce09a00e9
S <s>
2023-10-21 01:45:23 -0800

    {"types":"C","runs":17}

--- assignment2P3.java
+++ assignment2P3.java
@@ -10,7 +10,7 @@ public class assignment2P3 extends GraphicsProgram{
 		double y = getHeight() / 5;
 	    add(rect1, x, y);
 	    
-	    GLabel label1 = new GLabel (CENTER, 60, 60);
+	    GLabel label1 = new GLabel (CENTER, 100, 100);
 	    double xLabel = getWidth() / 2 - RECT_WIDTH / 2 ;
 	    double yLabel = getHeight() / 5;
 	    add(label1, xLabel, yLabel);
