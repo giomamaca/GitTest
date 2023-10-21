d8142fe2fe16f767bdf0ad25104648c827a3bf14
S <s>
2023-10-21 00:54:22 -0800

    {"types":"C","runs":2}

--- assignment2P3.java
+++ assignment2P3.java
@@ -1,4 +1,9 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class assignment2P3 {
-
+public class assignment2P3 extends GraphicsProgram{
+	public void run(){
+		GLabel label = new GLabel (CENTER, 44, 44);
+		add(label);
+	}
 }
