8c2209457c3305455f48f52193b124666daced99
S <s>
2023-10-21 01:33:11 -0800

    {"types":"C","runs":4}

--- assignment2P3.java
+++ assignment2P3.java
@@ -3,7 +3,7 @@ import acm.program.GraphicsProgram;
 
 public class assignment2P3 extends GraphicsProgram{
 	private static final double RECT_WIDTH = 150;
-	private static final double RECT_HEIGHT = 100;
+	private static final double RECT_HEIGHT = 75;
 	public void run(){
 		GRect rect1 = new GRect (RECT_WIDTH, RECT_HEIGHT);
 		add(rect1);
