6c0bc39a0e7ed862147dd42a361aaba81e1d9225
S <s>
2023-10-22 06:31:29 -0800

    {"types":"C","runs":25}

--- exercise.java
+++ exercise.java
@@ -2,6 +2,13 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
 public class exercise extends GraphicsProgram{	
+	private int max(int num1,int num2){
+		if(num1>=num2){
+			return num1;
+		}
+		return num2;
+	}
 	public void run(){
+		int larger = max(5, 1);
 	}
 }
