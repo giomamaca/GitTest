3183489aee200708acc158a847c794ca92b57525
S <s>
2023-10-22 00:45:30 -0800

    {"types":"C","runs":4}

--- assignment2P3.java
+++ assignment2P3.java
@@ -13,7 +13,7 @@ public class assignment2P3 extends GraphicsProgram{
 	    add(topRect, x, y);
 	    
 	    GLabel topLabel = new GLabel ("Program", getWidth() / 2 , getHeight() / 5);
-	    topLabel.getWidth();
+	    double a = topLabel.getWidth();
 	    add(topLabel);
 	    
 	    GRect midRect = new GRect (RECT_WIDTH, RECT_HEIGHT);
