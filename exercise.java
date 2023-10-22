bc108979ddb13ed9e635bdfeb12f241d8ba396a8
S <s>
2023-10-22 01:21:54 -0800

    {"types":"C","runs":21}

--- exercise.java
+++ exercise.java
@@ -3,8 +3,5 @@ import acm.program.GraphicsProgram;
 
 public class exercise extends GraphicsProgram{	
 	public void run(){
-		GLabel lab = new GLabel ("dasdadad", 100, 110);
-		double a = lab.getWidth();
-		add(lab);
 	}
 }
