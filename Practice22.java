2da57e2ea12f27133a5422ec8ed93c6fb7f92b69
S <s>
2023-10-22 06:06:59 -0800

    {"types":"C","runs":16}

--- Practice22.java
+++ Practice22.java
@@ -1,4 +1,23 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Practice22 {
-
+public class Practice22 extends GraphicsProgram{
+	private static final double BOARD_WIDTH = 30;
+	private static final double BOARD_HEIGHT = 30;
+	private static final double BLOCKS_IN_ROW = 8;
+	private static final double BLOCKS_IN_COLUMN = 8;
+	public void run(){
+		for(int i = 0; i < BLOCKS_IN_ROW; i++){
+			for(int j = 0; j < BLOCKS_IN_COLUMN; j++){
+				chessBoard(i, j);
+			}
+		}
+	}
+	private void chessBoard(int  i, int j) {
+		int chessX = (int) (i * BLOCKS_IN_ROW);
+		int chessY = (int) (j * BLOCKS_IN_COLUMN);
+		
+		GRect chess = new GRect (chessX, chessY, BOARD_WIDTH, BOARD_HEIGHT);
+		add(chess);
+	}
 }
