b3566271ab860dd63f6d6c0185282e874cfb62b3
S <s>
2023-10-22 05:24:53 -0800

    {"types":"C","runs":5}

--- Practice21.java
+++ Practice21.java
@@ -8,8 +8,8 @@ public class Practice21 extends GraphicsProgram {
 	private static final int CELL_NUMEBR_IN_ROW = 10;
 	private static final int CELL_NUMEBR_IN_COLUMN = 10;
 
-	private static final int CELL_HEIGHT = 50;
-	private static final int CELL_WIDTH = 50;
+	private static final int CELL_HEIGHT = 30;
+	private static final int CELL_WIDTH = 30;
 
 	public void run() {
 
