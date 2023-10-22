e51019d62b9d6cf6f624be36fbeaeea64b4884a9
S <s>
2023-10-22 05:31:52 -0800

    {"types":"C","runs":7}

--- Practice21.java
+++ Practice21.java
@@ -13,8 +13,24 @@ public class Practice21 extends GraphicsProgram {
 
 	public void run() {
 
+		for (int i = 0; i < CELL_NUMEBR_IN_COLUMN; i++) {
 			for (int j = 0; j < CELL_NUMEBR_IN_ROW; j++) {
-			int rectY = j * CELL_HEIGHT;
-		}
+				// (i;j) (column; row)
+				addCellOnIColumnJRow(i, j);
+			}
+		}	
 	}
+
+	private void addCellOnIColumnJRow(int i, int j) {
+		int rectX = i * CELL_WIDTH;
+		int rectY = j * CELL_HEIGHT;
+
+		GRect rect = new GRect(rectX, rectY, CELL_WIDTH, CELL_HEIGHT);
+		if ((i + j) % 2 == 0) {
+			rect.setFilled(true);
+			rect.setFillColor(Color.RED);
+		}
+		add(rect);
+	}
+
 }
