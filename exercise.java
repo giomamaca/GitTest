5c9708d9bfd779df4eb8285590f9c9c42f5f95ee
S <s>
2023-10-23 02:16:13 -0800

    {"types":"C","runs":1}

--- exercise.java
+++ exercise.java
@@ -1,14 +1,8 @@
-import acm.program.ConsoleProgram;
 
-public class exercise extends ConsoleProgram{	
-	private int max(int num1,int num2){
-		if(num1>=num2){
-			return num1;
-		}
-		return num2;
-	}
-	public void run(){
-		int larger = max(5, 1);
-		print(larger);
-	}
+import acm.program.GraphicsProgram;
+
+public class exercise extends GraphicsProgram{
+	public void run()
+	for(int i = 0; i <)
+
 }
