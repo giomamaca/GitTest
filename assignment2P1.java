081ee3b7692eec1f3addf1273b0d94ba22795a29
S <s>
2023-10-23 06:38:47 -0800

    {"types":"C","runs":65}

--- assignment2P1.java
+++ assignment2P1.java
@@ -14,8 +14,8 @@ public class assignment2P1 extends GraphicsProgram {
 		}
 	}
 	private void addFirstLine(int i, int j) {
-		double rectx = i * BRICK_WIDTH + (getWidth() - BRICK_HEIGHT) / 4 - BRICK_WIDTH / 2;
-		double recty = j * BRICK_HEIGHT + getHeight() - BRICK_HEIGHT;
+		double rectx = i * BRICK_WIDTH + (getWidth() - BRICK_HEIGHT) / 4 ;
+		double recty = j * BRICK_HEIGHT ;
 		
 		GRect rect = new GRect(rectx, recty, BRICK_WIDTH, BRICK_HEIGHT);
 		add(rect);
