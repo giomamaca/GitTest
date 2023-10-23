209b48b64ead30f63dcd217e380bf603bddbecfc
S <s>
2023-10-23 06:39:45 -0800

    {"types":"C","runs":69}

--- assignment2P1.java
+++ assignment2P1.java
@@ -9,7 +9,7 @@ public class assignment2P1 extends GraphicsProgram {
 	public void run(){
 		for(int i = 0; i < BRICK_IN_BASE; i++){
 			for(int j = 0; j < BRICK_IN_ROW; j++){
-				addFirstLine(i, j);
+				addFirstLine(i + 1, j);
 			}
 		}
 	}
