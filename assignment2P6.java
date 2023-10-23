4feaed29103f89fb4abbeb5201ac3b5df84c77e9
S <s>
2023-10-23 06:21:46 -0800

    {"types":"C","runs":50}

--- assignment2P6.java
+++ assignment2P6.java
@@ -4,8 +4,10 @@ public class assignment2P6 extends ConsoleProgram{
 	public void run(){
 		int a = readInt ("enter number = ");
 		for(int i = 1; i >= 1; i--){
-			int c = a / 2;
-			print(c);
+			if(a % 2 == 0){
+				int c = a / 2;
+				print(c);
+			}
 		}
 	}
 }
