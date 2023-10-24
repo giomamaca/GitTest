9a505e51c0984ecc356b152a4b2da47d18a03cd1
S <s>
2023-10-23 23:21:09 -0800

    {"types":"C","runs":1}

--- Practice26.java
+++ Practice26.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class Practice26 {
-
+public class Practice26 extends ConsoleProgram{
+	private static final int GRID_SIZE = 30;
+	private int gcd (int a, int b){
+		
+	}
+	public void run(){
+		
+	}
 }
