2ab069ac730f2bf8743e8912f2f2e563564885b1
S <s>
2023-10-24 06:34:32 -0800

    {"types":"C","runs":59}

--- assignment2P1.java
+++ assignment2P1.java
@@ -8,9 +8,12 @@ public class assignment2P1 extends GraphicsProgram{
 	public void run(){
 		for(int i = 0; i <= BLOCKS; i++){
 			for(int j = 0; j < i; j++){
-				GRect stairs = new GRect (j*BLOCK_WIDTH + getWidth() / 2 - BLOCK_WIDTH / 2 - i*(BLOCK_WIDTH / 2), i*BLOCK_HEIGHT + getHeight() - BLOCKS*BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT);
-				add(stairs);
+				addPiramid(i,j);
 			}
 		}
+	}
+	private void addPiramid(int i, int j) {
+		GRect stairs = new GRect (j*BLOCK_WIDTH + getWidth() / 2 - BLOCK_WIDTH / 2 - i*(BLOCK_WIDTH / 2), i*BLOCK_HEIGHT + getHeight() - BLOCKS*BLOCK_HEIGHT, BLOCK_WIDTH, BLOCK_HEIGHT);
+		add(stairs);
 	}
 }
