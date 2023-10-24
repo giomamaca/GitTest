df4c8d60cb9d3fa285943c9de54891ed56179be3
S <s>
2023-10-24 05:40:19 -0800

    {"types":"C","runs":1}

--- stairs.java
+++ stairs.java
@@ -1,4 +1,19 @@
+import acm.program.GraphicsProgram;
 
-public class stairs {
-
+public class stairs extends GraphicsProgram{
+	private static final double WIDTH = 30;
+	private static final double HEIGHT = 15;
+	private static final double DOWN = 15;
+	private static final double RIGHT = 30;
+	public void run(){
+		for(int i = 0; i < 10; i++){
+			for(int j = 0; j < 10; j++){
+				addStairs(i, j);
+			}
+		}
+	}
+	private void addStairs(int i, int j) {
+		int stairs = (int) ((int) i * WIDTH) + i*30;
+		int stairsy = (int) ((int) j *HEIGHT) + j*15;
+	}
 }
