66cb4805f248944151dd1d928d0f926109d957b9
S <s>
2023-10-24 06:32:07 -0800

    {"types":"C","runs":57}

--- assignment2P1.java
+++ assignment2P1.java
@@ -3,14 +3,12 @@ import acm.program.GraphicsProgram;
 
 public class assignment2P1 extends GraphicsProgram{
 	private static final double BLOCKS = 14;
-	private static final double WIDTH = 30;
-	private static final double HEIGHT = 15;
-	private static final double DOWN = 15;
-	private static final double RIGHT = 30;
+	private static final double BLOCK_WIDTH = 30;
+	private static final double BLOCK_HEIGHT = 15;
 	public void run(){
 		for(int i = 0; i <= BLOCKS; i++){
 			for(int j = 0; j < i; j++){
-				GRect stairs = new GRect (j*RIGHT + getWidth() / 2 - WIDTH / 2 - i*(WIDTH / 2), i*DOWN + getHeight() - BLOCKS*HEIGHT, WIDTH, HEIGHT);
+				GRect stairs = new GRect (j*BLOCK_WIDTH + getWidth() / 2 - WIDTH / 2 - i*(WIDTH / 2), i*BLOCK_HEIGHT + getHeight() - BLOCKS*HEIGHT, WIDTH, HEIGHT);
 				add(stairs);
 			}
 		}
