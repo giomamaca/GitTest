c896e7d375eb20878d5fea210c35660c5e654e61
S <s>
2023-10-23 23:57:35 -0800

    {"types":"C","runs":14}

--- Practice28.java
+++ Practice28.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Practice28 {
-
+public class Practice28 extends ConsoleProgram{
+	
+	private static final double SENTINEL = -1;
+	
+	public void run(){
+		int answer = 0;
+		while(true){
+			int a = readInt();
+			if(a == SENTINEL){
+				println(answer);
+			}else{
+				if(a % 2 == 0){
+					answer++;
+				}
+			}
+		}
+		
+	}
 }
