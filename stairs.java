3e0dce54f25829da8bbaab48f3b17e7cd040c894
S <s>
2023-10-24 06:13:29 -0800

    {"types":"C","runs":33}

--- stairs.java
+++ stairs.java
@@ -8,7 +8,7 @@ public class stairs extends GraphicsProgram{
 	private static final double RIGHT = 30;
 	public void run(){
 		for(int i = 0; i <= 7; i++){
-			for(int j = 0; j < i; j++){
+			for(int j = 0; j < 7; j++){
 				
 				GRect stairs = new GRect (j*RIGHT -WIDTH / 2 +300, i*DOWN, WIDTH, HEIGHT);
 				add(stairs);
