9a825de14f3a5aa65867b35ed6b1659f8b40a955
S <s>
2023-10-23 23:23:10 -0800

    {"types":"C","runs":2}

--- Practice27.java
+++ Practice27.java
@@ -4,7 +4,9 @@ public class Practice27 extends ConsoleProgram{
 	public void run(){
 		int n = readInt ("enter n = ");
 		int m = readInt ("enter m = ");
-		int a = (int) n^m;
-		print(a);
+		for(int i = 0; i < m ; i++){
+		    int	a = n * m;
+			print(a);
+		}
 	}
 }
