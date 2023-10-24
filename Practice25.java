30275ae76cd77b408e0c25d3cca13ba063fae1e9
S <s>
2023-10-23 23:57:36 -0800

    {"types":"C","runs":14}

--- Practice25.java
+++ Practice25.java
@@ -2,6 +2,10 @@ import acm.program.ConsoleProgram;
 
 public class Practice25 extends ConsoleProgram{
 	public void run(){
-		
+		int a = readInt ("enter a = ");
+		int b = readInt ("enter b = ");
+		for(int i = 0; true; i += a){
+			
+		}
 	}
 }
