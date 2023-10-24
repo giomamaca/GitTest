6588ee28684f35c1d0b58d5c6538f5bcabcdaccb
S <s>
2023-10-23 23:33:23 -0800

    {"types":"C","runs":7}

--- Practice26.java
+++ Practice26.java
@@ -3,9 +3,22 @@ import acm.program.ConsoleProgram;
 public class Practice26 extends ConsoleProgram{
 	private static final int GRID_SIZE = 30;
 	private int gcd (int a, int b){
-		
+		while( a != 0 && b != 0){
+			if(a > b){
+				a %= b;
+			}else{
+				b %= a;
+			}
+		}
+		if(a == 0){
+			return b;
+		}
+		return a;
 	}
 	public void run(){
+		int a =readInt();
+		int b = readInt();
+		int g = gcd(a,b);
 		
 	}
 }
