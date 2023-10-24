5fa9990a033c1868f2fa5ce487bb48f67edd6466
S <s>
2023-10-24 06:29:14 -0800

    {"types":"A","runs":53}

---
+++ assignment2P1.java
@@ -0,0 +1,21 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class assignment2P1 extends GraphicsProgram{
+	private static final double BLOCKS = 14;
+	private static final double WIDTH = 30;
+	private static final double HEIGHT = 12;
+	private static final double DOWN = 15;
+	private static final double RIGHT = 30;
+	public void run(){
+		for(int i = 0; i <= BLOCKS; i++){
+			for(int j = 0; j < i; j++){
+				addPiramid(i,j);
+			}
+		}
+	}
+	private void addPiramid(int i, int j) {
+		GRect stairs = new GRect (j*RIGHT + getWidth() / 2 - WIDTH / 2 - i*(WIDTH / 2), i*DOWN + getHeight() - 14*HEIGHT, WIDTH, HEIGHT);
+		add(stairs);
+	}
+}
