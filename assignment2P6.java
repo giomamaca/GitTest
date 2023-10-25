3b483c3d910147c9bd7087c099a479ac6822a877
S <s>
2023-10-25 01:11:36 -0800

    {"types":"C","runs":10}

--- assignment2P6.java
+++ assignment2P6.java
@@ -3,10 +3,12 @@ import acm.program.ConsoleProgram;
 public class assignment2P6 extends ConsoleProgram{
 	public void run(){
 		int a = readInt ("enter number = ");
-		if(a % 2 == 0){
-			for(int i = 0; true; i++){
-				int c = i*(a / 2);
-				print(c);
+		while(a != 1){
+			if(a % 2 == 0){
+				for(int i = 0; true; i++){
+					int c = i*(a / 2);
+					print(c);
+				}
 			}
 		}
 	}
