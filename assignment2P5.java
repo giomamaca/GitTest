dc29dc0b4e01ca6766d6f3c5d067d11d783f447c
S <s>
2023-10-26 07:32:44 -0800

    {"types":"C","runs":6}

--- assignment2P5.java
+++ assignment2P5.java
@@ -4,10 +4,8 @@ public class assignment2P5 extends ConsoleProgram{
 	public void run(){
 		println("This program finds smallest and largest numbers.");
 		int n = readInt ("? ");
-		while(n != 0){
-			if(n != 0){
+		if(n != 0){
 			println("?" + n);
-		}
 		}
 	}
 }
