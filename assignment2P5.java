467ee8fbd327303337066c045b72863ca42ad8da
S <s>
2023-10-26 07:28:58 -0800

    {"types":"C","runs":1}

--- assignment2P5.java
+++ assignment2P5.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class assignment2P5 {
+public class assignment2P5 extends ConsoleProgram{
+	public void run(){
+		int n = readInt ("enter number: ");
+		while(n != 0){
 
+		}
+	}
 }
