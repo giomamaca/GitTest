534faef215f614da64a6c15d221659f0a4198bdc
S <s>
2023-10-26 23:23:59 -0800

    {"types":"C","runs":5}

--- Practice30.java
+++ Practice30.java
@@ -5,13 +5,12 @@ public class Practice30 extends ConsoleProgram{
 		int k = 0;
 		while(n > 0){
 			k = k * 10 + n % 10;
-			int digital = n / 10;
+			n = n / 10;
 		}
 		return k;
 	}
 	public void run(){
 		int a = readInt("?");
-		println("aa");
 		println(reverseNumber(a));
 	}
 }
