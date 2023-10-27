4d1ffb4d4fec4b6a1429c8ae22ebc653fce0a969
S <s>
2023-10-27 11:12:18 -0800

    {"types":"C","runs":93}

--- Practice25.java
+++ Practice25.java
@@ -1,29 +1,7 @@
 import acm.program.ConsoleProgram;
 
 public class Practice25 extends ConsoleProgram{
-	
-	private int gcd(int n, int m) {
-		for (int i = Math.min(n, m); i >= 1; i--) {
-			if(n % i == 0 && m % i == 0) { 
-				return i;
-			}
-		}
-		return -1;
-	}
-	
-	private int readPositiveInt(String text) {
-		int n = readInt(text);
-		while( n <= 0) {
-			println("Number should positive");
-			n = readInt(text);
-		}
-		return n;
-	}
-	
 	public void run(){
-		int n = readPositiveInt("Enter n: ");
-		int m = readPositiveInt("Enter m: ");
-		int gcd = gcd(n, m);
-		println("GCD is: " + gcd);
+		
 	}
 }
--- Practice26.java
+++ Practice26.java
@@ -1,25 +1,19 @@
 import acm.program.ConsoleProgram;
 
 public class Practice26 extends ConsoleProgram{
-	private int gcd (int a, int b){
-		while( a != 0 && b != 0){
-			if(a > b){
-				a %= b;
-			}else{
-				b %= a;
+	
+	private int gcd(int a, int b){
+		while(a != b){
+			for(int i = 0; i < a; i++){
+				if(a > 0){
+					a *= i;
+					print(a);
+				}
 			}
 		}
-		if(a == 0){
-			return b;
-		}
-		return a;
 	}
 	
-	// 24 16 16 8 8 0  
 	public void run(){
-		int a =readInt("a = ");
-		int b = readInt("b = ");
-		int g = gcd(a,b);
-		println("gcd is:" + g);
+		
 	}
 }
