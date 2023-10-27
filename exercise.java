6bacc745b40b72219d50a96c2d6bd0b43ceb8045
S <s>
2023-10-27 09:57:42 -0800

    {"types":"C","runs":66}

--- exercise.java
+++ exercise.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class exercise extends ConsoleProgram{
 	
 	private int factorial(int a){
-		for(int i = 0; i < a; i++){
+		for(int i = 1; i < a; i++){
 			if(a > 0){
 				a = a * i*a;
 			}
