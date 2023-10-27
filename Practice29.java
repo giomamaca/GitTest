2abfdbe7dc823638f7cedf6a6cc3326f7572cadf
S <s>
2023-10-26 22:31:07 -0800

    {"types":"C","runs":7}

--- Practice29.java
+++ Practice29.java
@@ -1,13 +1,14 @@
 import acm.program.ConsoleProgram;
 
 public class Practice29 extends ConsoleProgram{
-	private void fibonacci(int n){
+	private int fibonacci(int n){
 		int a = 1, b = 1;
 		for(int i = 1; i<=n; i++){
 			int c = a+ b;
 			a = b;
 			b = c;
 		}
+		return b;
 	}
 	public void run(){
 
