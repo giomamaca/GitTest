9e3da7bca4257947618bb8f60874f37854bbc4a1
S <s>
2023-10-27 09:33:01 -0800

    {"types":"C","runs":53}

--- Practice31.java
+++ Practice31.java
@@ -3,10 +3,9 @@ import acm.program.ConsoleProgram;
 public class Practice31 extends ConsoleProgram{		
 	
 	private int simpleOrComplex(int a){
-		int n = readInt("");
 		int result = 0;
-		for(int i = 0; i <= (n / 2); i++){ 
-			if(n % result == 0){
+		for(int i = 0; i <= (a / 2); i++){ 
+			if(a % result == 0){
 				result++;
 			}
 		}
