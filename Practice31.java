7653aa5409313449ce2bec7bcda5556d6492ab6d
S <s>
2023-10-27 08:23:08 -0800

    {"types":"C","runs":6}

--- Practice31.java
+++ Practice31.java
@@ -2,18 +2,18 @@ import acm.program.ConsoleProgram;
 
 public class Practice31 extends ConsoleProgram{	
 	
-	private int simpleOrComplex(int b){
-		int result = 0;
-		int a = 2;
-		for (int i = 0; i <= (a / 2); i++){
-			a = a /(i*1);
-			result++;
+	private boolean isPrimeSlow(int n){
+		int numdivision = 0;
+		for(int i = 1; i <= n; i ++){
+			if(n % 1 == 0){
+				numdivision++;
+			}
 		}
-		return result;
+		return numdivision == 2;
 	}
 	
 	public void run(){
-		int k = readInt("enter your number = ");
-		println(simpleOrComplex(k));
+		int k = readInt("");
+		println(isPrimeSlow(k));
 	}
 }
