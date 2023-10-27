029a591ff17f6dfaa64420858d5192040f9ed45d
S <s>
2023-10-27 10:33:29 -0800

    {"types":"C","runs":87}

--- Practice25.java
+++ Practice25.java
@@ -1,7 +1,21 @@
 import acm.program.ConsoleProgram;
 
 public class Practice25 extends ConsoleProgram{
+	
+	private int gcdFast(int a, int b) {
+		while (a % b != 0 && b % a != 0) {
+			if (a > b) {
+				a %= b;
+			} else {
+				b %= a; 
+			}
+		}
+		return Math.min(a, b);
+	}
+	
 	public void run(){
-
+		int k = readInt("");
+		int c = readInt("");
+		println(gcdFast(k, c));
 	}
 }
