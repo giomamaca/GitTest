27c9663883f68c68fddb33cdf37e95e188592cd9
S <s>
2023-10-27 09:03:44 -0800

    {"types":"C","runs":43}

--- Practice30.java
+++ Practice30.java
@@ -4,13 +4,13 @@ public class Practice30 extends ConsoleProgram{
 	private int reverseNumber(int n){
 		int k = 0;
 		while(n > 0){
-			k = k * 10 + n % 10;
+			k = k + n % 10;
 			n = n / 10;
 		}
 		return k;
 	}
 	public void run(){
-		int a = readInt("?");
-		println(reverseNumber(a));
+		int n = readInt("enter num = ");
+		println(reverseNumber(n));
 	}
 }
