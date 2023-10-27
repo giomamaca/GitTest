fc37a7dc5de6d6539b74bfc6d2bd7bef69875c99
S <s>
2023-10-27 11:13:03 -0800

    {"types":"C","runs":96}

--- exercise.java
+++ exercise.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class exercise extends ConsoleProgram{	
 	public void run(){
-		int a = 5;
+		int a = 8;
 		for(int i = 0; i < a; i++){
 			if(a > 0){
 				a *= i;
