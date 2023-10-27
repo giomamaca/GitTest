df88d7860b836489d4a652f758953a7e267aacd5
S <s>
2023-10-27 09:03:45 -0800

    {"types":"C","runs":43}

--- Practice31.java
+++ Practice31.java
@@ -1,19 +1,7 @@
 import acm.program.ConsoleProgram;
 
 public class Practice31 extends ConsoleProgram{	
-	
-	private boolean isPrimeSlow(int n){
-		int numdivision = 0;
-		for(int i = 1; i <= n; i ++){
-			if(n % 2 == 0){
-				numdivision++;
-			}
-		}
-		return numdivision == 2;
-	}
-	
 	public void run(){
-		int k = readInt("");
-		println(isPrimeSlow(k));
+		
 	}
 }
