70276d8eda49964ddd80bbfffc96730d5d501923
S <s>
2023-10-26 22:29:03 -0800

    {"types":"C","runs":4}

--- Practice30.java
+++ Practice30.java
@@ -3,9 +3,16 @@ import acm.program.ConsoleProgram;
 public class Practice30 extends ConsoleProgram{
 	private int reverseNumber(int n){
 		int result = 0;
+		while(n > 0){
+			int digital = n / 10;
+			result += digital;
+			n /= 10;
+			
+		}
 		return result;
 	}
 	public void run(){
-		
+		int a = readInt("?");
+		println(reverseNumber(int n));
 	}
 }
