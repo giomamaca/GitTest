5367e7835163e7ff231a476d34b4965de3e68211
S <s>
2023-10-27 01:56:19 -0800

    {"types":"C","runs":8}

--- Practice31.java
+++ Practice31.java
@@ -5,7 +5,7 @@ public class Practice31 extends ConsoleProgram{
 		int a = readInt("");
 		for(int i = 0 ; i < a; i++){
 			if(true){
-			int	k = a / (a - i*1);
+			int	k = a / (a - (i/2)*1);
 			
 			println("k gamoyofebi" + k);
 			}
