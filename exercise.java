db94729f9d3561074808c5d8fdb972351638f11d
S <s>
2023-10-27 10:10:16 -0800

    {"types":"C","runs":76}

--- exercise.java
+++ exercise.java
@@ -4,10 +4,8 @@ public class exercise extends ConsoleProgram{
 	
 	private int factorial(int a){
 		for(int i = 1; i <= a; i++){
-			if(true){
 			   a *= a - 1 ;
 			   a = a - 1;
-			}
 		}
 		return a;
 	}
