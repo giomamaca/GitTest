097dd3cea72e6581bfc328ba493cb4699f42eea9
S <s>
2023-10-26 22:16:27 -0800

    {"types":"C","runs":3}

--- Practice30.java
+++ Practice30.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class Practice30 {
-
+public class Practice30 extends ConsoleProgram{
+	private int reverseNumber(int n){
+		int result = 0;
+		return result;
+	}
+	public void run(){
+		
+	}
 }
