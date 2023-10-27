14149224c40d5cff57e3641e947f88b8bac07150
S <s>
2023-10-27 08:53:23 -0800

    {"types":"C","runs":33}

--- Practice23.java
+++ Practice23.java
@@ -4,8 +4,8 @@ public class Practice23 extends ConsoleProgram{
 	public void run(){
 		int sum = readInt("enter number = ");
 		int n = 0;
-		for(int i = 1; i < sum; i++){
-			n += n + i * 1; 
+		for(int i = 0; i < sum; i++){
+			n += i; 
 		}
 		println("sum = " + n);
 	}
