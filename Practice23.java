5d0af3db373b94e3aff36417b800fe5b3d24e1a3
S <s>
2023-10-27 08:38:28 -0800

    {"types":"C","runs":22}

--- Practice23.java
+++ Practice23.java
@@ -4,10 +4,10 @@ public class Practice23 extends ConsoleProgram{
 	
 	private int sum(int k){
 		int n = 0;
-		for(int i = 0; true; i++){
+		for(int i = 0; i < 5; i++){
 			n = n + 1;
-			return n;
 		}
+		return n;
 	}
 	
 	public void run(){
