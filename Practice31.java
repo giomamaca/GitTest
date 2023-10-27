6ca8b2c36d624883ecaef56ccfbaf361737d97c3
S <s>
2023-10-27 09:21:32 -0800

    {"types":"C","runs":48}

--- Practice31.java
+++ Practice31.java
@@ -1,7 +1,15 @@
 import acm.program.ConsoleProgram;
 
-public class Practice31 extends ConsoleProgram{	
+public class Practice31 extends ConsoleProgram{		
 	public void run(){
-		
+		int n = readInt("");
+		int k = 0;
+		for(int i = 0; i<(n / 2); i++){
+			if(n % i == 0){
+				n = n /1; 
+				k++;
+				println("dsdsd" + k);
+			}
+		}
 	}
 }
