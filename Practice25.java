8647bd60d50e05a71d38a01236ab9a6553985aaf
S <s>
2023-10-27 02:32:59 -0800

    {"types":"C","runs":18}

--- Practice25.java
+++ Practice25.java
@@ -1,16 +1,14 @@
 import acm.program.ConsoleProgram;
 
 public class Practice25 extends ConsoleProgram{
-	public void run(){
-		int answer = 1;
-		int a = readInt ("enter a = ");
-		int b = readInt ("enter b = ");
-		for(int i = 0; true; i += a){
-			while (a != b){
-				answer += b;
-			    answer += a;
-			}
-			print(answer);
+	
+	private int gcd(int a, int b){
+		while(a % b != 0 && a != 0){
+			a %= b;
 		}
+	}
+	
+	public void run(){
+
 	}
 }
