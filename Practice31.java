1cf6b913bb02f84b25ab75045d6fec5a312c3394
S <s>
2023-10-27 09:43:25 -0800

    {"types":"C","runs":61}

--- Practice31.java
+++ Practice31.java
@@ -5,7 +5,9 @@ public class Practice31 extends ConsoleProgram{
 	private int simpleOrComplex(int a){
 		int result = 0;
 		for(int i = 0; i <= a; i++){ 
-			result++;
+			if(a % i == 0){
+				result++;
+			}
 		}
 		return result;
 	}
