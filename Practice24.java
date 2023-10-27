94884ffc6c629d807095ba80324357a0136a2d31
S <s>
2023-10-26 22:12:39 -0800

    {"types":"C","runs":1}

--- Practice24.java
+++ Practice24.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Practice24 {
-
+public class Practice24 extends ConsoleProgram{
+	private int numDivisorsSlow(int n) {
+		int result = 0;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				result++;
+			}
+		}
+		return result;
+	}	
 }
