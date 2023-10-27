718194a48c4b6befa461a64c4ea6555b78eeffeb
S <s>
2023-10-27 02:14:48 -0800

    {"types":"C","runs":14}

--- Practice31.java
+++ Practice31.java
@@ -2,13 +2,6 @@ import acm.program.ConsoleProgram;
 
 public class Practice31 extends ConsoleProgram{	
 	public void run(){
-		int a = readInt("");
-		for(int i = 0 ; i < a; i++){
-			if(true){
-			int	k = a / (a - i*1);
-			
-			println("k gamoyofebi" + k);
-			}
-		}
+		
 	}
 }
