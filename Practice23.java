9e4568db1d4a86e32589eb09b22e09b43bc4579c
S <s>
2023-10-27 08:33:16 -0800

    {"types":"C","runs":9}

--- Practice23.java
+++ Practice23.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class Practice23 {
-
+public class Practice23 extends ConsoleProgram{
+	
+	private int sum(int k){
+		int n = readInt("");
+		for(int i = 1; i <= n; i++){
+			n = n + 1;
+		}
+		return n;
+	}
+	
+	public void run(){
+		int k = readInt("enter num = ");
+		println(sum(k));
+	}
 }
