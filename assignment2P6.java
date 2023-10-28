0dc572f00228b5e8bc721e7c23b0fd5adaca2410
S <s>
2023-10-28 06:49:01 -0800

    {"types":"C","runs":6}

--- assignment2P6.java
+++ assignment2P6.java
@@ -1,23 +1,16 @@
 import acm.program.ConsoleProgram;
 
 public class assignment2P6 extends ConsoleProgram{
-	
-	private int oddOrEven(int k){
-		int a = readInt ("");
+	public void run(){
+		int a = readInt ("enter number = ");
 		while(a != 1){
 			if(a % 2 == 0){
 				println("" + a);
-				a = a / 2;
+				a /= 2;
 			}else{
 				println("" + a);
 				a = a*3 + 1;
 			}
 		}
-		return a;
-	}
-	
-	public void run(){
-		int c = readInt("");
-		println(oddOrEven(c));
 	}
 }
