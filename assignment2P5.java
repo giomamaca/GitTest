3776449763dafc3352429948d2bfdda1a90cf44f
S <s>
2023-10-28 08:20:14 -0800

    {"types":"C","runs":1}

--- assignment2P5.java
+++ assignment2P5.java
@@ -9,7 +9,7 @@ public class assignment2P5 extends ConsoleProgram{
 		int large = readInt("?");
 		int small = large;
 		if(large != STOP){
-			while(small != 0){
+			if(small != 0){
 				int k = readInt("?");
 				if(k != STOP){
 					if(k > large){
