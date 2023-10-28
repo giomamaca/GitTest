d346b25aebb71c7a27397225160322828982e4e6
S <s>
2023-10-28 06:33:28 -0800

    {"types":"C","runs":18}

--- exercise.java
+++ exercise.java
@@ -2,26 +2,6 @@ import acm.program.ConsoleProgram;
 
 public class exercise extends ConsoleProgram{		
 	public void run(){
-		int max = readInt("");
-		int min = max;
-		if(max == 0){
-			println("dasdadadad");
-		}else{
-			while(true){
-				int a = readInt("");
-				if(a == 0){
-					break;
-				}else{
-					if(a < min){
-						min = a;
-					}
-					if(a > max){
-						max = a;
-					}
-				}
-			}
-		}
-		println("largest " + max);
-		println("small " + min);
+
 	}
 }
