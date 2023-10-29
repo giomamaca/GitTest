b6922d67f34dd22e25f5d5749d8305a16d856760
S <s>
2023-10-29 04:33:16 -0800

    {"types":"C","runs":38}

--- Practise32.java
+++ Practise32.java
@@ -2,6 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class Practise32 extends ConsoleProgram{
 	public void run(){
+		int result = 0;
 		int a = readInt("enter a = ");
 		for(int i = 0; i <= a; i++){
 			println("");
