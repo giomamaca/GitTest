79e8a680c1271175d988c8ae2aab769b518781ea
S <s>
2023-10-29 10:53:35 -0800

    {"types":"C","runs":40}

--- Practise32.java
+++ Practise32.java
@@ -15,7 +15,7 @@ public class Practise32 extends ConsoleProgram{
 	}
 	
 	private void milion(){
-		for(int i = 2; i < 1000; i++){
+		for(int i = 2; i < 1000000; i++){
 			if(isSimple(i)){
 				println("? " + i);
 			}
