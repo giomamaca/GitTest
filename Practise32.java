a3018115b7da57aa1b9ef0882467b54947466c94
S <s>
2023-10-29 04:06:32 -0800

    {"types":"C","runs":17}

--- Practise32.java
+++ Practise32.java
@@ -3,8 +3,8 @@ import acm.program.ConsoleProgram;
 public class Practise32 extends ConsoleProgram{
 	
 	private int divisiors(int a){
-		for(int i = 1; i < a; i++){
-			return i;
+		for(int i = 0; i < a; i++){
+			return i*a;
 		}
 		return a;
 	}
