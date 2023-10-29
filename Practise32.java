b90cb363a110e82b20e461045cd32deba6b6bd31
S <s>
2023-10-29 08:56:25 -0800

    {"types":"C","runs":2}

--- Practise32.java
+++ Practise32.java
@@ -5,10 +5,8 @@ public class Practise32 extends ConsoleProgram{
 		int result = 0;
 		int a = readInt("enter a = ");
 		for(int i = 0; i <= a; i++){
-			for(int j = 0; j <= i; j++){
-				if(i % j == 0){
-					print(i);
-				}
+			if(a % i == 0){
+				result++;
 			}
 		}
 	}
