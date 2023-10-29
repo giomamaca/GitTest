a0ad0056393becdeaad8b1dfa45323680e9f8287
S <s>
2023-10-29 10:58:30 -0800

    {"types":"C","runs":43}

--- Practice322.java
+++ Practice322.java
@@ -2,21 +2,16 @@ import acm.program.ConsoleProgram;
 
 public class Practice322 extends ConsoleProgram{
 	
-	private void milion(){
-		int result = 0;
-		for(int i = 1; i <= 100; i++){
-			for(int j = 1; j <= i; i++){
-				if(i % j == 0){
-					result ++;
-				}
-				if(result == 2){
-					print(i);
-				}
+	private boolean simple(int k){
+		if(k == 1) return false;
+		for(int i = 0; i <= k; i++){
+			if(k % i == 0){
+				print(i);
 			}
 		}
 	}
 	
 	public void run(){
-		milion();
+		
 	}
 }
