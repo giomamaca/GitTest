8f17ac153d61d99920ebc8435eeeb78c6770134a
S <s>
2023-10-29 09:50:23 -0800

    {"types":"C","runs":16}

--- Practise32.java
+++ Practise32.java
@@ -7,7 +7,7 @@ public class Practise32 extends ConsoleProgram{
 		for(int i = 1; i <= n; i ++){
 				if(n % i == 0){
 				result++;
-				if(result == 2){
+				while(result == 2){
 					return i;
 				}
 			}
