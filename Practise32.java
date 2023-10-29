3fdb9d7593db1074049118912e17f9e870cd4403
S <s>
2023-10-29 09:54:29 -0800

    {"types":"C","runs":20}

--- Practise32.java
+++ Practise32.java
@@ -5,8 +5,10 @@ public class Practise32 extends ConsoleProgram{
 	private int milion(int n){
 		int result = 0;
 		for(int i = 1; i <= n; i ++){
-				if(n % i == 0){
-				result++;
+			for(int j = 0; j <= i; j++){
+				if(i % j == 0){
+					result ++;
+				}
 				while(n > 0){
 					if(result == 2){
 						return i;
