980979162297dd11d1f6c831e6dfdbce81fec56f
S <s>
2023-10-29 04:11:13 -0800

    {"types":"C","runs":26}

--- Practise32.java
+++ Practise32.java
@@ -5,8 +5,8 @@ public class Practise32 extends ConsoleProgram{
 	private int divisiors(int a){
 		for(int i = 0; i < a; i++){
 			a = 0;
+			println(a);
 			a = a + 1;
-			return a;
 		}
 		return 0;
 	}
