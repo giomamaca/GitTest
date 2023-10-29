c129ee5c0ef4d3a68ff0cc5bebead3caf5c8164c
S <s>
2023-10-29 09:45:21 -0800

    {"types":"C","runs":4}

--- Practice31.java
+++ Practice31.java
@@ -5,16 +5,16 @@ public class Practice31 extends ConsoleProgram{
 	public void run(){
 		int result = 0;
 		int n = readInt("enter n = ");
-		for (int i = 1; i <= n; i++) {
+		for(int i = 1; i <= n; i++) {
 			if (n % i == 0) {
 				result++;
 			}
 		}
 		if(result == 2){
-			println("martivia ");
+			println("simple ");
 		}
 		if(result != 2){
-			println("rtulia ");
+			println("complex ");
 		}
 	}
 }
