77675add46fad5aa81007bc04d9b54e36bc54b5d
S <s>
2023-10-30 02:58:14 -0800

    {"types":"C","runs":1}

--- Practice322.java
+++ Practice322.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
 
-public class Practice322 {
-
+public class Practice322 extends ConsoleProgram{
+	
+//	private int divisors(int a){
+//		for(int i = 1; i <= a; i++){
+//		}
+//		return 0;
+//	}
+	
+	private void milion(){
+		int result = 0;
+		int k = 100;
+		for(int j = 1; j < k; j++){
+			if(k % j == 0){
+				result++;
+			}
+			if(result == 2){
+				print(j);
+			}
+		}
+	}
+	
+	public void run(){
+		
+	}
 }
