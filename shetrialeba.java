76927b74400e9acf02b930263aa52efbc569f0b8
S <s>
2023-10-30 07:33:18 -0800

    {"types":"C","runs":1}

--- shetrialeba.java
+++ shetrialeba.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class shetrialeba {
-
+public class shetrialeba extends ConsoleProgram{
+	
+	private int turnNum(int a){
+		int k = 0;
+		while(a > 0){
+			k = k*10 + a % 10;
+			a = a / 10;
+		}
+	}
+	
+	public void run(){
+		int c = readInt();
+		println(turnNum(c));
+	}
 }
