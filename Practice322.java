d24f9681f86fe73dac5581807987d2eedeb09123
S <s>
2023-10-30 05:55:38 -0800

    {"types":"C","runs":2}

--- Practice322.java
+++ Practice322.java
@@ -2,12 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class Practice322 extends ConsoleProgram{
 	
-	private boolean isSimple(int a){
-		if(a == 1) return false;
-		if(a % 2 == 0) return false;
-		if(a == 2) return true;
-		for(int i = 3; i <= a; i += 2){
-			if (a % i == 0){
+	private boolean isSimple(int k){
+		if(k == 1) return false;
+		if(k % 2 == 0) return false;
+		if(k == 2) return true;
+		for(int i = 3; i <= k; i += 2){
+			if (k % i == 0){
 				return false;
 			}
 		}
