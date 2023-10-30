31c6376da58a120717aec17b91918efcddfd4648
S <s>
2023-10-30 05:55:58 -0800

    {"types":"C","runs":3}

--- Practice322.java
+++ Practice322.java
@@ -4,8 +4,8 @@ public class Practice322 extends ConsoleProgram{
 	
 	private boolean isSimple(int k){
 		if(k == 1) return false;
-		if(k % 2 == 0) return false;
 		if(k == 2) return true;
+		if(k % 2 == 0) return false;
 		for(int i = 3; i <= k; i += 2){
 			if (k % i == 0){
 				return false;
