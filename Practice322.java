894633e0bf29e4b3e29e7ac6f71735eed35ce15e
S <s>
2023-10-30 05:57:42 -0800

    {"types":"C","runs":6}

--- Practice322.java
+++ Practice322.java
@@ -15,7 +15,7 @@ public class Practice322 extends ConsoleProgram{
 	}
 	
 	private void milion(){
-		for(int i = 2; i < 1000; i++){
+		for(int i = 2; i < 1000; i += 2){
 			if(isSimple(i)){
 				println("? " + i);
 			}
