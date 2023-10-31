b2ff2eceaa224aabfff3449476bb5d23f3c3a682
S <s>
2023-10-31 00:09:27 -0800

    {"types":"C","runs":35}

--- Practice36.java
+++ Practice36.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Practice36 extends {
-
+public class Practice36 extends ConsoleProgram{
+	
+	RandomGenerator rgen = new RandomGenerator();
+	
+	
+	public void run(){	
+		int count = 0 ;
+		int result = 0;
+		
+		for(int i = 0; i < 1000000; i++){
+			boolean a = rgen.nextBoolean(); 
+			if(a == true){
+				println("ariol");
+				count++;
+			}else{
+				println("reshka");
+				count++;
+			}
+		}
+	}
 }
