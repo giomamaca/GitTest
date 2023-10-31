a9438e5631ec518e5f4afe4e98462050d55b3a6e
S <s>
2023-10-30 23:13:17 -0800

    {"types":"C","runs":1}

--- Practice33.java
+++ Practice33.java
@@ -1,4 +1,18 @@
+import java.awt.Color;
 
-public class Practice33 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class Practice33 extends GraphicsProgram{
+	public void run(){
+		int r = rgen.nextInt(10, 100);
+		int X = getWidth();
+		int Y = getHeight();
+		Color color = rgen.nextColor();
+		GOval oval = new GOval(X / 2 - r / 2, Y / 2 - r , r, r);
+		oval.setFilled(true);
+		oval.setColor(color);
+	}
+	RandomGenerator rgen = new RandomGenerator ();
 }
