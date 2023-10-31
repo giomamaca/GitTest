917032916978190fc0e402aa428d7242d6276048
S <s>
2023-10-31 09:19:58 -0800

    {"types":"C","runs":10}

--- Practice36.java
+++ Practice36.java
@@ -6,8 +6,8 @@ public class Practice36 extends ConsoleProgram{
 	RandomGenerator rgen = new RandomGenerator();
 	
 	public void run(){
-		int result = 0;
 		for(int i = 0; i < 100; i++){
+			int result = 0;
 			boolean a = rgen.nextBoolean(); 
 			if(a == true){
 				result++;
