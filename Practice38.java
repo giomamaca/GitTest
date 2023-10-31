2fa333b2d385c6b8e5e4891aed5913cc1f8344c5
S <s>
2023-10-31 10:30:43 -0800

    {"types":"C","runs":16}

--- Practice38.java
+++ Practice38.java
@@ -5,15 +5,27 @@ public class Practice38 extends ConsoleProgram{
 	
 	RandomGenerator rGen = new RandomGenerator();
 	
+	private boolean k(int k){
+		if(k >= 0) return true;
+		if(k <= 36) return true;
+		
+		if(k > 36) return false;
+		if(k < 0) return false;
+	}
+	
 	public void run(){
-		int bount = 100;
+		int bount = 1000;
 		int money = readInt("prise = ");
 		
 		for(int i = 0; i < 100000; i++){
 			int k = readInt("enter num = ");
+			if(k(k)){
+				print(k);
+			}
 			int a = rGen.nextInt(0, 36);
 			println("" + a);
 				
+			
 			if(a == k){
 				bount = bount  + money;
 				println("you won : " + bount);
