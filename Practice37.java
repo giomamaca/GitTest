92274a391c6709eb931897e93ac975e86596c90f
S <s>
2023-10-31 09:40:16 -0800

    {"types":"C","runs":18}

--- Practice37.java
+++ Practice37.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Practice37 {
-
+public class Practice37 extends ConsoleProgram{
+	
+	RandomGenerator rGen = new RandomGenerator();
+	
+	public void run(){
+		int bount = 1000; 
+		
+		boolean a = rGen.nextBoolean();
+		
+		while(bount != 0){
+			if(a == true){
+				bount = bount + 50;
+				println("you win: " + bount); 
+			}else{
+				bount = bount - 50;
+				println("you lose: " + bount);
+			}
+		}
+	}
 }
