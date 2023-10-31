77d37f71a2500a68af14a620c8c9ae81299a39ab
S <s>
2023-10-31 10:09:06 -0800

    {"types":"C","runs":29}

--- Practice38.java
+++ Practice38.java
@@ -1,4 +1,27 @@
+import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
-public class Practice38 {
-
+public class Practice38 extends ConsoleProgram{
+	
+	RandomGenerator rGen = new RandomGenerator();
+	
+	public void run(){
+		int bount = 1000;
+		int a = rGen.nextInt(0, 36);
+		int money = readInt();
+		int k = readInt("enter num = ");
+		
+		while(bount != 0){
+			for(int i = 0; true; i++){
+				boolean num = rGen.nextBoolean();
+				
+				if(a == k){
+					bount = bount  + money;
+				}
+				if(a != k ){
+					bount = bount - money;
+				}
+			}
+		}
+	}
 }
