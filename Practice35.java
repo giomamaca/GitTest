fbe747cbff6c138fb1ffe1f8c83d8862fc4aa79f
S <s>
2023-10-30 23:46:01 -0800

    {"types":"C","runs":14}

--- Practice35.java
+++ Practice35.java
@@ -1,4 +1,24 @@
+import acm.graphics.*;
+import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Practice35 {
-
+public class Practice35 extends ConsoleProgram{
+	
+	RandomGenerator rgen = new RandomGenerator();
+	
+	public void run(){
+		
+		int r = rgen.nextInt(10, 200);
+		int x = getWidth();
+		int y  = getHeight();
+		
+		boolean a = rgen.nextBoolean();
+		
+		if(a){
+			println("arioli");
+		}else{
+			println("reshka");
+		}
+	}	
 }
