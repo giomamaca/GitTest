6af5000a5da5965bb5fc67efb293a42918bdbab3
S <s>
2023-10-31 09:19:10 -0800

    {"types":"C","runs":9}

--- Practice36.java
+++ Practice36.java
@@ -11,7 +11,7 @@ public class Practice36 extends ConsoleProgram{
 			boolean a = rgen.nextBoolean(); 
 			if(a == true){
 				result++;
-				if(result != 3){
+				if(result != 4){
 					println("ariol");
 				}else{
 					break;
