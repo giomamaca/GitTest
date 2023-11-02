c804f797c88fe6e17ebc0d87828b1f22151e7f80
S <s>
2023-11-02 08:21:55 -0800

    {"types":"C","runs":20}

--- Practice25.java
+++ Practice25.java
@@ -4,14 +4,17 @@ public class Practice25 extends ConsoleProgram{
 	
 	private int divisors(int d, int p){
 		int l = 0;
-		for(int i = 1; i*i < d; i++){
+		for(int i = 2; i*i < d; i++){
 			if(d % i == 0){
 				l = i;
-				break;
 			}
 		}
-		d = d / l;
-		p = p / l;
+		if(d % l == 0){
+			d = d / l;
+		}
+		if(p % l == 0){
+			p = p / l;
+		}
 		l = d * p * l;
 		
 		return l;
