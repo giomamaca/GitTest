564eef53b8001a9041522a4077795ccee04ef491
S <s>
2023-11-02 07:52:03 -0800

    {"types":"C","runs":2}

--- Practice26.java
+++ Practice26.java
@@ -3,18 +3,10 @@ import acm.program.ConsoleProgram;
 public class Practice26 extends ConsoleProgram{
 	
 	private int gcd(int a, int b){
-		while(a != b){
-			for(int i = 0; i < a; i++){
-				if(a > 0){
-					a *= i;
-					print(a);
-				}
-			}
-		}
-		return a;
+		
 	}
 	
 	public void run(){
-	gcd(32, 16);	
+		gcd(32, 16);	
 	}
 }
