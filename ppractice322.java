b0f22ca3cf547fe78ed01bab2fdc22b86ec3905c
S <s>
2023-11-03 10:03:34 -0800

    {"types":"C","runs":3}

--- ppractice322.java
+++ ppractice322.java
@@ -4,19 +4,19 @@ public class ppractice322 extends ConsoleProgram{
 	
 	private int num = 1000;
 	
-	private void divisors(){
-		int c = 0;
-		for(int i = 2; i < num; i++){
-			if(num % i == 0){
-				c++;
-				if(c == 2){
-					print(i);
-				}else{
-					c = 0;
-				}
-			}
-		}
-	}
+//	private void divisors(){
+//		int c = 0;
+//		for(int i = 2; i < num; i++){
+//			if(num % i == 0){
+//				c++;
+//				if(c == 2){
+//					print(i);
+//				}else{
+//					c = 0;
+//				}
+//			}
+//		}
+//	}
 	
 	public void run(){
 		int c = 0;
