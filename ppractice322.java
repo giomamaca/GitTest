d60f6d1d31dac488d9f8990bd361d0ae7e5a01af
S <s>
2023-11-03 10:07:04 -0800

    {"types":"C","runs":6}

--- ppractice322.java
+++ ppractice322.java
@@ -1,35 +1,35 @@
 import acm.program.ConsoleProgram;
 
-public class ppractice322 extends ConsoleProgram{
-	
+public class ppractice322 extends ConsoleProgram {
+
 	private int num = 1000;
-	
-//	private void divisors(){
-//		int c = 0;
-//		for(int i = 2; i < num; i++){
-//			if(num % i == 0){
-//				c++;
-//				if(c == 2){
-//					print(i);
-//				}else{
-//					c = 0;
-//				}
-//			}
-//		}
-//	}
-	
-	public void run(){
+
+	// private void divisors(){
+	// int c = 0;
+	// for(int i = 2; i < num; i++){
+	// if(num % i == 0){
+	// c++;
+	// if(c == 2){
+	// print(i);
+	// }else{
+	// c = 0;
+	// }
+	// }
+	// }
+	// }
+
+	public void run() {
 		int c = 0;
-		for(int j = 2; j < 100; j++)
-		for(int i = 2; i < 100; i++){
-			if(i % i == 0){
-				c++;
-				if(c == 2){
-					print(i);
-				}else{
-					c = 0;
+		for (int j = 2; j < 100; j++)
+			for (int i = 2; i < 100; i++) {
+				if (j % i == 0) {
+					c++;
+					if (c == 2) {
+						print(j);
+					} else {
+						c = 0;
+					}
 				}
 			}
-		}
 	}
 }
