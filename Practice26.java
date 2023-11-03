30338ed88afdf8d152b240e3f93d79bf63ae1a13
S <s>
2023-11-03 02:07:52 -0800

    {"types":"C","runs":6}

--- Practice26.java
+++ Practice26.java
@@ -1,18 +1,18 @@
 import acm.program.ConsoleProgram;
 
-public class Practice26 extends ConsoleProgram{
-	
-	private int GCM(int a, int b){
+public class Practice26 extends ConsoleProgram {
+
+	private int GCM(int a, int b) {
 		int divisor = 0;
-		for(int i = 1; i <= a; i++){
-			if(a % i == 0 & b % i == 0){
+		for (int i = 1; i <= a; i++) {
+			if (a % i == 0 & b % i == 0) {
 				divisor = i;
 			}
 		}
 		return divisor;
 	}
-	
-	public void run(){
+
+	public void run() {
 		int a = readInt("a = ");
 		int b = readInt("b = ");
 		println("GCM = " + GCM(a, b));
