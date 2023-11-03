cd01caf7b96dde5488e0662232aca1236cc2bea4
S <s>
2023-11-02 21:56:31 -0800

    {"types":"C","runs":2}

--- exercise.java
+++ exercise.java
@@ -1,7 +1,23 @@
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 
 public class exercise extends ConsoleProgram{		
-	public void run(){
+//  Width of each brick in pixels */
+	private static final int BRICK_WIDTH = 30;
 
-	}
+//  Width of each brick in pixels */
+	private static final int BRICK_HEIGHT = 12;
+
+//  Number of bricks in the base of the pyramid */
+	private static final int BRICKS_IN_BASE = 14;
+		public void run(){
+			for(int i = 0; i <= BRICKS_IN_BASE; i++){
+				for(int j = 0; j < i; j++){
+					addPiramid(i,j);
+				}
+			}
+		}
+		private void addPiramid(int i, int j) {
+			addPiramid(i, j);
+		}
 }
