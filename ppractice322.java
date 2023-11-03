73be7ca8136fe9c9014c11637e8d8f9b8bf590c9
S <s>
2023-11-03 10:02:53 -0800

    {"types":"C","runs":1}

--- ppractice322.java
+++ ppractice322.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class ppractice322 {
-
+public class ppractice322 extends ConsoleProgram{
+	
+	private int num = 1000;
+	
+	private void divisors(){
+		int c = 0;
+		for(int i = 2; i < num; i++){
+			if(num % i == 0){
+				c++;
+				if(c == 2){
+					print(i);
+				}else{
+					c = 0;
+				}
+			}
+		}
+	}
+	
+	public void run(){
+		
+	}
 }
