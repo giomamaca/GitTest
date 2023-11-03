8fab271039157be675fcba75e47221d31f22a47b
S <s>
2023-11-02 22:59:37 -0800

    {"types":"C","runs":20}

--- Practice36.java
+++ Practice36.java
@@ -5,25 +5,27 @@ public class Practice36 extends ConsoleProgram {
 
 	RandomGenerator rgen = new RandomGenerator();
 
+	private static final double NUM_EXPERIMENTS = 20;
+
 	public void run() {
-		int divisor = 0;
-		int average = 0;
+		int experiment = 0;
 		int result = 0;
-		for (int i = 1; i < 1000000; i++) {
-			boolean a = rgen.nextBoolean();
-			if (a == true) {
-				average += i;
-				divisor ++;
-				println("arioli");
-				result++;
-			} else {
-				println("reshka");
-				result = 0;
-			}
-			if (result == 3) {
-				break;
+		for (int j = 1; j < NUM_EXPERIMENTS; j++) {
+			for (int i = 1; i < 1000000; i++) {
+				boolean a = rgen.nextBoolean();
+				if (a == true) {
+					println("arioli");
+					result++;
+				} else {
+					println("reshka");
+					result = 0;
+				}
+				if (result == 3) {
+					experiment ++;
+					break;
+				}
 			}
 		}
-		print(average / divisor);
+		print(experiment / NUM_EXPERIMENTS);
 	}
 }
