80d02a6e1ea970d63cec8c7f45ddf1308c8b84f6
S <s>
2023-11-03 08:48:01 -0800

    {"types":"C","runs":12}

--- exercise.java
+++ exercise.java
@@ -5,11 +5,9 @@ import java.awt.*;
 public class exercise extends ConsoleProgram {
 	public void run() {
 		int a = 100;
-		for (int j = 0; j < 10; j++) {
-			for (int i = 0; i < a; i++) {
-				int c = 100 - i;
-				println(c + " + " + i);
-			}
+		for(int i = 0; i < a; i++){
+			int c = 100 - i;
+			println(c + " + " + i);
 		}
 	}
 }
