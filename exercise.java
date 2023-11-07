c11e60fe1a5a05402a5868332c273eb55cbb9293
S <s>
2023-11-06 23:29:18 -0800

    {"types":"C","runs":18}

--- exercise.java
+++ exercise.java
@@ -15,9 +15,11 @@ public class exercise extends GraphicsProgram {
 		while(true){
 			Ball.move(x, y);
 			Ball.pause(5);
-			if(Ball.getX()== getWidth() - 18 && Ball.getY() == getHeight() - 18){
+			if(Ball.getX()== getWidth() - 18){
 				x *= -1;
-				y *= -1;
+				if(Ball.getY() == getHeight() - 18){
+					y *= -1;
+				}
 			}
 			if(Ball.getX() == 0){
 				x *= -1;					
