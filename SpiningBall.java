5a29e025b525ca4351abf4071829b1ce0c8267c0
S <s>
2023-11-07 00:01:15 -0800

    {"types":"C","runs":44}

--- SpiningBall.java
+++ SpiningBall.java
@@ -17,8 +17,8 @@ public class SpiningBall extends GraphicsProgram {
 			pause(10);
 
 			for (int i = 0; i < 10; i++) {
-				x = x + i * 0;
-				y = y + i * 0;
+				x = x + i * (1 / 10);
+				y = y + i * (1 / 10);
 			}
 		}
 	}
