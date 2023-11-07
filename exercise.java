f453b22e17fff0ddb1311b5348752294e0fd54d3
S <s>
2023-11-06 23:17:47 -0800

    {"types":"C","runs":7}

--- exercise.java
+++ exercise.java
@@ -2,20 +2,23 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class exercise extends GraphicsProgram {	
+public class exercise extends GraphicsProgram {
+	
+	private int x = 1;
+	
 	public void run() {
 		GOval Ball = new GOval (0, getHeight() / 2, 18, 18);
 		Ball.setFilled(true);
 		add(Ball);
 		
 		while(true){
+			Ball.move(x, 0);
+			Ball.pause(5);
 			if(Ball.getX()== 0){
-				Ball.move(100, 0);
-				pause(5);
+				x *= 1;
 			}
 			if(Ball.getX() == getWidth()){
-				Ball.move(-1,  0);
-				pause(5);
+				x *= -1;
 			}
 		}
 	}
