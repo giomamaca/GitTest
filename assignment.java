6b8e1e74d1ebd7fda21b312f4494eaa05d85d4ac
S <s>
2023-11-10 11:06:38 -0800

    {"types":"C","runs":37}

--- assignment.java
+++ assignment.java
@@ -57,7 +57,7 @@ public class assignment extends GraphicsProgram{
 		while(true){			
 			ball.move(vx, vy);
 			pause(10);
-			if(ball.getX() == APPLICATION_WIDTH - PADDLE_WIDTH){
+			if(ball.getX() == getWidth() - PADDLE_WIDTH){
 				vx = -vx;
 			}
 			if(ball.getX() == 0){
