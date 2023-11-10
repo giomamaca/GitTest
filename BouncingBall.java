899f10ea8be9232ed686c5a441d8bc29f1ae95af
S <s>
2023-11-10 02:30:35 -0800

    {"types":"C","runs":27}

--- BouncingBall.java
+++ BouncingBall.java
@@ -1,4 +1,19 @@
+import acm.program.*;
+import acm.graphics.*; 
 
-public class BouncingBall {
-
+public class BouncingBall extends GraphicsProgram{
+	
+	private static final double DIAM = 30;
+	private static final double GRAVITY = 0.1;
+	private double X = 5;
+	
+	public void run(){
+		GOval ball = new GOval (DIAM, DIAM);
+		ball.setFilled(true);
+		add(ball);
+		
+		while(ball.getX() > getWidth() - DIAM){
+			ball.move(X, 5);
+		}
+	}
 }
