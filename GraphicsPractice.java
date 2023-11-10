ae99839f4175a78566ef41cd39db6da8413bd111
S <s>
2023-11-10 03:15:30 -0800

    {"types":"C","runs":21}

--- GraphicsPractice.java
+++ GraphicsPractice.java
@@ -5,22 +5,9 @@ import acm.program.GraphicsProgram;
 
 public class GraphicsPractice extends GraphicsProgram {
 	public void run(){
-		
-		GOval rugbyBall = new GOval(100,200);
-		
-		add(rugbyBall, 100, 100);
-		//GOval secondOval = new GOval(100, 200);
-		//add(secondOval, 200, 200);
-		rugbyBall = new GOval(200, 200);
-		add(rugbyBall, 200, 200);
-		
-		GOval oldBall = rugbyBall;
-		rugbyBall = new GOval(100, 200, 200, 300);
-		
-		add(rugbyBall);
-		rugbyBall.setFilled(true);
-		rugbyBall.setColor(Color.RED);
-		
-		oldBall.setFilled(true);
+		GOval example = null;
+		if(example == null){
+			add(example);
+		}
 	}
 }
