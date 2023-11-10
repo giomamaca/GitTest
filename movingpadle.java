2d354ec17e6955ad5bb44d650fa14ccba38bd817
S <s>
2023-11-10 04:26:24 -0800

    {"types":"C","runs":15}

--- movingpadle.java
+++ movingpadle.java
@@ -13,7 +13,7 @@ public class movingpadle extends GraphicsProgram{
 		r.setFilled(true);
 	}
 	public void mouseMoved(MouseEvent e){
-		if(r.getX() != getWidth() - 50){
+		if(r.getX() != getWidth() - 100){
 			
 			r.setLocation(e.getX() - 50 , getHeight() - 50);
 			add(r);
