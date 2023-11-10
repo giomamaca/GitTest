60c517c3b0706090296ae48eeb4be27a4a0c5d15
S <s>
2023-11-09 22:47:32 -0800

    {"types":"C","runs":10}

--- Practice41.java
+++ Practice41.java
@@ -1,4 +1,26 @@
+import acm.program.GraphicsProgram;
 
-public class Practice41 {
+import java.awt.Component;
 
+import com.sun.glass.events.MouseEvent;
+
+import acm.graphics.GOval;
+import acm.program.*;
+
+public class Practice41 extends GraphicsProgram{
+	
+	private GOval ball;
+	
+	public void run(){
+		addMouseListeners();
+		PaintBall();
+	}
+
+	private void PaintBall() {
+		
+	}
+	
+	public void mouseMoved(MouseEvent e){
+		add(ball);
+	}
 }
