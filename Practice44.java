835245d98b9fce758b4f2db3a2f66b59ce7d5932
S <s>
2023-11-10 02:18:30 -0800

    {"types":"C","runs":26}

--- Practice44.java
+++ Practice44.java
@@ -5,11 +5,8 @@ public class Practice44 extends GraphicsProgram {
 
 	private static final int DIAM_BALL = 30;
 	private static final double GRAVITY = 3;
-	private static final int DELAY = 50;
-	private static final double X_START = DIAM_BALL / 2;
-	private static final double Y_START = 100;
 	private static final double X_VEL = 5;
-	private static final double BOUNCE_REDUCE = 0.9;
+	private static final double BOUNCE_REDUCE = 0.2;
 	private double xVel = X_VEL;
 	private double yVel = 0.0;
 	private GOval ball;
@@ -19,7 +16,7 @@ public class Practice44 extends GraphicsProgram {
 		while (ball.getX() < getWidth()) {
 			moveBall();
 			checkForCollision();
-			pause(DELAY);
+			pause(50);
 		}
 	}
 
@@ -28,10 +25,12 @@ public class Practice44 extends GraphicsProgram {
 		ball.setFilled(true);
 		add(ball);
 	}
+
 	private void moveBall() {
 		yVel += GRAVITY;
 		ball.move(xVel, yVel);
 	}
+
 	private void checkForCollision() {
 		if (ball.getY() > getHeight() - DIAM_BALL) {
 			yVel = -yVel * BOUNCE_REDUCE;
