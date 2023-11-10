2f477b861cbc3c461442c6d296da7fe7de1d630f
S <s>
2023-11-10 01:08:01 -0800

    {"types":"C","runs":24}

--- SpiningBall.java
+++ SpiningBall.java
@@ -3,21 +3,21 @@ import acm.program.*;
 import java.awt.*;
 
 public class SpiningBall extends GraphicsProgram {
-	
+
 	private static final double GRAVITY = 0.1;
-	
-	public void run(){
-		GOval ball = new GOval (60, 60);
+
+	public void run() {
+		GOval ball = new GOval(60, 60);
 		ball.setFilled(true);
 		add(ball);
-		
+
 		double x = 1;
 		double y = 0;
-		
-		while(true){
+
+		while (true) {
 			ball.move(x, y);
 			pause(10);
-			while(ball.getY() != getHeight() - 60){
+			if (ball.getY() != getHeight() - 60) {
 				y += GRAVITY;
 			}
 		}
