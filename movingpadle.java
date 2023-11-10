3ab321cf23b456579849f0444d57a3b50478ec00
S <s>
2023-11-10 03:05:20 -0800

    {"types":"C","runs":19}

--- movingpadle.java
+++ movingpadle.java
@@ -5,12 +5,12 @@ import java.awt.event.MouseEvent;
 
 public class movingpadle extends GraphicsProgram{	
 	public void run(){
-//		GRect r = new GRect (getWidth() / 2 - 50, getHeight() - 50, 100, 10);
-//		r.setFilled(true);
-//		add(r);
+		GRect r = new GRect (getWidth() / 2 - 50, getHeight() - 50, 100, 10);
+		r.setFilled(true);
+		add(r);
 		addMouseListeners();
 	}
 	public void mouseMoved(MouseEvent e){
-		add(new GRect(5, 5), e.getX() , getHeight() - 100);
+		add(new GRect(100, 5), e.getX() , getHeight() - 100);
 	}
 }
