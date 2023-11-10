b4a5d69ca93de195d0c7bf228db1284ffd9f291d
S <s>
2023-11-09 22:42:35 -0800

    {"types":"C","runs":8}

--- Practice44.java
+++ Practice44.java
@@ -7,29 +7,31 @@ import com.sun.glass.events.MouseEvent;
 import acm.graphics.GOval;
 import acm.program.*;
 
-public class Practice44 extends GraphicsProgram{
-	
+public class Practice44 extends GraphicsProgram {
+
 	private static final double BALL_SIZE = 50;
-	
+
 	private GOval oval;
-	
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
 		initBall();
 		dropBall();
 	}
-	
-	private void initBall(){
-		oval = new GOval (BALL_SIZE, BALL_SIZE);
+
+	private void initBall() {
+		oval = new GOval(BALL_SIZE, BALL_SIZE);
 		add(oval);
 	}
-	
-	private void dropBall(){
-		oval.move(0, 1);
-		pause(10);
+
+	private void dropBall() {
+		while (true) {
+			oval.move(0, 1);
+			pause(10);
+		}
 	}
 
 	private void mouseClicked(MouseEvent e) {
-		oval.setLocation( oval.getWidth() / 2, oval.getHeight() / 2);
+		oval.setLocation(oval.getWidth() / 2, oval.getHeight() / 2);
 	}
 }
