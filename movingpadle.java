c20ea290c54943eed1b8e9011df71dafbda6a7c9
S <s>
2023-11-10 02:51:31 -0800

    {"types":"C","runs":2}

--- movingpadle.java
+++ movingpadle.java
@@ -1,4 +1,15 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
 
-public class movingpadle {
-
+public class movingpadle extends GraphicsProgram{
+	public void run(){
+		addMouseListeners();
+	}
+	private void MouseMoved(MouseEvent e){
+		GRect r = new GRect (100, 10, getWidth() / 2 - 50, getHeight() - 50);
+		r.setFilled(true);
+		add(r);
+	}
 }
