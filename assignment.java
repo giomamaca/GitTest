007f4488f1b3ac29b39f367a830e9f418d3878c0
S <s>
2023-11-11 12:06:18 -0800

    {"types":"C","runs":23}

--- assignment.java
+++ assignment.java
@@ -52,7 +52,7 @@ public class assignment extends GraphicsProgram{
 
 
 	private void movingBall() {
-		ball = new GOval (0,getHeight() - 50, RADIUS, RADIUS);
+		ball = new GOval (0,0, RADIUS, RADIUS);
 		ball.setFilled(true);
 		add(ball);
 		while(true){			
