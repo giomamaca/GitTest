804203c53353f04e6298e173beda17bcc4748dc3
S <s>
2023-11-12 05:38:26 -0800

    {"types":"C","runs":6}

--- crashingBall.java
+++ crashingBall.java
@@ -12,10 +12,11 @@ public class crashingBall extends GraphicsProgram {
 
 	public void run() {
 		GOval ball = new GOval(0, getHeight() / 2, 10, 10);
+		ball.setFilled(true);
 		add(ball);
 		while (true) {
 			ball.move(1, 0);
-			pause(10);
+			pause(5);
 			GRect block = new GRect(700, getHeight() / 2 - HEIGHT / 2, WIDTH, HEIGHT);
 			block.setFilled(true);
 			block.setColor(Color.red);
