c6f641aa8691cfdc192d8d92349ee3c3fc0833af
S <s>
2023-11-11 21:54:49 -0800

    {"types":"C","runs":11}

--- crashingBall.java
+++ crashingBall.java
@@ -1,4 +1,19 @@
+import java.awt.event.MouseEvent;
 
-public class crashingBall {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
+public class crashingBall extends GraphicsProgram{
+	
+	private GOval ball;
+	
+	public void run(){
+		addMouseListeners();
+	}
+	public void mouseMoved(MouseEvent e){
+		while(true){
+			ball.move(1, 0);			
+			pause(10);
+		}
+	}
 }
