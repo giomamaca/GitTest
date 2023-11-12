87d85d294aa9b3b0e8b3ed00758c419431b56e01
S <s>
2023-11-11 21:58:08 -0800

    {"types":"C","runs":18}

--- crashingBall.java
+++ crashingBall.java
@@ -7,7 +7,9 @@ public class crashingBall extends GraphicsProgram{
 	public void run(){
 		GOval ball = new GOval (getWidth() / 2, getHeight() / 2, 10, 10);
 		add(ball);
-		ball.move(1, 0);			
-		pause(10);
+		while(true){
+			ball.move(1, 0);			
+			pause(10);			
+		}
 	}
 }
