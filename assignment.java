dc3f13907ec896cfeaf2cdf9862d5044de6412de
S <s>
2023-11-13 03:20:07 -0800

    {"types":"C","runs":22}

--- assignment.java
+++ assignment.java
@@ -70,7 +70,7 @@ public class assignment extends GraphicsProgram {
 		ball.setFilled(true);
 		add(ball);
 		while (true) {
-			ball.move(-vx, -vy);
+			ball.move(0, 1);
 			pause(15);
 			if (ball.getX() > APPLICATION_WIDTH - RADIUS / 2) {
 				vx *= -1;
