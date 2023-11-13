95f601020976d6ef14e415ce76007453aa471937
S <s>
2023-11-12 22:33:55 -0800

    {"types":"C","runs":20}

--- assignment.java
+++ assignment.java
@@ -47,7 +47,7 @@ public class assignment extends GraphicsProgram {
 	private void ballHitsPaddle() {
 		GObject hitPad1 = getElementAt(ball.getX(), ball.getY() + 2 * BALL_RADIUS);
 		GObject hitPad2 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
-		if (hitPad1 == paddle) {
+		if (hitPad1 == paddle || hitPad2 == paddle) {
 			if (hitPad1 != null || hitPad2 != null) {
 				vy = -vy;
 				vy *= 1.05;
