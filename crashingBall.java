f1ff9ee58b0c27781fa3c6f1d07470fb07a8611d
S <s>
2023-11-13 23:16:41 -0800

    {"types":"C","runs":1}

--- crashingBall.java
+++ crashingBall.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GObject;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
@@ -16,9 +17,15 @@ public class crashingBall extends GraphicsProgram {
 		GRect block = new GRect(700, getHeight() / 2 - HEIGHT / 2, WIDTH, HEIGHT);
 		block.setFilled(true);
 		block.setColor(Color.red);
+		GObject cord1 = getElementAt(650, getWidth()- 100);
+		GObject coed2 = getElementAt(650, 500);
 		
 		add(block);
 		while (true) {
+			GObject hitPad1 = getElementAt(ball.getX(), ball.getY() + 2 * );
+			GObject hitPad2 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY() + 2 * BALL_RADIUS);
+			GObject hitPad3 = getElementAt(ball.getX() + 2 * BALL_RADIUS, ball.getY());
+			GObject hitPad4 = getElementAt(ball.getX(), ball.getY());
 			ball.move(1, 0);
 			pause(10);
 		}
