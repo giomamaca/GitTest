3b2205aa6624d1b9d2588bd4d4b0dc285b719b67
S <s>
2023-11-14 09:08:18 -0800

    {"types":"C","runs":3}

--- assignment.java
+++ assignment.java
@@ -102,8 +102,12 @@ public class assignment extends GraphicsProgram {
 			GObject collider2 = getCollidingObject(ball.getX() + 2 * BALL_RADIUS, ball.getY());
 			if (ball.getX() > 4) {
 				if (collider1 != null || collider2 != null) {
-					remove(collider1);
-					remove(collider2);
+					if(collider1 != null){
+						remove(collider1);						
+					}
+					if(collider2 != null){
+						remove(collider2);						
+					}
 					ball.setLocation(ball.getX(), ball.getY());
 					vy *= -1;
 					if(collider1 == paddle){
