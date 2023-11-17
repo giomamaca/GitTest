b9f9118dda739229abc915c9ec21b1881841a331
S <s>
2023-11-17 00:42:42 -0800

    {"types":"C","runs":8}

--- Practice43.java
+++ Practice43.java
@@ -5,29 +5,29 @@ import acm.program.GraphicsProgram;
 
 public class Practice43 extends GraphicsProgram{
 	private static final double RADIUS = 100;
-	private GOval circle;
+	private GOval ball;
 	
 	private double x = 100;
 	private double y = 100;
 	
 	
 	public void run(){
-		circle = new GOval (2 * RADIUS, 2 * RADIUS);
-		circle.setFilled(true);
-		add(circle);
+		ball = new GOval (2 * RADIUS, 2 * RADIUS);
+		ball.setFilled(true);
+		add(ball);
 		
 		addMouseListeners();
 	}
 	
 	public void mouseDragged(MouseEvent e){
-		if(getElementAt(e.getX(), e.getY()) == circle){
-			circle.setLocation(e.getX() - x, e.getY() - y);
+		if(getElementAt(e.getX(), e.getY()) == ball){
+			ball.setLocation(e.getX() - x, e.getY() - y);
 		}
 	}
-	public void mousePressed(MouseEvent e){
-		if(getElementAt(e.getX(), e.getY()) == circle){
-			x = e.getX() - circle.getX();
-			y = e.getY() - circle.getY();
-		}
+//	public void mousePressed(MouseEvent e){
+//		if(getElementAt(e.getX(), e.getY()) == ball){
+//			x = e.getX() - ball.getX();
+//			y = e.getY() - ball.getY();
+//		}
 	}
 }
