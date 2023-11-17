746e439e0e5b91cf13ab9280a67f9860b0684c96
S <s>
2023-11-17 00:33:58 -0800

    {"types":"C","runs":1}

--- Practice43.java
+++ Practice43.java
@@ -1,4 +1,12 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
-public class Practice43 {
-
+public class Practice43 extends GraphicsProgram{
+	private static final double RADIUS = 10;
+	private GOval circle;
+	public void run(){
+		circle = new GOval (2 * RADIUS, 2 * RADIUS);
+		circle.setFilled(true);
+		add(circle);
+	}
 }
