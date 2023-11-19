bcfd4aecb1355ae93184fd0fc36cb8ecb00cd043
S <s>
2023-11-18 22:38:20 -0800

    {"types":"C","runs":18}

--- assignment.java
+++ assignment.java
@@ -57,6 +57,7 @@ public class assignment extends GraphicsProgram {
 	private GObject getCollidingObject(double a, double b) {
 		return getElementAt(a, b);
 	}
+	AudioClip bounceClip = MediaTools.loadAudioClip("bounce.au");
 
 	/* Method: run() */
 	/** Runs the Breakout program. */
@@ -144,12 +145,16 @@ public class assignment extends GraphicsProgram {
 				// If ball hits any brick it will be removed
 				if (collider1 != null && collider1 != paddle) {
 					remove(collider1);
+					bounceClip.play();
 				} else if (collider2 != null && collider2 != paddle) {
 					remove(collider2);
+					bounceClip.play();
 				} else if (collider3 != null && collider3 != paddle) {
 					remove(collider3);
+					bounceClip.play();
 				} else if (collider4 != null && collider4 != paddle) {
 					remove(collider4);
+					bounceClip.play();
 				}
 				//If ball hits bricks "vy" will be multiplayed by -1
 				vy *= -1;
