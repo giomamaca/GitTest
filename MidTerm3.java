6196f940cb99c32b1212043a72425208ae8d9395
S <s>
2023-11-20 10:09:56 -0800

    {"types":"C","runs":24}

--- MidTerm3.java
+++ MidTerm3.java
@@ -1,4 +1,21 @@
+import java.awt.event.MouseEvent;
 
-public class MidTerm3 {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+public class MidTerm3 extends GraphicsProgram{
+	
+	private GLine line;
+	
+	public void run(){
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e){
+		line = new GLine(e.getX(),e.getY(), e.getX(), e.getY());
+		add(line);
+	}
+	public void mouseDragged(MouseEvent q){
+		line.setEndPoint(q.getX(), q.getY());
+	}
 }
