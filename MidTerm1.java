86af23d703a2f189276e7b3af77596a55ee4f9e5
S <s>
2023-11-20 09:59:00 -0800

    {"types":"C","runs":11}

--- MidTerm1.java
+++ MidTerm1.java
@@ -2,12 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class MidTerm1 extends ConsoleProgram{
 	public void run(){
-		int log = 1;
+		int log = 0;
 		int n = readInt("n = ");
 		int m = readInt("m = ");
 		while (n <= m){
-			log++;
 			n *= n;
+			log++;
 		}
 		print("log = " + log);
 	}
