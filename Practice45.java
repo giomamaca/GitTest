f2d6b822636f3fa5c0602dd5a4f18b7ce7649cb9
S <s>
2023-11-20 23:35:36 -0800

    {"types":"C","runs":13}

--- Practice45.java
+++ Practice45.java
@@ -1,7 +1,23 @@
-import acm.program.GraphicsProgram;
+import acm.program.ConsoleProgram;
 
-public class Practice45 extends GraphicsProgram{
+public class Practice45 extends ConsoleProgram{
 	public void run(){
-		
+		String str = readLine();
+		String chr = readLine();
+		if(chr.length() != 1){
+			println("addada");
+			return;
+		}
+		println(countChar(str, chr.charAt(0)));
+	}
+
+	private int countChar(String str, char charAt) {
+		int charCount = 0;
+		int lastIndex = -2;
+		while(lastIndex != -1){
+			lastIndex = str.indexOf(charAt, lastIndex + 1);
+			charCount++;
+		}
+		return charCount - 1;
 	}
 }
