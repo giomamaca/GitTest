e47c5e26d132698201bc71f149a4544832a861f0
S <s>
2023-11-20 23:20:26 -0800

    {"types":"C","runs":1}

--- Practice42.java
+++ Practice42.java
@@ -1,4 +1,30 @@
+import java.awt.event.MouseEvent;
 
-public class Practice42 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class Practice42 extends GraphicsProgram{
+	
+	private GLine line;
+	private int startX = 0;
+	private int startY = 0;
+	private int lastX = 0;
+	private int lastY = 0;
+	
+	public void run(){
+		addMouseListeners();
+	}
+	
+	public void mousePressed(MouseEvent e){
+		startX = e.getX();
+		startY = e.getY();
+		lastX = e.getX();
+		lastY = e.getY();
+	}
+	
+	public void mouseMoved(MouseEvent e){
+		line = new GLine (startX, startY, lastX, lastY);
+		add(line);
+		
+	}
 }
