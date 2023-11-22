1a1102c573181ef6179331f21b26782cd752a325
S <s>
2023-11-22 04:01:28 -0800

    {"types":"C","runs":1}

--- Practice50.java
+++ Practice50.java
@@ -1,4 +1,20 @@
+import java.util.StringTokenizer;
 
-public class Practice50 {
+import acm.program.ConsoleProgram;
 
+public class Practice50 extends ConsoleProgram{
+	public void run(){
+		String s = readLine("enter string : ");
+		println("Tokens are : ");
+		printTokens(s);
+	}
+
+	private void printTokens(String s) {
+		StringTokenizer tokenizer = new StringTokenizer(s, "iao");
+		int count = 1;
+		while(tokenizer.hasMoreTokens()){
+			println("Token #" + count + ":" + tokenizer.nextToken());
+			count++;
+		}
+	}
 }
