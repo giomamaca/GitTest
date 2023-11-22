1d0e646265ec587509b3ec2d9955328a2c433460
S <s>
2023-11-22 00:28:09 -0800

    {"types":"C","runs":17}

--- exercise.java
+++ exercise.java
@@ -7,7 +7,7 @@ public class exercise extends ConsoleProgram {
 		String str = "dessert";
 		String answer = "";
 		for(int i = 0; i < str.length(); i++){
-			answer = answer + str.charAt(i);
+			answer = str.charAt(i) + answer;
 		}
 		println(answer);
 	}
