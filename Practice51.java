8e7239bbfe5479a5b789e39ee45c832da335ca9a
S <s>
2023-11-22 04:19:25 -0800

    {"types":"C","runs":19}

--- Practice51.java
+++ Practice51.java
@@ -4,10 +4,12 @@ public class Practice51 extends ConsoleProgram{
 	public void run(){
 		String s = readLine("enter string : ");
 		for(int i = 0; i < s.length(); i++){
-			char a = s.charAt(i);
-			char b = (char) (a - 1);
-			if(a == b){
-				print(s);
+			for(int j = 1; j < s.length(); j++){
+				char a = s.charAt(i);
+				char b = s.charAt(j);
+				if(a != b){
+					print(s);
+				}				
 			}
 		}
 	}
