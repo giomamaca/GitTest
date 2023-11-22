60e2b732940a4a0e7a7e45711601e30b72331c15
S <s>
2023-11-22 02:12:19 -0800

    {"types":"C","runs":31}

--- Practice48.java
+++ Practice48.java
@@ -6,15 +6,18 @@ public class Practice48 extends ConsoleProgram{
 		char c = 'a';
 		for(char chr = 'a'; chr < 'z'; chr++){
 			int num = countCharacter(str, c);
-			print(num);
+			println((char)('a' + chr) + ":" + num);
 		}
 	}
-	private int countCharacter(String str, char c){
+
+	private int countCharacter(String s, char c) {
 		int count = 0;
-		int index = 0;
-		while(index != -1){
-			count = str.indexOf(c, index +1);
-			count++;
+		c = 'a';
+		for(c = 'a'; c < s.length(); c++){
+			int result = s.charAt(c);
+			if(result == c){
+				count++;
+			}
 		}
 		return count - 1;
 	}
