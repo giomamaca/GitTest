4fec59f93e62c8daff2190bc65d7c40336f9b7e7
S <s>
2023-11-22 02:42:13 -0800

    {"types":"C","runs":54}

--- Practice48.java
+++ Practice48.java
@@ -2,20 +2,21 @@ import acm.program.ConsoleProgram;
 
 public class Practice48 extends ConsoleProgram{
 	public void run(){
-		String str = readLine();
-		for(int ch = 0; ch  < 26; ch++){
-			char c = (char)('a' + ch);
-			int num = countCharacter(str, c);
-			if(num > 0){
-				println(c + ":" + (double) num / str.length() * 100 + '%');			
+		String s = readLine("enter string : ");
+		for(int c = 0; c < 26; c++){
+			char chr = (char)('a' + c);
+			int percent = countCharacter(s, chr);
+			if(percent > 0){
+				println(chr + ":" + (double)s.length() / percent * 100 + "%");
 			}
 		}
 	}
-	private int countCharacter(String s, char c) {
+
+	private int countCharacter(String s, char chr) {
 		int count = 0;
-		for(int i = 0; i < s.length(); i++){
-			int result = s.charAt(i);
-			if(result == c){
+		for(int i = 0; i < 26; i++){
+			int num = s.charAt(i);
+			if(num == chr){
 				count++;
 			}
 		}
