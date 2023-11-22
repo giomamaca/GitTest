0559d3b6253a822a2995f08644d52b969aef9212
S <s>
2023-11-22 01:31:28 -0800

    {"types":"C","runs":27}

--- exercise.java
+++ exercise.java
@@ -4,11 +4,7 @@ import java.awt.*;
 
 public class exercise extends ConsoleProgram {	
 	public void run() {
-		String str = "dessert";
-		String answer = "";
-		for(int i = 0; i < str.length(); i++){
-			answer = str.charAt(i) + answer;
-		}
-		println(answer);
+		char  c = 'a';
+		println(c + 1);
 	}
 }
