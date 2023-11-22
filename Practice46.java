9f812a295de89dd53aae02d4c3faeaf94027058c
S <s>
2023-11-22 00:15:59 -0800

    {"types":"C","runs":5}

--- Practice46.java
+++ Practice46.java
@@ -3,25 +3,24 @@ import acm.program.ConsoleProgram;
 public class Practice46 extends ConsoleProgram{
 	public void run(){
 		String str = readLine();
-		char answer = 'a';
-		int Max = 0;
-		for(char chr = 'a'; chr <= 'z' ; chr++){
-			int count = countCharacter(str, chr);
-			if(count > Max){
-				Max = count;
-				answer = chr;
+		char chr = 'a';
+		int max = 0;
+		for(char c = 'a'; c <= 'z'; c++){
+			int count = countCharacter(str, c);
+			if(count > max){
+				chr = c;
+				max++;
 			}
 		}
-		println(answer);
+		println(chr);
 	}
 
-	private int countCharacter(String str, char chr) {
+	private int countCharacter(String str, char c) {
 		int count = 0;
 		int index = 0;
 		while(index != -1){
-			index = str.indexOf(chr, index + 1);
-			count++;
+			index =str.indexOf(c, index + 1);
 		}
-		return count + 1;
+		return count - 1;
 	}
 }
