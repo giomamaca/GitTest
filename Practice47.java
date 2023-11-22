390a0a71305bbbd6bfc792400ec6380e4f583ccd
S <s>
2023-11-22 03:33:52 -0800

    {"types":"C","runs":62}

--- Practice47.java
+++ Practice47.java
@@ -2,6 +2,22 @@ import acm.program.ConsoleProgram;
 
 public class Practice47 extends ConsoleProgram{
 	public void run(){
-		
+		String s = readLine("enter string : ");
+		if(predicat(s)){
+			println("predicat");
+		}else{
+			println("no predicat");
+		}
+	}
+
+	private boolean predicat(String s) {
+		for(int i = 0; i < s.length(); i++){
+			char a = s.charAt(i);
+			char b = s.charAt(s.length() - i -1);
+			if(a != b){
+				return false; 
+			}
+		}
+		return true;
 	}
 }
