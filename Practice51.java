39a56f27fad3851c38ba4da24e45e8f341975a04
S <s>
2023-11-22 04:16:00 -0800

    {"types":"C","runs":10}

--- Practice51.java
+++ Practice51.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
 
-public class Practice51 {
-
+public class Practice51 extends ConsoleProgram{
+	public void run(){
+		String s = readLine("enter string : ");
+		for(int i = 0; i < s.length(); i++){
+			char a = s.charAt(i);
+			char b = s.charAt(i -1);
+			if(a != b){
+				print(s);
+			}else{
+				return;
+			}
+		}
+	}
 }
