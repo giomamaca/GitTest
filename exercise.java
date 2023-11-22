3cc4da0d9d4b99c16646a28fc55af8efd8510f75
S <s>
2023-11-22 00:24:47 -0800

    {"types":"C","runs":9}

--- exercise.java
+++ exercise.java
@@ -2,41 +2,12 @@ import acm.graphics.*;
 import acm.program.*;
 import java.awt.*;
 
-public class exercise extends GraphicsProgram {
-
-	private static final double BLOCK_WIDTH = 60;
-	private static final double BLOCK_HEIGHT = 20;
-	
-	private GOval ball;
-	private GRect block;
-	
-	private int x = 1;
-	private int y = 1;
-
-	private GObject getCollidingObject(double a, double b){
-		return getElementAt(a, b);
-	}
-	
+public class exercise extends ConsoleProgram {	
 	public void run() {
-		for(int i = 0 ; i < 9; i++){
-			block = new GRect(getWidth() - 100, getHeight() / 2 - 100, 100,200);
-			
+		String str = "dessert";
+		for(int i = 0; i <= str.length(); i++){
+			str = str + str.charAt(i);
 		}
-		block.setFilled(true);
-		add(block);
-		
-		ball = new GOval(0, getHeight() / 2 - 10, 20, 20);
-		add(ball);
-		while(true){
-			ball.move(x, 0);
-			pause(5);
-			GObject collider = getCollidingObject(ball.getX(), ball.getY());
-			if(ball.getX() > getWidth() - 100 - 20){
-				if(collider != null){
-					remove(collider);
-					x *= -1;
-				}
-			}
-		}
+		println(str);
 	}
 }
