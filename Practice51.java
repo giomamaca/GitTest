58754b282dcdfd9d22d464df65cb7919c871e11e
S <s>
2023-11-22 04:17:51 -0800

    {"types":"C","runs":17}

--- Practice51.java
+++ Practice51.java
@@ -6,7 +6,7 @@ public class Practice51 extends ConsoleProgram{
 		for(int i = 0; i < s.length(); i++){
 			char a = s.charAt(i);
 			char b = (char) (a - 1);
-			if(a != b){
+			if(a == b){
 				print(s);
 			}
 		}
