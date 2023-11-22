21cee53585cdce1e4cc5bf33df803baaad6efcf6
S <s>
2023-11-22 00:55:11 -0800

    {"types":"C","runs":25}

--- Practice48.java
+++ Practice48.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class Practice48 {
-
+public class Practice48 extends ConsoleProgram{
+	public void run(){
+		String str = readLine();
+		char c = 'a';
+		for(char chr = 'a'; chr < str.length(); chr++){
+			int num = countCharacter(str, c);
+			println(num);
+			
+		}
+	}
+	private int countCharacter(String str, char c){
+		int count = 0;
+		int index = 0;
+		while(index != -1){
+			count = str.indexOf(c, index +1);
+			count++;
+		}
+		return count - 1;
+	}
 }
