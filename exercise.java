560dc551b550c41a738892f5ea32e2c16b5e9aba
S <s>
2023-11-22 02:24:27 -0800

    {"types":"C","runs":45}

--- exercise.java
+++ exercise.java
@@ -1,10 +1,28 @@
-import acm.graphics.*;
-import acm.program.*;
-import java.awt.*;
+import acm.program.ConsoleProgram;
 
-public class exercise extends ConsoleProgram {	
+public class exercise extends ConsoleProgram {
 	public void run() {
-		char  c = 'a';
-		println((char) ('a' + 1));
+		String s = readLine("Enter string: ");
+		printFrequency(s);
+	}
+
+	private void printFrequency(String s) {
+		for (int i = 0; i < 26; i++) {
+			char next = (char) ('a' + i);
+			int countSymbol = countSymbolInString(s, next);
+			if (countSymbol > 0)
+				println(next + ":" + (double) countSymbol / s.length() * 100 + '%');
+		}
+	}
+
+	private int countSymbolInString(String s, char ch) {
+		int count = 0;
+		for (int i = 0; i < s.length(); i++) {
+			int current = s.charAt(i);
+			if (current == ch) {
+				count++;
+			}
+		}
+		return count;
 	}
 }
