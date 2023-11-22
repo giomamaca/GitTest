ad43ef6124074b26d19da1a69e6bfad371977432
S <s>
2023-11-22 04:17:27 -0800

    {"types":"C","runs":15}

--- Practice51.java
+++ Practice51.java
@@ -5,7 +5,7 @@ public class Practice51 extends ConsoleProgram{
 		String s = readLine("enter string : ");
 		for(int i = 0; i < s.length(); i++){
 			char a = s.charAt(i);
-			char b = s.charAt(i -1);
+			char b = (char) (a - 1);
 			if(a != b){
 				print(s);
 			}
