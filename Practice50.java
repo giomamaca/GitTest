c700a1e3880467db46614b602861556d25dd5945
S <s>
2023-11-22 04:09:37 -0800

    {"types":"C","runs":7}

--- Practice50.java
+++ Practice50.java
@@ -10,7 +10,7 @@ public class Practice50 extends ConsoleProgram{
 	}
 
 	private void printTokens(String s) {
-		StringTokenizer tokenizer = new StringTokenizer(s, "go");
+		StringTokenizer tokenizer = new StringTokenizer(s, " ");
 		int count = 1;
 		while(tokenizer.hasMoreTokens()){
 			println("Token #" + count + ":" + tokenizer.nextToken());
