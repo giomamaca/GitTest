891b113e2f7195edb0d69b7540d8035b49511b4e
S <s>
2023-11-23 00:43:24 -0800

    {"types":"C","runs":20}

--- Midterm21.java
+++ Midterm21.java
@@ -1,4 +1,33 @@
+import java.awt.event.MouseEvent;
 
-public class Midterm21 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class Midterm21 extends GraphicsProgram{
+	
+	private static final double BOX_SIZE = 200; 
+	private GRect rect;
+	
+	private double recX = getWidth() / 2 - BOX_SIZE / 2;
+	private double recY =  getHeight() / 2 - BOX_SIZE / 2;
+	
+	public void run(){
+		int number = 0;
+		
+		
+		rect = new GRect (recX, recY, BOX_SIZE, BOX_SIZE); 
+		add(rect);
+		
+		GLabel num = new GLabel("k");
+		double X = num.getWidth();
+		double Y = num.getAscent();
+		add(num, recX + BOX_SIZE / 2 - X / 2, recY + BOX_SIZE / 2 + Y / 2);
+		
+		addMouseListeners();
+	}
+	
+	public void mouseDragged(MouseEvent e){	
+		rect.setLocation(e.getX(), e.getY());
+
+	}
 }
