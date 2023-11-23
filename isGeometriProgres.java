8eabe8074fa3f667eaa72e91c544f8a9a2ec5275
S <s>
2023-11-23 02:19:58 -0800

    {"types":"C","runs":65}

--- isGeometriProgres.java
+++ isGeometriProgres.java
@@ -1,4 +1,9 @@
+import acm.program.ConsoleProgram;
 
-public class isGeometriProgres {
-
+public class isGeometriProgres extends ConsoleProgram{
+	public void run(){
+		for(int i = 0; i != -1; i++){
+			int k = readInt();
+		}
+	}
 }
