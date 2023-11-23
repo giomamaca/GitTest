9433e4fdc2663f8281a0e5abac4e35738532f86e
S <s>
2023-11-23 01:38:48 -0800

    {"types":"C","runs":49}

--- MouseListener.java
+++ MouseListener.java
@@ -1,12 +1,28 @@
 import acm.program.GraphicsProgram;
 
-import com.sun.glass.events.MouseEvent;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.MouseEvent;
 
 import acm.graphics.GOval;
+import acm.graphics.*;
 import acm.program.*;
 
 public class MouseListener extends GraphicsProgram{
+	
+	private static final double SIZE = 100;
+	
+	private GRect rect;
+	
 	public void run(){
+		rect = new GRect(getWidth() / 2 - SIZE / 2,getHeight()/ 2 - SIZE / 2 ,SIZE, SIZE);
+		rect.setFilled(true);
+		add(rect);
 		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e){
+		if(getElementAt(e.getX(), e.getY()) == rect){
+			rect.setColor(Color.red);
+		}
 	}
 }
