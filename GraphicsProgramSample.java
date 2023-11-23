288448a520a6b2918378e5ed91986bfba29063d8
S <s>
2023-11-23 10:39:05 -0800

    {"types":"C","runs":4}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -1,3 +1,4 @@
+
 /*
  * File: GraphicsHierarchy.java
  * ----------------------------
@@ -12,18 +13,18 @@ import acm.graphics.*;
 import acm.program.*;
 
 public class GraphicsProgramSample extends GraphicsProgram {
-	
+
 	private static final double SIZE = 50;
-	
+
 	private GRect rect;
 	private int count = 0;
-	
+
 	public void run() {
-		for(int i = 0; i < 8; i++){
-			for(int j = 0; j < 8; j++){
-				double rectX = (double)(50 + i * SIZE);
-				double rectY = (double)(50 + j * SIZE);
-				rect = new GRect (rectX, rectY, SIZE, SIZE);
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				double rectX = (double) (50 + i * SIZE);
+				double rectY = (double) (50 + j * SIZE);
+				rect = new GRect(rectX, rectY, SIZE, SIZE);
 				rect.setFilled(true);
 				rect.setFillColor(Color.WHITE);
 				add(rect);
@@ -33,23 +34,16 @@ public class GraphicsProgramSample extends GraphicsProgram {
 		System.out.println(count);
 	}
 
-
-
-	public void mouseClicked(MouseEvent e){
+	public void mouseClicked(MouseEvent e) {
 		GObject click1 = getElementAt(e.getX(), e.getY());
 		GObject click2 = getElementAt(e.getX(), e.getY());
-		if(click1 != null || click2 != null){
-			if(count <= 2){
-				((GRect) click1).setFillColor(Color.BLACK);		
-				count++;
-				((GRect) click2).setFillColor(Color.BLACK);
-				count++;
-			}
-			if(count == 2){
+		if (click1 != null || click2 != null) {
+			((GRect) click1).setFillColor(Color.BLACK);
+			count++;
+			((GRect) click2).setFillColor(Color.BLACK);
+			count++;
+			if(count > 2 && count % 2 == 1){
 				((GRect) click1).setFillColor(Color.white);
-				count--;
-				((GRect) click2).setFillColor(Color.BLACK);
-				count++;
 			}
 		}
 	}
