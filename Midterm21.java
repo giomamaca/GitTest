b039cca88a2f9a3eb274aaa313b3f5efadb7a91e
S <s>
2023-11-23 00:59:02 -0800

    {"types":"C","runs":37}

--- Midterm21.java
+++ Midterm21.java
@@ -16,12 +16,12 @@ public class Midterm21 extends GraphicsProgram{
 		double recY =  getHeight() / 2 - BOX_SIZE / 2;
 		
 		rect = new GRect (recX, recY, BOX_SIZE, BOX_SIZE); 
-		num = new GLabel("k");
+		add(rect);
+		
+		num = new GLabel("0");
 		double X = num.getWidth();
 		double Y = num.getAscent();
 		add(num, recX + BOX_SIZE / 2 - X / 2, recY + BOX_SIZE / 2 + Y / 2);
-		add(rect);
-		
 		
 		addMouseListeners();
 	}
