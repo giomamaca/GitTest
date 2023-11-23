4a86576416849259ec552724b02e24f51c98d32e
S <s>
2023-11-23 03:12:16 -0800

    {"types":"C","runs":89}

--- GraphicsProgramSample.java
+++ GraphicsProgramSample.java
@@ -37,6 +37,7 @@ public class GraphicsProgramSample extends GraphicsProgram {
 		GObject click = getElementAt(e.getX(), e.getY());
 		if(click != null){
 			((GRect) click).setColor(Color.BLACK);
+			System.out.println("ffsaas");
 		}
 	}
 }
