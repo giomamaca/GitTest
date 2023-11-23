79df0d0066b36c6da6f087984af4f31e4deeaaa2
S <s>
2023-11-23 00:13:56 -0800

    {"types":"C","runs":1}

--- MidTerm22.java
+++ MidTerm22.java
@@ -1,4 +1,29 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm22 {
-
+public class MidTerm22 extends ConsoleProgram{
+	
+	private int divisor = 0;
+	
+	public void run(){
+		int k = readInt();
+		getBiggestDivisor(k);
+	}
+	private void getBiggestDivisor(int a){
+		for(int i = 0; i <= a; i++){
+			if(simple(i) && a % i == 0){
+				divisor = a;
+			}
+		}
+	}
+	private boolean simple(int a) {
+		if(a == 1) return false;
+		if(a == 2) return true;
+		if(a % 2 == 0) return false;
+		for(int i = 3; i * i <= a; i++){
+			if(a % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
 }
