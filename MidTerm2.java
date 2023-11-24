2e856b5a5a0b56d0a96ffc5dc97729b557672287
S <s>
2023-11-24 00:11:17 -0800

    {"types":"C","runs":8}

--- MidTerm2.java
+++ MidTerm2.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm2 {
+public class MidTerm2 extends ConsoleProgram{
+	public void run(){
+		String father = readLine();
+		String son = readLine();
+		println(isFath(father, son));
+	}
 
+	private boolean isFath(String father, String son) {
+		for(int i = 0; i < son.length(); i++){
+			char a = son.charAt(i);
+			for(int j = 0; i< father.length(); j++){
+				
+			}
+		}
+		return false;
+	}	
 }
