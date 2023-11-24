ec803551687856a4395d3a666c23b232a3c3f3c3
S <s>
2023-11-24 12:25:46 -0800

    {"types":"C","runs":5}

--- Practice30.java
+++ Practice30.java
@@ -2,8 +2,8 @@ import acm.program.ConsoleProgram;
 
 public class Practice30 extends ConsoleProgram{
 	public void run(){
-		int k = 0;
 		int a = readInt("?");
+		int k = 0;
 		while(a > 0){
 			k = k * 10 + k % 10;
 			a = a / 10;
