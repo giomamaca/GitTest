6dc5f6ec1f61f1bd82dca5a18c0e5921820cbbda
S <s>
2023-11-23 23:42:16 -0800

    {"types":"C","runs":5}

--- World.java
+++ World.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class World {
-
+public class World extends ConsoleProgram{
+	public void run(){
+		String erti = readLine("1 : ");
+		String ori = readLine("2 : ");
+		println(possible(erti, ori));
+	}
+	
+	private boolean possible(String erti, String ori){
+		boolean ans = true;
+		for(int i = 0; i < ori.length(); i++){
+			char aso = ori.charAt(i);
+			int index = erti.indexOf(aso);
+			if(index == -1) return false;
+			erti = erti.substring(index + 1, erti.length() - 1);
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
 }
