9707597e457c68745268abf6b802e09b51fdd207
S <s>
2023-11-23 22:31:12 -0800

    {"types":"C","runs":5}

--- Practice52.java
+++ Practice52.java
@@ -1,4 +1,24 @@
+import acm.program.ConsoleProgram;
 
-public class Practice52 {
-
+public class Practice52 extends ConsoleProgram{
+	public void run(){
+		int count = 0;
+		int n = readInt("enter num of numbers : ");
+		int large = 0;
+		int small = 0;
+		while(true){
+			int num = readInt("num = ");
+			count++;
+			if(count == n) break;
+			if(num > large){
+				large = num;
+			}
+			num = small;
+			if(small < large){
+				large = small;
+			}
+		}
+		print(large);
+		print(small);
+	}
 }
