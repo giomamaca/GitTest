ee29e02bdcd233b7e100e5adc1137db358887b0b
S <s>
2023-11-23 22:16:44 -0800

    {"types":"C","runs":4}

--- Practice51.java
+++ Practice51.java
@@ -11,7 +11,7 @@ public class Practice51 extends ConsoleProgram {
 	private String duble(String s) {
 		String answer = "";
 		for(int i = 0; i < s.length(); i++){
-			if(i =0 || s.charAt(i) != s.charAt(i - 1)){
+			if(i == 0 || s.charAt(i) != s.charAt(i - 1)){
 				answer += s.charAt(i);
 			}
 		}
