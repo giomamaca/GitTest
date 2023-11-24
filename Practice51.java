6160673cf0da51c4d1b088a9c2e2fe9783531698
S <s>
2023-11-23 22:31:12 -0800

    {"types":"C","runs":5}

--- Practice51.java
+++ Practice51.java
@@ -4,14 +4,22 @@ import acm.program.ConsoleProgram;
 
 public class Practice51 extends ConsoleProgram {
 	public void run() {
+		int x = readInt();
+		int y = readInt();
+		if (x > y && x / (x - y) > 10) {
+			println("good");
+		} else {
+			println("bad");
+		}
+
 		String s = readLine("enter string : ");
 		println(duble(s));
 	}
 
 	private String duble(String s) {
 		String answer = "";
-		for(int i = 0; i < s.length(); i++){
-			if(i == 0 || s.charAt(i) != s.charAt(i - 1)){
+		for (int i = 0; i < s.length(); i++) {
+			if (i == 0 || s.charAt(i) != s.charAt(i - 1)) {
 				answer += s.charAt(i);
 			}
 		}
