9b322145aa846729288e03f96e2f644ac381ce15
S <s>
2023-11-26 10:26:24 -0800

    {"types":"C","runs":2}

--- MidTerm1614.java
+++ MidTerm1614.java
@@ -8,14 +8,16 @@ public class MidTerm1614 extends ConsoleProgram{
 	}
 	private String longestRunCompression(String text){
 		int count = 0;
+		String S = "";
 		for(int i = 1; i < text.length(); i++){
 			char ch = text.charAt(i);
 			char chr = text.charAt(i - 1);
 			if(ch == chr){
 				count++;
-				println(count + ch);
+				S = "" +count + ch;
 			}
 			count = 0;
 		}
+		return S;
 	}
 }
