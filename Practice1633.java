78a3a306f909fdb622567132b8655824c61c0775
S <s>
2023-11-26 01:07:02 -0800

    {"types":"C","runs":21}

--- Practice1633.java
+++ Practice1633.java
@@ -1,4 +1,36 @@
+import acm.program.ConsoleProgram;
 
-public class Practice1633 {
+public class Practice1633 extends ConsoleProgram{
+	public void run(){
+		String s = readLine("Fibonacci");
+		int a = readInt();
+		int b = readInt();
+		if(a != 0 && b != 1){
+			return;
+		}
+		if(a != 1 && b != 0){
+			return;
+		}
+		if(a == 1 && b == 0){
+			int temp = a;
+			a = b;
+			b = temp;
+		}
+		while(true){
+			int num = readInt();
+			if(fibonaci(num, b)){
+				println(s);
+			}
+		}
+	}
 
+	private boolean fibonaci(int num, int b) {
+		if(num == 1) return true;
+		int sum = 0;
+		sum = num + b;
+		b = num;
+		if(num == sum) return true;
+		
+		return false;
+	}
 }
