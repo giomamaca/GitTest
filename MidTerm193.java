045485b4a4456ef8bfe62d77edd6604cce4fc87b
S <s>
2023-11-26 00:13:11 -0800

    {"types":"C","runs":1}

--- MidTerm193.java
+++ MidTerm193.java
@@ -1,4 +1,21 @@
+import java.awt.event.MouseEvent;
 
-public class MidTerm193 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class MidTerm193 extends GraphicsProgram{
+	
+	private static final double RADIUS = 50;
+	
+	private GOval oval;
+	
+	public void run(){
+		addMouseListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e){
+		oval = new GOval(e.getX(), e.getY(), RADIUS, RADIUS);
+		add(oval);
+	}
+	
 }
