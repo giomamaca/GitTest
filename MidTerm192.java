3c3ab78b43269b5f0dbafa1efaf614127843d758
S <s>
2023-11-25 22:47:13 -0800

    {"types":"C","runs":1}

--- MidTerm192.java
+++ MidTerm192.java
@@ -1,4 +1,15 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm192 {
-
+public class MidTerm192 extends ConsoleProgram{
+	public void run(){
+		int count = 0;
+		int k = readInt();
+		while(k != 0){
+			k = k / 10;
+			count++;
+			for(int i = 0; i < count; i++){
+				println(i + k % 10);
+			}
+		}
+	}
 }
