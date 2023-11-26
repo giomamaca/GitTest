479a504a7dded86bfbb90ce483e54decd184609d
S <s>
2023-11-26 10:24:17 -0800

    {"types":"C","runs":1}

--- MidTerm1614.java
+++ MidTerm1614.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm1614 {
-
+public class MidTerm1614 extends ConsoleProgram{
+	public void run(){
+		String text = readLine();
+		longestRunCompression(text);
+		
+	}
+	private String longestRunCompression(String text){
+		int count = 0;
+		for(int i = 1; i < text.length(); i++){
+			char ch = text.charAt(i);
+			char chr = text.charAt(i - 1);
+			if(ch == chr){
+				count++;
+				println(count + ch);
+			}
+			count = 0;
+		}
+	}
 }
