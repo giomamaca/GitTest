e3a9b50a75a3d74303d008bea5dcf4807e6b1463
S <s>
2023-11-26 00:15:50 -0800

    {"types":"C","runs":4}

--- MidTerm193.java
+++ MidTerm193.java
@@ -2,6 +2,8 @@ import java.awt.event.MouseEvent;
 
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
+import javafx.scene.paint.Color;
 
 public class MidTerm193 extends GraphicsProgram{
 	
@@ -9,13 +11,16 @@ public class MidTerm193 extends GraphicsProgram{
 	
 	private GOval oval;
 	
+	RandomGenerator rgen = new RandomGenerator();
+	
 	public void run(){
 		addMouseListeners();
 	}
 	
 	public void mouseClicked(MouseEvent e){
 		oval = new GOval(e.getX() - RADIUS, e.getY() - RADIUS, 2*RADIUS, 2*RADIUS);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
 		add(oval);
 	}
-	
 }
