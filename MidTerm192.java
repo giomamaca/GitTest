6580c816f2941defa815fd08d343acd4111cf1fc
S <s>
2023-11-25 23:32:25 -0800

    {"types":"C","runs":63}

--- MidTerm192.java
+++ MidTerm192.java
@@ -21,11 +21,11 @@ public class MidTerm192 extends ConsoleProgram{
 			int div2 = n / a;			
 			while(div2 != 0){
 				swap = swap * 10 + n % 10;
-				div2 = div2 / 10;
+				div1 = div1 / 10;
 			}		
 			println(div1);
 			println(swap);
-			if(div1 == swap){
+			if(div2 == swap){
 				println("correct");
 			}else{
 				println("wrong");
@@ -40,7 +40,7 @@ public class MidTerm192 extends ConsoleProgram{
 			}
 			println(div1);
 			println(swap);
-			if(div1 == swap){
+			if(div2 == swap){
 				println("correct");
 			}else{
 				println("wrong");
