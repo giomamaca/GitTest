2bf1420434c4e84bbd2ef588168945ae9bb7d458
S <s>
2023-11-25 23:03:34 -0800

    {"types":"C","runs":8}

--- MidTerm192.java
+++ MidTerm192.java
@@ -3,23 +3,23 @@ import acm.program.ConsoleProgram;
 public class MidTerm192 extends ConsoleProgram{
 	public void run(){
 		int count = 0;
+		int swap = 0;
 		int k = readInt();
 		int n = k;
+		int a = k;
 		while(k != 0){
 			k = k / 10;
 			count++;
 		}
-		int divider1 = 0;
-		int divider2 = 0;
-		for(int i = 1; i < count; i++){
-			int div1 = n % (10 * i);
-			divider1 = div1;
-			for(int j = count; j > 0; j++){
-				int div2 = n / 10 * j; 
-				divider2 = div2;
-			}
+		int div1 = n % (10 * count / 2);
+		int div2 = a / (10 * count / 2);
+		while(div2 != 0){
+			swap = swap + (n % 10) *10;
 		}
-		if(divider1 == divider2){
+		swap = swap / 10;
+				
+				
+		if(div1 == swap){
 			println("correct");
 		}else{
 			println("wrong");
