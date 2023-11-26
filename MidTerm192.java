32753f02eebe869b9b6b36987d00c0dd50c4cf56
S <s>
2023-11-25 23:18:27 -0800

    {"types":"C","runs":33}

--- MidTerm192.java
+++ MidTerm192.java
@@ -5,15 +5,14 @@ public class MidTerm192 extends ConsoleProgram{
 		int count = 0;
 		int k = readInt();
 		int n = k;
-		int a = k;
 		while(k != 0){
 			k = k / 10;
 			count++;
 		}
 		int swap = 0;
 		if(count % 2 == 0){
-			int div1 = n % (10 * (count / 2 + 1 ));
-			int div2 = a / (10 ^ (count / 2));			
+			int div1 = n % (10 * (count / 2));
+			int div2 = n / (10 ^ (count / 2));			
 //			while(div2 != 0){
 //				swap = swap * 10 + n % 10;
 //				div2 = div2 / 10;
@@ -28,7 +27,7 @@ public class MidTerm192 extends ConsoleProgram{
 		}
 		if(count % 2 == 1){
 			int div1 = n % (10 ^ (count / 2));
-			int div2 = a / (10 ^ ((count / 2) + 1));			
+			int div2 = n / (10 ^ ((count / 2) + 1));			
 //			while(div2 != 0){
 //				swap = swap * 10 + div2 % 10;
 //				div2 = div2 / 10;
