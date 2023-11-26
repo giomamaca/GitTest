f57033eced7e4c326cacf596546e896c6fa50ffa
S <s>
2023-11-26 01:34:59 -0800

    {"types":"C","runs":40}

--- MidTerm1633.java
+++ MidTerm1633.java
@@ -1,33 +1,34 @@
 import acm.program.ConsoleProgram;
 
-public class MidTerm1633 extends ConsoleProgram{
-	public void run(){
-//		String s = readLine("Fibonacci");
+public class MidTerm1633 extends ConsoleProgram {
+	public void run() {
+		// String s = readLine("Fibonacci");
 		int a = readInt();
 		int b = readInt();
-//		if(a != 0 && b != 1){
-//			return;
-//		}
-		if(a == 1 && b == 0){
+		// if(a != 0 && b != 1){
+		// return;
+		// }
+		if (a == 1 && b == 0) {
 			int temp = a;
 			a = b;
 			b = temp;
 		}
-		while(true){
+		while (true) {
 			int num = readInt();
-			while(num != -1){				
-				
+			while (num != -1) {
+
 			}
 		}
 	}
+}
 
-//	private boolean fibonaci(int num, int b) {
-//		if(num >= 1) return true;
-//		int sum = 0;
-//		sum = num + b;
-//		b = num;
-//		if(num == sum) return true;
-//		
-//		return false;
-//	}
-//}
+// private boolean fibonaci(int num, int b) {
+// if(num >= 1) return true;
+// int sum = 0;
+// sum = num + b;
+// b = num;
+// if(num == sum) return true;
+//
+// return false;
+// }
+// }
