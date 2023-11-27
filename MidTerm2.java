bb95013314c86138ce8c764a178c63e3c7360c9e
S <s>
2023-11-27 01:50:50 -0800

    {"types":"C","runs":0}

--- MidTerm2.java
+++ MidTerm2.java
@@ -1,24 +1,26 @@
 import acm.program.ConsoleProgram;
 
-public class MidTerm2 extends ConsoleProgram{
-	public void run(){
+public class MidTerm2 extends ConsoleProgram {
+	public void run() {
 		String father = readLine();
 		String son = readLine();
 		println(isFath(father, son));
 	}
 
 	private boolean isFath(String father, String son) {
-		for(int i = 0; i < son.length(); i++){
-			char s1 = son.charAt(i);
-			char s2 = son.charAt(i + 1);
-			for(int j = 0; i< father.length(); j++){
-				char f1 = father.charAt(j);
-				char f2 =father.charAt(j + 1);
-				if(s1 == f1){
-					return true;
+		for (int i = 0; i < son.length(); i++) {
+			for (int j = 0; j < son.length(); j++) {
+				String s = son.substring(i, j);
+				for (int a = 0; a < father.length(); a++) {
+					for (int b = 0; b < father.length(); b++) {
+						String f = father.substring(a, b);
+						if (s == f) {
+							println("correct");
+						}
+					}
 				}
+
 			}
 		}
-		return false;
-	}	
+	}
 }
