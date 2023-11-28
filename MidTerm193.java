e156bf3253bffa91b0101897b4b302612cb98d47
S <s>
2023-11-28 10:08:16 -0800

    {"types":"C","runs":46}

--- MidTerm193.java
+++ MidTerm193.java
@@ -6,21 +6,59 @@ import acm.util.RandomGenerator;
 import javafx.scene.paint.Color;
 
 public class MidTerm193 extends GraphicsProgram{
-	
-	private static final double RADIUS = 25;
-	
-	private GOval oval;
-	
-	RandomGenerator rgen = new RandomGenerator();
-	
-	public void run(){
+	private final int CIRCLE_D = 100;
+	private final int DELAY = 100;
+	private RandomGenerator rgen;
+	private GObject selectedObject = null;
+
+	@Override
+	public void init() {
 		addMouseListeners();
+		rgen = RandomGenerator.getInstance();
 	}
-	
-	public void mouseClicked(MouseEvent e){
-		oval = new GOval(e.getX() - RADIUS, e.getY() - RADIUS, 2*RADIUS, 2*RADIUS);
-		oval.setFilled(true);
-		oval.setColor(rgen.nextColor());
-		add(oval);
+
+	public void run() {
+		while (true) {
+			if (selectedObject != null) {
+				while (selectedObject.getColor() != Color.GREEN) {
+					Color color = myRandomColor();
+					selectedObject.setColor(color);
+					println(selectedObject);
+					pause(DELAY);
+				}
+				selectedObject = null;
+			}
+			println("E");
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+
+		GObject object = getElementAt(x, y);
+		println(object);
+		if (object == null) {
+			GOval oval = new GOval(CIRCLE_D, CIRCLE_D);
+			add(oval, x - CIRCLE_D / 2, y - CIRCLE_D / 2);
+			oval.setFilled(true);
+			oval.setColor(myRandomColor());
+		} else {
+			selectedObject = object;
+		}
+	}
+
+	private Color myRandomColor() {
+		int randomNum = rgen.nextInt(5);
+		if (randomNum == 0)
+			return Color.GREEN;
+		if (randomNum == 1)
+			return Color.RED;
+		if (randomNum == 2)
+			return Color.BLUE;
+		if (randomNum == 3)
+			return Color.BLACK;
+		return Color.YELLOW;
 	}
 }
