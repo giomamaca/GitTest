e5d37d45af17987aeda545071d378d30b789396e
S <s>
2023-11-28 07:34:08 -0800

    {"types":"C","runs":5}

--- MidTerm172.java
+++ MidTerm172.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm172 {
+public class MidTerm172 extends ConsoleProgram{
+	public void run(){
+		int k = readInt();
+		int div = 0;
+		for(int i = 1; i <= k; i++){
+			if(k % i == 0 && simple(i)){
+				div = i;
+			}
+		}
+	}
 
+	private boolean simple(int i) {
+		if(i == 1) return false;
+		if(i % 2 == 0) return false;
+		for(int j = 3; j * j < i; j+=2){
+			if(i % j ==0) return false;
+		}
+		return true;
+	}
 }
