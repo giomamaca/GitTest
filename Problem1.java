688f5cd785f9ed78aca749ebd83f828f9ec57ba7
S <s>
2023-11-28 07:39:38 -0800

    {"types":"C","runs":14}

--- Problem1.java
+++ Problem1.java
@@ -6,13 +6,13 @@ public class Problem1 extends ConsoleProgram {
 		int x = 1;
 		int y = 2;
 		GObject o = new GRect(x, y);
-		String s = "shalaxxxo";
+		String s = "shalaxo";
 		println(iseRa(x, y, o, s));
 		println(iseRa(x, y, o, s));
 	}
 
 	private int iseRa(int x, double y, GObject r, String s) {
-		x++;
+		x+= 1;
 		y += 1;
 		r.move(x, y);
 		s = s.substring(s.indexOf('a'));
