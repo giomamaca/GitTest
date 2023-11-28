61a6d9e60a8a31816b7e07aed6e8ad69352c4933
S <s>
2023-11-28 08:49:30 -0800

    {"types":"C","runs":32}

--- MidTerm174.java
+++ MidTerm174.java
@@ -7,15 +7,14 @@ public class MidTerm174 extends ConsoleProgram{
 	
 	public void run(){
 		int k = readInt();
-		
-		while(k != 0){
+		for(int i = 0; true; i++){
 			boolean bool = rgen.nextBoolean();
 			if(bool == true){
 				k /= 2;
 			}
 			if(bool == false){
 				k -= 1;
-			}
+			}			
 		}
 	}
 }
