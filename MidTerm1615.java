6c465daaabfaeea80ae6c6acf8d811fa11986c33
S <s>
2023-11-28 10:08:14 -0800

    {"types":"C","runs":46}

--- MidTerm1615.java
+++ MidTerm1615.java
@@ -1,4 +1,19 @@
+import java.awt.event.MouseEvent;
 
-public class MidTerm1615 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
 
+public class MidTerm1615 extends GraphicsProgram{
+	
+	private static final double CIRCLE_D = 40; 
+	
+	public void run(){
+		addMouseListeners();
+		GOval cir = new GOval (getWidth() / 2 - CIRCLE_D /2, getHeight() / 2 - CIRCLE_D / 2, CIRCLE_D, CIRCLE_D);
+		add(cir);
+	}
+	public void mouseDragged(MouseEvent e){
+		
+	}
+	
 }
