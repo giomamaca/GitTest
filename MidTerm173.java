be5bcca5179229943d3a9a71f89d1afc68886cb6
S <s>
2023-11-28 07:48:50 -0800

    {"types":"C","runs":15}

--- MidTerm173.java
+++ MidTerm173.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class MidTerm173 {
-
+public class MidTerm173 extends ConsoleProgram{
+	public void run(){
+		String gamarjoba = readLine();
+		String baro = readLine();
+		
+		for(int i = 0; i< baro.length(); i++){
+			int k = 0;
+			char bar = baro.charAt(i);
+			if(gamarjoba.contains(baro)){
+				println("correct");
+				k++;
+				if(k == baro.length()){
+					println(baro);
+				}
+			}
+		}
+		
+	}
 }
