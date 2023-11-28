202af00ad16853e4aa8fbbff12ec01b1187e68a6
S <s>
2023-11-27 23:20:26 -0800

    {"types":"C","runs":1}

--- MidTerm1645.java
+++ MidTerm1645.java
@@ -1,4 +1,40 @@
+import java.awt.event.MouseEvent;
 
-public class MidTerm1645 {
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
+public class MidTerm1645 extends GraphicsProgram{
+	
+	private final static int CIRCLE_D = 40;
+	private final static int CIRCLE_NUM = 50;
+	private final static int DELAY = 2000;
+	
+	private int count = 0;
+	private GOval ball;
+	RandomGenerator rgen = new RandomGenerator();
+	
+	public void run(){
+		while(getElementCount() != 0 && getElementCount() <= 50){
+			drawCircle();			
+			pause(DELAY);
+		}
+		addMouseListeners();
+	}
+
+	public void mouseClicked (MouseEvent e){
+		if(getElementAt(e.getX(), e.getY()) != null){
+			count++;
+			if(count == 3){
+				remove(ball);
+				count = 0;
+			}
+		}
+	}
+
+	private void drawCircle() {
+		ball = new GOval (rgen.nextInt(0, getWidth() - CIRCLE_D), rgen.nextInt(0, getHeight() - CIRCLE_D), CIRCLE_D / 2, CIRCLE_D / 2);
+		ball.setFilled(true);
+		add(ball);
+	}
 }
