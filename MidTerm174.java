b8a03d2489d0f4c9de1d69c290fc8502e7d1c59b
S <s>
2023-11-28 08:53:04 -0800

    {"types":"C","runs":37}

--- MidTerm174.java
+++ MidTerm174.java
@@ -1,21 +1,21 @@
 import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
-public class MidTerm174 extends ConsoleProgram{
-	
+public class MidTerm174 extends ConsoleProgram {
+
 	RandomGenerator rgen = new RandomGenerator();
-	
-	public void run(){
+
+	public void run() {
 		int k = readInt();
-		
-		while(k != 0){
-			for(int i = 0; true; i++){
-				boolean bool = rgen.nextBoolean();				
-				if(bool == true){
+
+		for (int i = 0; true; i++) {
+			boolean bool = rgen.nextBoolean();
+			if (k != 0) {
+				if (bool == true) {
 					k /= 2;
 				}
-				if(bool == false){
-					k -=1;
+				if (bool == false) {
+					k -= 1;
 				}
 				println(k);
 			}
