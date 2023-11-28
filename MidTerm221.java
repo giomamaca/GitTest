b0c5b430e4d953d436fb9ec81e6fa413c4df0a18
S <s>
2023-11-28 00:05:19 -0800

    {"types":"C","runs":17}

--- MidTerm221.java
+++ MidTerm221.java
@@ -2,29 +2,21 @@ import acm.program.ConsoleProgram;
 
 public class MidTerm221 extends ConsoleProgram{
 	public void run(){
-		int count = 0;
-		int max = count;
 		while(true){
 			String s = readLine();
-			if(s == "END"){
+			if(s.equals("END")){
 				break;
 			}
-			if(count > max){
-				max = count;
-			}
-			for(int i = 0; i < 256; i++){
-				char a = s.charAt(i);
-				for(int j = i; j < 256; j++){
-					char b = s.charAt(j);
-					if(a == b){
-						return;
-					}
-					if(a != b){
-						count++;
-						println(s);
-					}
-				}
+			
+		}
+	}
+	private int countcharacter(String s){
+		String result = "";
+		for(int i = 0; i< s.length(); i++){
+			if(!result.contains("" + s.charAt(i))){
+				result += s.charAt(i);
 			}
 		}
+		return result.length();
 	}
 }
