0b97f5e02adddfe2cef509bf66c2a06b54156745
S <s>
2023-12-01 01:15:29 -0800

    {"types":"C","runs":24}

--- MidTerm1645.java
+++ MidTerm1645.java
@@ -4,58 +4,57 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class MidTerm1645 extends GraphicsProgram{
-	
+public class MidTerm1645 extends GraphicsProgram {
+
 	private final static int CIRCLE_D = 40;
 	private final static int CIRCLE_NUM = 50;
 	private final static int DELAY = 2000;
-	
+
 	private int count = 0;
 	private GOval ball;
 	RandomGenerator rgen = new RandomGenerator();
-	
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
-		for(int i = 0; i< 10; i++){
-			drawCircle();			
+		for (int i = 0; i < 10; i++) {
+			drawCircle();
 		}
 		pause(DELAY);
-		while(getElementCount() != 0 && getElementCount() <= CIRCLE_NUM){
-			drawCircle();				
+		while (getElementCount() != 0 && getElementCount() <= CIRCLE_NUM) {
+			drawCircle();
 			pause(DELAY);
 		}
 	}
 
-	public void mouseClicked (MouseEvent e){
+	public void mouseClicked(MouseEvent e) {
 		double x = e.getX();
 		double y = e.getY();
-		
-		if(getElementAt(x, y) != null){
+
+		if (getElementAt(x, y) != null) {
 			GObject obj = getElementAt(x, y);
-			GObject objTest = obj;
-			if(getElementAt(x, y) == obj){
-				if(count == 1){					
-					if(getElementAt(x, y) == objTest){
-						count++;
-					}
+			if (getElementAt(x, y) == obj) {
+				count++;
+				if (count >= 1) {
+					GObject objTest = obj;
 					if(getElementAt(x, y) != objTest){
 						count = 0;
-						obj = null;
+						objTest = null;
 					}
 				}
-				if(count == 3){
-					remove(obj);
-					count = 0;
-				}
 			}
-			if(getElementAt(x, y) == null){
+			if (count == 3) {
+				remove(obj);
+				count = 0;
+			}
+			if (getElementAt(x, y) == null) {
 				count = 0;
 			}
 		}
 	}
 
 	private void drawCircle() {
-		ball = new GOval (rgen.nextInt(0, getWidth() - CIRCLE_D), rgen.nextInt(0, getHeight() - CIRCLE_D), CIRCLE_D / 2, CIRCLE_D / 2);
+		ball = new GOval(rgen.nextInt(0, getWidth() - CIRCLE_D), rgen.nextInt(0, getHeight() - CIRCLE_D), CIRCLE_D / 2,
+				CIRCLE_D / 2);
 		ball.setFilled(true);
 		add(ball);
 	}
