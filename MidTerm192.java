b5db60831d2dd795219b039e57cff391836c1fa9
S <s>
2023-12-01 06:15:40 -0800

    {"types":"C","runs":139}

--- MidTerm192.java
+++ MidTerm192.java
@@ -2,45 +2,18 @@ import acm.program.ConsoleProgram;
 
 public class MidTerm192 extends ConsoleProgram{
 	public void run(){
-		int count = 0;
 		int k = readInt();
-		int n = k;
+		int p = k;
+		int a = 0;
+		
 		while(k != 0){
-			k = k / 10;
-			count++;
+			a = a*10 + k%10;
+			k /= 10;
 		}
-		int half = count / 2;
-		int swap = 0;
-		int a = 1;
-		while(half != 0){
-			a *= 10;
-			half--;
-		}
-		if(count % 2 == 0){
-			int div1 = n % a;
-			int div2 = n / a;			
-			while(div2 != 0){
-				swap = swap * 10 + div2 % 10;
-				div2 = div2 / 10;
-			}		
-			if(div1 == swap){
-				println("correct");
-			}else{
-				println("wrong");
-			}
-		}
-		if(count % 2 == 1){
-			int div1 = n % a;
-			int div2 = n / (a*10);			
-			while(div2 != 0){
-				swap = swap * 10 + div2 % 10;
-				div2 = div2 / 10;
-			}
-			if(div1 == swap){
-				println("correct");
-			}else{
-				println("wrong");
-			}
+		if(p == a){
+			println("palidrom");
+		}else{
+			println("no");
 		}
 	}
 }
