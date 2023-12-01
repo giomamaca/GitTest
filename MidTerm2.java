1353393443009e2eb11da7646fcd75baa296699f
S <s>
2023-12-01 06:20:30 -0800

    {"types":"C","runs":1}

--- MidTerm2.java
+++ MidTerm2.java
@@ -2,21 +2,25 @@ import acm.program.ConsoleProgram;
 
 public class MidTerm2 extends ConsoleProgram {
 	public void run() {
-		String father = readLine();
 		String son = readLine();
-		println(isFath(father, son));
+		String father = readLine();
+		
+		if(isFather(son, father)){
+			println("is son");
+		}else{
+			println("not");
+		}
 	}
 
-	private boolean isFath(String father, String son) {
-		String so = "";
-		for (int i = 0; i < son.length(); i++) {
-			for (int j = i+1; j < son.length(); j++) {
-				String s = son.substring(i, j);
-					so = s;
-					if (father.contains(s) && so.length() >= father.length() / 2){ 
-						return true;
-					}
+	private boolean isFather(String son, String father) {
+		String s = "";
+		for(int i = 0; i< son.length(); i++){
+			for(int j = 0; j < son.length(); j++){
+				s = son.substring(i, j);
 			}
+		}
+		if(father.contains(s)){
+			return true;
 		}
 		return false;
 	}
