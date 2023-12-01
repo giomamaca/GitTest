e952519d555901c18c60499cc15103b01a3b3b5e
S <s>
2023-12-01 00:11:42 -0800

    {"types":"C","runs":7}

--- MidTerm1625.java
+++ MidTerm1625.java
@@ -1,4 +1,15 @@
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class MidTerm1625 {
-
+public class MidTerm1625 extends GraphicsProgram{
+	
+	private final static int CIRCLE_D = 40;
+	private final static int CIRCLE_NUM = 25;
+	private final static int DELAY = 2000;
+	
+	RandomGenerator rgen = new RandomGenerator();
+	
+	public void run(){
+		
+	}
 }
