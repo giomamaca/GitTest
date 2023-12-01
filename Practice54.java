856ec8b886f6e8e5d1553340bf9d09461619ebd6
S <s>
2023-12-01 08:27:56 -0800

    {"types":"C","runs":5}

--- Practice54.java
+++ Practice54.java
@@ -1,4 +1,28 @@
+import acm.program.ConsoleProgram;
 
-public class Practice54 {
-
+public class Practice54 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("number of nums = ");
+		int biggest = readInt("num = ");
+		int secondBiggest = readInt("num = ");
+		if(secondBiggest > biggest){
+			int a =  secondBiggest;
+			secondBiggest = biggest;
+			biggest = a;
+		}
+		
+		for(int i = 0; i < n-2; i++){
+			int num = readInt("num = ");
+			if(num > biggest){
+				int temp = biggest;
+				biggest = num;
+				secondBiggest = temp;
+			}
+			if(num > secondBiggest){
+				secondBiggest = num;
+			}
+		}
+		println(biggest);
+		println(secondBiggest);
+	}
 }
