6ef1b5f81f647a9ac28dc292a307c23595676814
S <s>
2023-12-01 05:36:12 -0800

    {"types":"C","runs":121}

--- MidTerm193.java
+++ MidTerm193.java
@@ -34,7 +34,7 @@ public class MidTerm193 extends GraphicsProgram {
 			obj = o;
 			while(obj.getColor() != java.awt.Color.GREEN){				
 				obj.setColor(randomColor());
-				pause(500);
+				pause(100);
 			}
 		}
 	}
