a7b28d9cb9dc5abc4a6aaccb39cf915ec1f30474
S <s>
2023-12-01 05:05:35 -0800

    {"types":"C","runs":111}

--- MidTerm193.java
+++ MidTerm193.java
@@ -5,49 +5,44 @@ import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 import javafx.scene.paint.Color;
 
-public class MidTerm193 extends GraphicsProgram{
+public class MidTerm193 extends GraphicsProgram {
 	private final int CIRCLE_D = 100;
 	private final int DELAY = 100;
 	RandomGenerator rgen = new RandomGenerator();
-	private GObject obj;
 
-	public void run(){
+	public void run() {
+		drawCircle();
 		addMouseListeners();
 	}
-	
-	public void mouseClicked(MouseEvent e){
+
+	private void drawCircle() {
+		GOval oval = new GOval (CIRCLE_D, CIRCLE_D);
+		oval.setFilled(true);
+		oval.setColor(rgen.nextColor());
+		add(oval);
+	}
+
+	public void mouseClicked(MouseEvent e) {
 		double x = e.getX();
 		double y = e.getY();
 		
-		if(getElementAt(x,y) == null){
-			GOval oval = new GOval (x - CIRCLE_D / 2, y - CIRCLE_D / 2, CIRCLE_D, CIRCLE_D);
-			oval.setFilled(true);
-			oval.setColor(randomColor());
-			add(oval);
-			pause(DELAY);
-		}		
-		if(getElementAt(x,y) != null){
-			obj = getElementAt(x,y);
-			while(obj.getColor() !=  java.awt.Color.GREEN){
-				obj.setColor(randomColor());
-			}
+		drawCircle();
+		if(getElementAt(x, y) != null){
+			
 		}
 	}
 
-	private java.awt.Color randomColor() {
+	private Color randomColor() {
 		int a = rgen.nextInt(5);
-		if(a == 0){
-			return java.awt.Color.green;
+		if (a == 0) {
+			return Color.GREEN;
+		} else if (a == 1) {
+			return Color.RED;
+		} else if (a == 2) {
+			return Color.BLUE;
+		} else if (a == 3) {
+			return Color.BLACK;
 		}
-		if(a == 1){
-			return java.awt.Color.RED;
-		}
-		if(a == 2){
-			return java.awt.Color.BLUE;
-		}
-		if(a == 3){
-			return java.awt.Color.BLACK;
-		}
-		return java.awt.Color.YELLOW;
+			return Color.YELLOW;
 	}
 }
