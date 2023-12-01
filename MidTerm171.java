9f57045a9a7403a32551f29d7cb9966c3d9de383
S <s>
2023-12-01 04:31:49 -0800

    {"types":"C","runs":93}

--- MidTerm171.java
+++ MidTerm171.java
@@ -11,7 +11,7 @@ public class MidTerm171 extends GraphicsProgram {
 	private GRect rect;
 	private GLabel num;
 	private int k = 0;
-	private double coordinate1 = coordinate1 = num.getX() - num.getWidth() / 2;
+	private double coordinate1 = num.getX() - num.getWidth() / 2;
 	private double coordinate2 = coordinate1;
 
 	public void run() {
