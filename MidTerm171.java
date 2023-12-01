bbdfd1923e79c37047c689a72f10da5cb8bcc2ef
S <s>
2023-12-01 03:52:50 -0800

    {"types":"C","runs":78}

--- MidTerm171.java
+++ MidTerm171.java
@@ -11,35 +11,35 @@ public class MidTerm171 extends GraphicsProgram {
 	private GRect rect;
 	private GLabel num;
 	private int k = 0;
-	public double cen1 = getWidth() / 2 - SIZE / 2;
-	public double cen2 = getHeight() / 2 - SIZE / 2;
+	private double cen1 = getWidth() / 2 - SIZE / 2;
+	private double cen2 = getHeight() / 2 - SIZE / 2;
 
 	public void run() {
 		addMouseListeners();
 
-		GRect rect = new GRect(cen1, cen2, SIZE, SIZE);
-//		num = new GLabel("da");
-//		add(num, cen1 - num.getWidth() / 2, cen2 + num.getAscent() / 2);
+		GRect rect = new GRect(getWidth() / 2 - SIZE / 2, getHeight() / 2 - SIZE / 2, SIZE, SIZE);
+		num = new GLabel("da");
+		add(num, getWidth() / 2 - SIZE / 2 - num.getWidth() / 2, getHeight() / 2 - SIZE / 2 + num.getAscent() / 2);
 		add(rect);
 
 		System.out.println(k);
 	}
 
-//	public void mouseDragged(MouseEvent e) {
-//		double x = e.getX();
-//		double y = e.getY();
-//
-//		double moveX = x - SIZE / 2;
-//		rect.setLocation(moveX, y - SIZE / 2);
-//		if (k != 9) {
-//			if ((rect.getX() + SIZE / 2) > X) {
-//				k++;
-//				X = rect.getX() + SIZE / 2;
-//			}
-//			if ((rect.getX() + SIZE / 2) < X) {
-//				k--;
-//				X = rect.getX() + SIZE / 2;
-//			}
-//		}
-//	}
+	public void mouseDragged(MouseEvent e) {
+		double x = e.getX();
+		double y = e.getY();
+
+		double moveX = x - SIZE / 2;
+		rect.setLocation(moveX, y - SIZE / 2);
+		if (k != 9) {
+			if ((rect.getX() + SIZE / 2) > X) {
+				k++;
+				X = rect.getX() + SIZE / 2;
+			}
+			if ((rect.getX() + SIZE / 2) < X) {
+				k--;
+				X = rect.getX() + SIZE / 2;
+			}
+		}
+	}
 }
