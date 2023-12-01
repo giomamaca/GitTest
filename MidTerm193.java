4852a4b7b84d7fa55aeb969de6ceef842c09fb44
S <s>
2023-12-01 05:38:48 -0800

    {"types":"C","runs":123}

--- MidTerm193.java
+++ MidTerm193.java
@@ -40,6 +40,7 @@ public class MidTerm193 extends GraphicsProgram {
 
 	private Color randomColor() {
 		int a = rgen.nextInt(N_COLORS);
+		System.out.println(a);
 		if (a == 0) {
 			return Color.GREEN;
 		} else if (a == 1) {
