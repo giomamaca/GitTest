6f8b74f3f8e535e10f3abc5d0a4aab0953eed24f
S <s>
2023-12-01 03:43:46 -0800

    {"types":"C","runs":63}

--- MidTerm171.java
+++ MidTerm171.java
@@ -15,12 +15,12 @@ public class MidTerm171 extends GraphicsProgram{
 	private double cen2	= getHeight() / 2 - SIZE / 2;
 	
 	public void run(){
+		addMouseListeners();
 		
-		rect = new GRect(cen1, cen2, SIZE, SIZE);
+		rect = new GRect(0, 0, SIZE, SIZE);
 //		num = new GLabel(k);
 //		add(num, cen1 - num.getWidth() / 2, cen2 + num.getAscent() / 2);
 		add(rect);
-		addMouseListeners();
 		
 	}
 	
