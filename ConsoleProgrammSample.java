2df41ea0292753e6f2815e3687bebea3e83c9a74
S <s>
2023-12-01 21:38:52 -0800

    {"types":"C","runs":33}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -13,7 +13,7 @@ public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
 		String s = "fshdfbasffsafsaf";
 		for(int i = 0; i< s.length(); i++){
-			String sub1 = s.substring(1,4);
+			String sub1 = s.substring(0,4) + s.substring(0,s.length());
 			s =  sub1;
 		}
 		print(s);
