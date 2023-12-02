03ff64f176ab05fefa43d3da3890e2ede7bf3e62
S <s>
2023-12-02 01:49:32 -0800

    {"types":"C","runs":29}

--- exercise.java
+++ exercise.java
@@ -1,14 +1,30 @@
 import acm.graphics.*;
+import acm.program.ConsoleProgram;
 import acm.program.*;
 import java.awt.*;
 
 public class exercise extends ConsoleProgram {	
 	public void run() {
-		String str = "dessert";
-		String answer = "";
-		for(int i = 0; i < str.length(); i++){
-			answer = str.charAt(i) + answer;
+		String a = readLine();
+		String b = readLine();
+		println(isIn(a, b));
+		
+	}
+
+	private boolean isIn(String a, String b) {
+		for(int i = 0; i < b.length(); i++){
+			if(a.contains(b.charAt(i) + "")){
+				a = cut(a, b.charAt(i));
+				b = cut(b, b.charAt(i));
+			}else return false;
 		}
-		println(answer);
+		return true;
+	}
+
+	private String cut(String a, char c) {
+		String sub1 = a.substring(0, a.indexOf(c));
+		String sub2 = a.substring(a.indexOf(c) + 1);
+		
+		return sub1 + sub2;
 	}
 }
