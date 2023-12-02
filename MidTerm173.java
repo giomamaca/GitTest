899b5a90e8acdb6f7b2523d32ced1f578f64ce8a
S <s>
2023-12-01 21:27:36 -0800

    {"types":"C","runs":4}

--- MidTerm173.java
+++ MidTerm173.java
@@ -9,12 +9,14 @@ public class MidTerm173 extends ConsoleProgram{
 	}
 
 	private boolean isIn(String gamarjoba, String baro) {
+		String sub = "";
 		
 		for(int i = 0; i< baro.length(); i++){
 			char bar = baro.charAt(i);
 			for(int j = i; j < gamarjoba.length(); j++){
 				char gam = gamarjoba.charAt(j);
 				if(bar == gam){
+					String s = gamarjoba.substring(0, j) + gamarjoba.substring(j, gamarjoba.length());
 					return true;
 				}
 			}
