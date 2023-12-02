10d618ed810bc7e0fca8ee37c303203eea522ce3
S <s>
2023-12-02 01:11:21 -0800

    {"types":"C","runs":9}

--- MidTerm173.java
+++ MidTerm173.java
@@ -1,22 +1,30 @@
 import acm.program.ConsoleProgram;
 
-public class MidTerm173 extends ConsoleProgram{
-	public void run(){
-		String gamarjoba = readLine();
-		String baro = readLine();
-	
-		String sub = gamarjoba;
-		String dzm = baro;
-		
-		for(int i = 0; i< dzm.length(); i++){
-			char bar = dzm.charAt(i);
-			for(int j = 0; j < sub.length(); j++){
-				char gam = sub.charAt(j);
-				if(bar == gam){
-					sub = sub.substring(0, j) + sub.substring(j + 1);
-					System.out.println(sub);
+public class MidTerm173 extends ConsoleProgram {
+	public void run() {
+		String a = readLine();
+		String b = readLine();
+		println(isIn(a, b));
+	}
+
+	private boolean isIn(String a, String b) {
+		String gam = a;
+		String bar = b;
+
+		for (int i = 0; i < bar.length(); i++) {
+			char x = bar.charAt(i);
+			for (int j = 0; j < gam.length(); j++) {
+				char y = gam.charAt(j);
+				if (x == y) {
+					bar = bar.substring(0, i) + bar.substring(i + 1);
+					gam = gam.substring(0, j) + bar.substring(j + 1);
+					
 				}
 			}
 		}
+		if(bar != null){
+			return false;			
+		}
+		return true;
 	}
 }
