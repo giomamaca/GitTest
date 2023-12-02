f126058da8b7c11cd190922591f0b54433fee41c
S <s>
2023-12-02 04:36:49 -0800

    {"types":"C","runs":1}

--- texili.java
+++ texili.java
@@ -1,4 +1,24 @@
+import java.awt.event.MouseEvent;
 
-public class texili {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
+public class texili extends GraphicsProgram{
+	
+	private double startX = 0;
+	private double startY = 0;
+	
+	private GLine line;
+	
+	public void run(){
+		addMouseListeners();
+	}
+	public void mouseClicked(MouseEvent e){
+		line = new GLine(startX, startY, e.getX(), e.getY());
+		add(line);
+		
+		startX = e.getX();
+		startY = e.getY();
+		
+	}
 }
