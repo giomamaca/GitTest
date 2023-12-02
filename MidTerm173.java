927496a8c794e8c099e999e415906ad5c9e93526
S <s>
2023-12-02 00:12:05 -0800

    {"types":"C","runs":21}

--- MidTerm173.java
+++ MidTerm173.java
@@ -19,7 +19,9 @@ public class MidTerm173 extends ConsoleProgram{
 				if(bar == gam){
 					sub = sub.substring(0, j) + sub.substring(j + 1);
 					dzm = dzm.substring(0, i) + dzm.substring(i + 1);
-					return true;
+					if(dzm == null){
+						return true;						
+					}
 				}
 			}
 		}
