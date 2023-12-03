ab1577777c93c416ffc201f2b7755f6297ac7895
S <s>
2023-12-03 10:54:13 -0800

    {"types":"C","runs":20}

--- MidTerm1624.java
+++ MidTerm1624.java
@@ -1,4 +1,30 @@
+import java.util.StringTokenizer;
 
-public class MidTerm1624 {
+import acm.program.ConsoleProgram;
 
+public class MidTerm1624 extends ConsoleProgram{
+	public void run(){
+		String text = readLine();
+		println(unEvilText(text));
+	}
+	private String unEvilText(String text){
+		String answer = "";
+		
+		StringTokenizer tok = new StringTokenizer(text, " ");
+		
+		while(tok.hasMoreTokens()){
+			String s = tok.nextToken();
+			for(int i = 0; i < s.length(); i++){
+				char a = s.charAt(i);
+				char b = s.charAt(i -1);
+				
+				if(a == b){
+					String sub1 = s.substring(0, i - 1);
+					String sub2 = s.substring(i);
+					answer = sub1 + sub2;
+					return;
+				}
+			}
+		}
+	}
 }
