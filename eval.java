6f90c0c028679f44eb33dc69d9c875e704a6d5d6
S <s>
2023-12-03 05:05:07 -0800

    {"types":"C","runs":11}

--- eval.java
+++ eval.java
@@ -1,7 +1,21 @@
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class eval extends ConsoleProgram{
 	public void run(){
+		String s = readLine();
+		int i =  Integer.valueOf(s); 
+		angarishi(s);
+		print(i);
+	}
+
+	private int angarishi(String s) {
+		StringTokenizer tok = new StringTokenizer(s, " ");
 		
+		
+		
+		
+		return 0;
 	}
 }
