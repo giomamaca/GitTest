18eac65769cd7807238d66d89a7cfe0b24c51eb8
S <s>
2023-12-03 10:41:26 -0800

    {"types":"C","runs":15}

--- Misterm1624.java
+++ Misterm1624.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class Misterm1624 {
-
+public class Misterm1624 extends ConsoleProgram{
+	public void run(){
+		String text = readLine();
+		println(unEvilText(text));
+	}
+	private String unEvilText(String text){
+		String answer ="" + text.charAt(0);
+		
+		for(int i = 1; i < text.length(); i++){
+			char a = text.charAt(i);
+			char b = text.charAt(i - 1);
+			
+			if(a != b){
+				answer += a;
+			}
+		}
+		return answer;
+	}
 }
