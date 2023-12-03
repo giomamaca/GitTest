bd02ed165cdf14d2be86dfa9a3b3d537ffae61e3
S <s>
2023-12-02 22:47:37 -0800

    {"types":"C","runs":3}

--- random.java
+++ random.java
@@ -2,26 +2,25 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class random extends GraphicsProgram{
-	
+public class random extends GraphicsProgram {
+
 	private static final double MIN_RADIUS = 30;
 	private static final double MAX_RADIUS = 80;
 	private GOval oval;
-	
+
 	RandomGenerator rgen = new RandomGenerator();
-	
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
 		double radius = rgen.nextDouble(MIN_RADIUS, MAX_RADIUS);
 		double coordX = rgen.nextDouble(0, getWidth() - radius);
 		double coordY = rgen.nextDouble(0, getHeight() - radius);
-		
-		
-			for(int i = 0; i < 25; i++){
-				oval = new GOval(coordX, coordY, radius, radius);
-				oval.setFilled(true);
-				add(oval);
-						
+
+		for (int i = 0; i < 25; i++) {
+			oval = new GOval(coordX, coordY, radius, radius);
+			oval.setFilled(true);
+			add(oval);
+
 		}
 	}
 }
