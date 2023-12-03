ee4bff9039925938cbf07bf21f87eb577204483b
S <s>
2023-12-02 22:47:56 -0800

    {"types":"C","runs":4}

--- random.java
+++ random.java
@@ -17,7 +17,7 @@ public class random extends GraphicsProgram {
 		double coordY = rgen.nextDouble(0, getHeight() - radius);
 
 		for (int i = 0; i < 25; i++) {
-			oval = new GOval(coordX, coordY, radius, radius);
+			oval = new GOval(coordX, coordY, 50, 50);
 			oval.setFilled(true);
 			add(oval);
 
