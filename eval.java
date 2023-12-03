68767d4b5cb2d0e4213c773ba490f835fbd5c9ec
S <s>
2023-12-03 05:15:11 -0800

    {"types":"C","runs":15}

--- eval.java
+++ eval.java
@@ -5,17 +5,22 @@ import acm.program.ConsoleProgram;
 public class eval extends ConsoleProgram{
 	public void run(){
 		String s = readLine();
-		int i =  Integer.valueOf(s); 
 		angarishi(s);
-		print(i);
 	}
 
-	private int angarishi(String s) {
-		StringTokenizer tok = new StringTokenizer(s, " ");
+	private int angarishi(String s) {		
+		int k = 0;
+		int a = k;
 		
+		String str = "";
 		
-		
-		
+		for(int i = 0; i < s.length(); i++){
+			char ch = s.charAt(i);
+			if("" + ch != " "){
+				str += ch;
+				System.out.println(str);
+			}
+		}
 		return 0;
 	}
 }
