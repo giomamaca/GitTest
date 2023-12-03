d60a840e352d479f9f96c1750e62b20d3dcdd93a
S <s>
2023-12-03 10:34:05 -0800

    {"types":"C","runs":13}

--- MidTerm1625.java
+++ MidTerm1625.java
@@ -5,90 +5,81 @@ import acm.graphics.*;
 import acm.program.GraphicsProgram;
 import acm.util.RandomGenerator;
 
-public class MidTerm1625 extends GraphicsProgram{
-	
+public class MidTerm1625 extends GraphicsProgram {
+
 	private final static int CIRCLE_D = 40;
 	private final static int CIRCLE_NUM = 25;
 	private final static int DELAY = 2000;
-	
+
 	private GOval oval;
-	
+
 	private GOval o1 = null;
 	private GOval o2 = null;
-	
-	private Color c1;
-	private Color c2;
-	
+
 	RandomGenerator rgen = new RandomGenerator();
-	
-	
-	public void run(){
+
+	public void run() {
 		addMouseListeners();
 		drawOvals();
 	}
 
 	private void drawOvals() {
-		for(int i = 0; i < CIRCLE_NUM; i++){
+		for (int i = 0; i < CIRCLE_NUM; i++) {
 			double corX = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double corY = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			double corX1 = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double corY1 = rgen.nextDouble(0, getHeight() - CIRCLE_D);
-			 
+
 			Color col = rgen.nextColor();
-			
-			oval = new GOval(corX, corY, CIRCLE_D,CIRCLE_D);
+
+			oval = new GOval(corX, corY, CIRCLE_D, CIRCLE_D);
 			oval.setFilled(true);
 			oval.setColor(col);
 			add(oval);
-			
-			oval = new GOval(corX1, corY1, CIRCLE_D,CIRCLE_D);
+
+			oval = new GOval(corX1, corY1, CIRCLE_D, CIRCLE_D);
 			oval.setFilled(true);
 			oval.setColor(col);
 			add(oval);
 		}
-		while(true){
+		while (true) {
 			double corX = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double corY = rgen.nextDouble(0, getHeight() - CIRCLE_D);
 			double corX1 = rgen.nextDouble(0, getWidth() - CIRCLE_D);
 			double corY1 = rgen.nextDouble(0, getHeight() - CIRCLE_D);
-			 
+
 			Color col = rgen.nextColor();
-			
-			oval = new GOval(corX, corY, CIRCLE_D,CIRCLE_D);
+
+			oval = new GOval(corX, corY, CIRCLE_D, CIRCLE_D);
 			oval.setFilled(true);
 			oval.setColor(col);
 			add(oval);
-			
-			oval = new GOval(corX1, corY1, CIRCLE_D,CIRCLE_D);
+
+			oval = new GOval(corX1, corY1, CIRCLE_D, CIRCLE_D);
 			oval.setFilled(true);
 			oval.setColor(col);
 			add(oval);
-			
+
 			pause(DELAY);
 		}
 	}
-	
-	public void mouseClicked(MouseEvent e){
+
+	public void mouseClicked(MouseEvent e) {
 		GObject o = getElementAt(e.getX(), e.getY());
+
+		if (o == null)
+			return;
+
+		o2 = o1;
+		o1 = (GOval) o;
 		
-		if(o1 == null){
-			o1 = (GOval) o;
-			c1 = o1.getColor();
-		}else{
-			if(o2 == null){
-				o2 = (GOval) o;
-				c2 = o2.getColor();
+		if(o2 != null){
+			if(o1.getColor() == o2.getColor()){
+				remove(o1);
+				remove(o2);
+				o1 = null;
+				o2 = null;
 			}
 		}
-		if(c1 == c2){
-			remove(o1);
-			remove(o2);
-			o1 = null;
-			o2 = null;
-		}else{
-			o1 = null;
-			o2 = null;
-		}
 	}
-	
 }
