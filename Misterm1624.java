11166b867ce54a03988465afd08b151b00858637
S <s>
2023-12-03 10:47:44 -0800

    {"types":"C","runs":17}

--- Misterm1624.java
+++ Misterm1624.java
@@ -1,5 +1,3 @@
-import java.util.StringTokenizer;
-
 import acm.program.ConsoleProgram;
 
 public class Misterm1624 extends ConsoleProgram{
@@ -10,18 +8,14 @@ public class Misterm1624 extends ConsoleProgram{
 	private String unEvilText(String text){
 		String answer = "";
 		
-		StringTokenizer tok = new StringTokenizer(text, " ");
-		
-		while(tok.hasMoreTokens()){
-			for(int i = 0; i< text.length(); i++){
-				char a = text.charAt(i);
-				char b = text.charAt(i-1);
-				
-				if(a == b){
-					String sub1 = text.substring(0, i -1);
-					String sub2 = text.substring(i);
-					answer = sub1 + sub2;
-				}
+		for(int i = 1; i < text.length(); i++){
+			char a = text.charAt(i);
+			char b = text.charAt(i - 1);
+			
+			if(a == b){
+				String sub1 = text.substring(0, i - 1);
+				String sub2 = text.substring(0, i);
+				answer = sub1 + sub2;
 			}
 		}
 		return answer;
