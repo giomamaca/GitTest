566cc7f86edaa50dc82c5164e6fcd1aaefd85211
S <s>
2023-12-03 05:15:47 -0800

    {"types":"C","runs":16}

--- eval.java
+++ eval.java
@@ -16,7 +16,7 @@ public class eval extends ConsoleProgram{
 		
 		for(int i = 0; i < s.length(); i++){
 			char ch = s.charAt(i);
-			if("" + ch != " "){
+			if("" + ch == " "){
 				str += ch;
 				System.out.println(str);
 			}
