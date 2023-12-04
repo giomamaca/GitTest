8b6e359ea34b41f81f4e15572ebd6be010baca10
S <s>
2023-12-04 07:22:21 -0800

    {"types":"C","runs":24}

--- MidTerm1624.java
+++ MidTerm1624.java
@@ -14,13 +14,20 @@ public class MidTerm1624 extends ConsoleProgram{
 		while(tok.hasMoreTokens()){
 			String s = tok.nextToken();
 			String cor = "";
+			int count = 1;
 			char wrong = 'a';
 			for(int i = 1; i < s.length(); i++){
 				char a = s.charAt(i);
 				char b = s.charAt(i - 1);
 				if(a == b){
+					wrong = a;
+					for(int k = 0; k < s.length(); k++){
+						if(s.charAt(i) == wrong){
+							count++;
+						}
+					}
 					String sub1 = s.substring(0, i - 1);
-					String sub2 = s.substring(i + 1);
+					String sub2 = s.substring(i + count);
 					cor = sub1 + sub2;
 				}
 			}
