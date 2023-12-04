381e9525c8f4e697f9adba434bd828726134fe45
S <s>
2023-12-04 08:23:35 -0800

    {"types":"C","runs":35}

--- MidTerm1624.java
+++ MidTerm1624.java
@@ -2,35 +2,50 @@ import java.util.StringTokenizer;
 
 import acm.program.ConsoleProgram;
 
-public class MidTerm1624 extends ConsoleProgram{
-	public void run(){
+public class MidTerm1624 extends ConsoleProgram {
+	public void run() {
 		String text = readLine();
 		println(unEvilText(text));
 	}
-	private String unEvilText(String text){
+
+	private String unEvilText(String text) {
 		StringTokenizer tok = new StringTokenizer(text, " ");
 		String str = "";
-		
-		while(tok.hasMoreTokens()){
+
+		while (tok.hasMoreTokens()) {
 			String s = tok.nextToken();
 			String cor = "";
+
 			int count = 1;
 			char wrong = 'a';
-			for(int i = 1; i < s.length(); i++){
+			for (int i = 1; i < s.length(); i++) {
 				char a = s.charAt(i);
 				char b = s.charAt(i - 1);
-				if(a == b){
+				if (a == b) {
 					wrong = a;
 				}
 			}
-			for(int j = 0; j < s.length(); j++){
-				char c = s.charAt(j);
-				if(c != wrong){							
-					cor += c;
+			if(bevri(s)){
+				for (int j = 0; j < s.length(); j++) {
+					char c = s.charAt(j);
+					if (c != wrong) {
+						cor += c;
+					}
 				}
-			}
-			str += cor + " ";
+				str += cor + " ";
+			}				
 		}
 		return str;
+	}
+
+	private boolean bevri(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			char a = s.charAt(i);
+			char b = s.charAt(i - 1);
+			if (a == b) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
