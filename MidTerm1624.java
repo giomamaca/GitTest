31a6e6c46407135426d40b7b997873615e0f16e1
S <s>
2023-12-04 09:19:53 -0800

    {"types":"C","runs":54}

--- MidTerm1624.java
+++ MidTerm1624.java
@@ -16,6 +16,7 @@ public class MidTerm1624 extends ConsoleProgram {
 			String s = tok.nextToken();
 			String cor = s;
 
+			int count = 1;
 			char wrong = 'a';
 			for (int i = 1; i < s.length(); i++) {
 				char a = s.charAt(i);
@@ -28,12 +29,22 @@ public class MidTerm1624 extends ConsoleProgram {
 			for (int j = 0; j < cor.length(); j++) {
 				char c = cor.charAt(j);
 				if (c != wrong) {
-					cor = "";
 					cor += c;
 				}
 			}
 			str += cor + " ";
 		}
 		return str;
+	}
+
+	private boolean bevri(String s) {
+		for (int i = 0; i < s.length(); i++) {
+			char a = s.charAt(i);
+			char b = s.charAt(i - 1);
+			if (a == b) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
