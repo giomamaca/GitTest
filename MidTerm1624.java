c68575ae7d75e250c8e18632dcacda3af649c451
S <s>
2023-12-04 08:26:11 -0800

    {"types":"C","runs":38}

--- MidTerm1624.java
+++ MidTerm1624.java
@@ -18,13 +18,12 @@ public class MidTerm1624 extends ConsoleProgram {
 
 			int count = 1;
 			char wrong = 'a';
-			if(bevri(s)){				
-				for (int i = 1; i < s.length(); i++) {
-					char a = s.charAt(i);
-					char b = s.charAt(i - 1);
-					if (a == b) {
-						wrong = a;
-					}
+			for (int i = 1; i < s.length(); i++) {
+				char a = s.charAt(i);
+				char b = s.charAt(i - 1);
+				if (a == b) {
+					wrong = a;
+
 				}
 			}
 			for (int j = 0; j < s.length(); j++) {
