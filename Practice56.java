d438186a73274cfe88b0c9990093231fc6e10578
S <s>
2023-12-05 09:42:50 -0800

    {"types":"C","runs":18}

--- Practice56.java
+++ Practice56.java
@@ -4,8 +4,8 @@ public class Practice56 extends ConsoleProgram{
 	public void run(){
 		int n = readInt();
 		int [] num = new int[n];
-		
 		for(int i = 0; i < num.length; i++){
+			num[i] = readInt();
 			
 		}
 		println(num);
