8fe5d61a1a8efb1e99754f295e779674df61871f
S <s>
2023-12-05 00:09:57 -0800

    {"types":"C","runs":30}

--- Practice55.java
+++ Practice55.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class Practice55 {
-
+public class Practice55 extends ConsoleProgram{
+	public void run(){
+		int n = readInt();
+		for(int i = 0; i < n; i++){
+			
+		}
+	}
 }
