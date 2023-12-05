2934d8cdbee6df9f21cc0d3275be2b320d82fc77
S <s>
2023-12-05 10:13:06 -0800

    {"types":"C","runs":22}

--- Practice56.java
+++ Practice56.java
@@ -3,14 +3,10 @@ import acm.program.ConsoleProgram;
 public class Practice56 extends ConsoleProgram{
 	public void run(){
 		int n = readInt();
-		
-		int[] arr;
-		
-		arr = new int[n];
+		int[] arr = new int[n];
 		
 		for(int i = 0; i < arr.length; i++){
-			int k = readInt();
-			println("arr#" + i + "[" + k + "]");
+			
 			if(zrdadia(arr)){
 				println("sorted");
 			}else{
