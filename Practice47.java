fd7a5f207ab21789f1e59bf11faa7cc0ad2fe510
S <s>
2023-12-06 00:38:32 -0800

    {"types":"C","runs":7}

--- Practice47.java
+++ Practice47.java
@@ -8,7 +8,7 @@ public class Practice47 extends ConsoleProgram {
 
 	private boolean isPredicat(String s) {
 		String str = "";
-		for (int i = 0; i > s.length(); i++) {
+		for (int i = 0; i < s.length(); i++) {
 			char a = s.charAt(i);
 			str = a + str;
 		}
