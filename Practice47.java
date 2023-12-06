fd6c1d43e7aef331e36c70451227c82b09d64379
S <s>
2023-12-06 00:38:05 -0800

    {"types":"C","runs":4}

--- Practice47.java
+++ Practice47.java
@@ -3,14 +3,14 @@ import acm.program.ConsoleProgram;
 public class Practice47 extends ConsoleProgram {
 	public void run() {
 		String s = readLine();
-		println(izPredicat(s));
+		println(isPredicat(s));
 	}
 
-	private boolean izPredicat(String s) {
+	private boolean isPredicat(String s) {
 		String str = "";
-		for (int i = s.length(); i > 0; i--) {
+		for (int i = 0; i > s.length(); i++) {
 			char a = s.charAt(i);
-			str += a;
+			str = a + str;
 		}
 		if (str == s) {
 			return true;
