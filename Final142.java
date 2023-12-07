e83e792f99da415cf133dcc43ecbea97f4fd08fc
S <s>
2023-12-07 10:33:46 -0800

    {"types":"C","runs":1}

--- Final142.java
+++ Final142.java
@@ -1,4 +1,38 @@
+import java.util.StringTokenizer;
 
-public class Final142 {
+import acm.program.ConsoleProgram;
 
+public class Final142 extends ConsoleProgram {
+	public void run() {
+		String text = readLine();
+	}
+
+	private String translateToTitle(String text) {
+		StringTokenizer tk = new StringTokenizer(text);
+		String result = "";
+		
+
+		while (tk.hasMoreTokens()) {
+			String token = tk.nextToken();
+
+			String answer = "";
+
+			for (int i = 0; i < token.length(); i++) {
+				String s = "";
+				char a = token.charAt(i);
+				String st = "";
+				if (i == 0) {
+					s += a;
+					s.toUpperCase();
+				} else {
+					st += a;
+					st.toLowerCase();
+				}
+				s += st;
+				answer += s;
+			}
+			result += answer;
+		}
+		return result;
+	}
 }
