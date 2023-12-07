0009190625331ac3e2b355cc08f47a17e693b391
S <s>
2023-12-07 10:34:17 -0800

    {"types":"C","runs":2}

--- Final142.java
+++ Final142.java
@@ -5,6 +5,7 @@ import acm.program.ConsoleProgram;
 public class Final142 extends ConsoleProgram {
 	public void run() {
 		String text = readLine();
+		translateToTitle(text);
 	}
 
 	private String translateToTitle(String text) {
