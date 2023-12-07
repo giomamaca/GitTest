5074faa6e435910dea79dec92b01d7017dd12537
S <s>
2023-12-07 09:27:56 -0800

    {"types":"C","runs":1}

--- Practice52.java
+++ Practice52.java
@@ -1,4 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class Practice52 {
+import acm.program.ConsoleProgram;
 
+public class Practice52 extends ConsoleProgram {
+	public void run() {
+		try {
+			BufferedReader rd = new BufferedReader(new FileReader("Practice32.java"));
+			while (true) {
+				String line = rd.readLine();
+				if (line == null) {
+					break;
+				}
+				println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
