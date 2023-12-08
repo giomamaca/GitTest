7bc3d061b9346575e91dad679fb6eccd34c81cd2
S <s>
2023-12-07 22:40:22 -0800

    {"types":"C","runs":3}

--- Practice60.java
+++ Practice60.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class Practice60 {
+import acm.program.ConsoleProgram;
 
+public class Practice60 extends ConsoleProgram{
+	public void run(){
+		int k = readInt();
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		
+		while(k != -1){
+			list.add(k);
+			k = readInt();
+		}
+		for(int i = list.size(); i > 0; i++){
+			println(list.get(i));
+		}
+	}
 }
