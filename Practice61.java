1df42d9aa745f178f98e1599e90f2ce4543fa6b7
S <s>
2023-12-08 01:44:54 -0800

    {"types":"C","runs":7}

--- Practice61.java
+++ Practice61.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
 
-public class Practice61 {
+import acm.program.ConsoleProgram;
 
+public class Practice61 extends ConsoleProgram{
+	public void run(){
+		int count = 0;
+		
+		int m = readInt();
+		int n = readInt();
+		
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		while(count != n){
+			int num = readInt();
+			list.add(num);
+			count++;
+		}
+		for(int i = 0; i < list.size() - 1; i++){
+			int a = list.get(i);
+			for(int j = i; j < list.size(); j++){
+				int b = list.get(j);
+				if((a + b) == m){
+					println(i + "and" + j);
+				}
+			}
+		}
+	}
 }
