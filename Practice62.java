006264109ff80ababb30b5ad569f476f96323a3e
S <s>
2023-12-08 01:55:40 -0800

    {"types":"C","runs":11}

--- Practice62.java
+++ Practice62.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
 
-public class Practice62 {
+import acm.program.ConsoleProgram;
 
+public class Practice62 extends ConsoleProgram{
+	public void run(){
+		String s = readLine();
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		while(s != ""){
+			s = readLine();
+			list.add(s);
+		}
+		for(int i = 0; i < list.size(); i++){
+			String a = list.get(i);
+			for(int j = 0; j < list.size(); j++){
+				String b = list.get(j);
+				if(a == b){
+					println((i + 1) + " and " + (j + 1));
+				}
+			}
+		}
+	}
 }
