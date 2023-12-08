ae7baac24dba5620ec60c15c166a6cf21dc80b9c
S <s>
2023-12-08 00:45:05 -0800

    {"types":"C","runs":19}

--- Practice60.java
+++ Practice60.java
@@ -4,15 +4,16 @@ import acm.program.ConsoleProgram;
 
 public class Practice60 extends ConsoleProgram{
 	public void run(){
-		int k = readInt();
-		ArrayList <Integer> list = new ArrayList<Integer>();
-		
-		while(k != -1){
-			list.add(k);
-			k = readInt();
-		}
-		for(int i = list.size() - 1; i >= 0; i--){
-			println(list.get(i));
+	ArrayList<Integer> list = new ArrayList<Integer>();
+	
+	int k = readInt();
+	
+	while(k != -1){
+		list.add(k);
+		k = readInt();
+	}
+	for(int i = list.size(); i > 0; i--){
+		println(list);
 		}
 	}
 }
