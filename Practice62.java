69ad6143905509ea9f2f7617541ffd92e9447441
S <s>
2023-12-08 02:13:45 -0800

    {"types":"C","runs":25}

--- Practice62.java
+++ Practice62.java
@@ -8,7 +8,7 @@ public class Practice62 extends ConsoleProgram{
 		
 		ArrayList<String> list = new ArrayList<String>();
 		
-		while(!s.equals("")){
+		while(s != ("")){
 			s = readLine();
 			list.add(s);
 		}
