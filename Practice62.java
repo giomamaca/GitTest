594f4ba7ba852050b03f92b81a9655fec5df96d7
S <s>
2023-12-08 04:16:49 -0800

    {"types":"C","runs":7}

--- Practice62.java
+++ Practice62.java
@@ -16,11 +16,11 @@ public class Practice62 extends ConsoleProgram{
 		for(int i = 0; i < list.size(); i++){
 			String a = list.get(i);
 			System.out.println("a" + a);
-			for(int j = 0; j < list.size() - 1; j++){
+			for(int j = i; j < list.size() - 1; j++){
 				String b = list.get(j + 1);
 				System.out.println("b" + b);
 				if(a.equals(b)){
-					println(i + " and " +  (j + 1));
+					println((i + 1) + " and " +  (j + 1));
 					break;
 				}
 			}
