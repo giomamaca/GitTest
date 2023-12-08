0e020362cda7d166a07094a22a9e668e75fba5a9
S <s>
2023-12-08 02:13:29 -0800

    {"types":"C","runs":24}

--- Practice62.java
+++ Practice62.java
@@ -16,7 +16,7 @@ public class Practice62 extends ConsoleProgram{
 			String a = list.get(i);
 			for(int j = 0; j < list.size(); j++){
 				String b = list.get(j);
-				if(a.equals(b)){
+				if(a == b){
 					println((i + 1) + " and " + (j + 1));
 				}
 			}
