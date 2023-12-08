9d20cf13d6b18ce32ef775a70c24c12657715cca
S <s>
2023-12-08 10:00:30 -0800

    {"types":"C","runs":17}

--- Practice63.java
+++ Practice63.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Practice63 {
+import acm.program.ConsoleProgram;
 
+public class Practice63 extends ConsoleProgram{
+	public void run(){
+		ArrayList<String> list = new ArrayList<String>();
+		String s = readLine();
+		list.add(s);
+		
+		while(!s.equals("")){
+			s = readLine();
+			list.add(s);
+			
+		}
+		for(int i = 0; i < list.size(); i++){
+			StringTokenizer tk = new StringTokenizer(list.get(i), " ");
+			
+			while(tk.hasMoreTokens()){
+				String str = tk.nextToken();
+			}
+		}
+	}
 }
