46ec4a0a1718bfffe3672e03404c90b1bad168e8
S <s>
2023-12-08 02:12:15 -0800

    {"types":"C","runs":20}

--- Practice62.java
+++ Practice62.java
@@ -12,7 +12,7 @@ public class Practice62 extends ConsoleProgram{
 			s = readLine();
 			list.add(s);
 		}
-		for(int i = 0; i < list.size() - 1; i++){
+		for(int i = 0; i < list.size(); i++){
 			String a = list.get(i);
 			for(int j = i + 1; j < list.size(); j++){
 				String b = list.get(j);
