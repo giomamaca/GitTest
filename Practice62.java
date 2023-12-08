9e27a7a1a79bd58e375ac5732335d5c4ce3b86f7
S <s>
2023-12-08 02:14:18 -0800

    {"types":"C","runs":26}

--- Practice62.java
+++ Practice62.java
@@ -8,13 +8,13 @@ public class Practice62 extends ConsoleProgram{
 		
 		ArrayList<String> list = new ArrayList<String>();
 		
-		while(s != ("")){
+		while(!s.equals("")){
 			s = readLine();
 			list.add(s);
 		}
 		for(int i = 0; i < list.size(); i++){
 			String a = list.get(i);
-			for(int j = 0; j < list.size(); j++){
+			for(int j = i + 1; j < list.size() - 1; j++){
 				String b = list.get(j);
 				if(a == b){
 					println((i + 1) + " and " + (j + 1));
