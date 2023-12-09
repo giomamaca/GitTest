acba17dbbe2bcb878a20d875f0b96056a2afc429
S <s>
2023-12-09 09:10:00 -0800

    {"types":"C","runs":32}

--- Practice63.java
+++ Practice63.java
@@ -55,10 +55,10 @@ public class Practice63 extends ConsoleProgram {
 					how1++;
 					System.out.println(how1);
 				}
-//				if (how1 > how2) {
-//					answer = r;
-//					how2 = how1;
-//				}
+				if (how1 > how2) {
+					answer = r;
+					how2 = how1;
+				}
 			}
 			how1 = 0;
 		}
