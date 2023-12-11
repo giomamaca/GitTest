a90474c3df38da9c57994312de0a1ad148654d07
S <s>
2023-12-11 10:17:27 -0800

    {"types":"C","runs":7}

--- exercise.java
+++ exercise.java
@@ -2,29 +2,36 @@ import acm.graphics.*;
 import acm.program.ConsoleProgram;
 import acm.program.*;
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class exercise extends ConsoleProgram {	
 	public void run() {
-		String a = readLine();
-		String b = readLine();
-		println(isIn(a, b));
 		
-	}
-
-	private boolean isIn(String a, String b) {
-		for(int i = 0; i < b.length(); i++){
-			if(a.contains(b.charAt(i) + "")){
-				a = cut(a, b.charAt(i));
-				b = cut(b, b.charAt(i));
-			}else return false;
+		ArrayList<String> list = new ArrayList<String>();
+		try{
+			BufferedReader rd = new BufferedReader(new FileReader("HangmanLexicon.txt"));
+			while (true) {
+				String line = rd.readLine();
+				list.add(line);
+				if (line == null) {
+					break;
+				}
+			}
+			list.get(1);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		return true;
+		 getWordCount(list);
 	}
 
-	private String cut(String a, char c) {
-		String sub1 = a.substring(0, a.indexOf(c));
-		String sub2 = a.substring(a.indexOf(c) + 1);
+	private void getWordCount(ArrayList<String> list) {
+		int count = 0;
 		
-		return sub1 + sub2;
+		for(int i = 0; i < list.size(); i++){
+			count++;
+		}
 	}
 }
