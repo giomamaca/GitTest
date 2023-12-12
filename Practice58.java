78b1928e5a537f42520908d6b26d1b4667cb562e
S <s>
2023-12-12 00:09:56 -0800

    {"types":"C","runs":0}

--- Practice58.java
+++ Practice58.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class Practice58 {
-
+public class Practice58 extends ConsoleProgram{
+	public void run(){
+		int[][] a = new int[5][5];
+		
+		println(matrix(a));
+	}
+	private boolean matrix(int[][] a){
+		for(int i = 0; i < a.length; i++){
+			for(int j = 0; j < a[i].length; j++){
+				int[] k = new int[i * (a.length) + j];
+				
+				
+				
+			}
+		}
+		return false;
+	}
 }
