d62b02e82f34c9a823aeca5c68ba10ce6b1088f6
S <s>
2023-12-12 00:09:55 -0800

    {"types":"C","runs":0}

--- Practice59.java
+++ Practice59.java
@@ -1,4 +1,13 @@
+import acm.program.ConsoleProgram;
 
-public class Practice59 {
-
+public class Practice59 extends ConsoleProgram{
+	public void run(){
+		
+	}
+	private boolean isFilled(int[][] a, int startX, int startY, int x, int y){
+		for(int i = startX; i < startX + x; i++){
+			for(int j = startY; j < startY + y; j++){
+				
+			}
+		}
 }
