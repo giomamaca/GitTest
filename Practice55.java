d81dbc7acce1ef94cbde47d543d02be73cde59b7
S <s>
2023-12-12 09:32:21 -0800

    {"types":"C","runs":3}

--- Practice55.java
+++ Practice55.java
@@ -3,8 +3,27 @@ import acm.program.ConsoleProgram;
 public class Practice55 extends ConsoleProgram{
 	public void run(){
 		int n = readInt();
-		for(int i = 0; i < n; i++){
-			
+		isSorted(n);
+		if(isSorted(n)){
+			println("sorted");
+		}else{
+			println("not sorted");
 		}
+	}
+
+	private boolean isSorted(int n) {
+		int perv = 0;
+		int curr = 0;
+		for(int i = 0; i < n; i++){
+			int num = readInt();
+			perv = num;
+			
+			if(perv > curr){
+				curr = perv;
+			}else{
+				return false;
+			}
+		}
+		return true;
 	}
 }
