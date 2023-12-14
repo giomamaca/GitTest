70b7c090c0f97f41a6a2e567b2999d6025b6c493
S <s>
2023-12-14 01:18:03 -0800

    {"types":"C","runs":1}

--- Practice57.java
+++ Practice57.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
 
-public class Practice57 {
+import acm.program.ConsoleProgram;
 
+public class Practice57 extends ConsoleProgram{
+	public void run(){
+		int n = readInt();
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		
+		fillList(list, n);
+		makeCorrect(list);
+	}
+
+	private void makeCorrect(ArrayList<Integer> list) {
+		for(int i = 0; i < list.size(); i++){
+			for(int j = 0; j < list.size(); j++){
+				if(list.get(j) > list.get(i)){
+					swap(j, i);
+				}
+			}
+		}
+	}
+
+	private void swap(int j, int i) {
+		int temp = i;
+		j = i;
+		
+	}
+
+	private void fillList(ArrayList<Integer> list, int n) {
+		for(int i = 0; i < n; i++){
+			list.add(readInt());
+		}
+	}
 }
