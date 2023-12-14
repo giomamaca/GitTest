9111723b5ce7aa230508f0899cd6b486e6fde310
S <s>
2023-12-14 02:08:23 -0800

    {"types":"C","runs":4}

--- Practice56.java
+++ Practice56.java
@@ -1,26 +1,37 @@
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 
 public class Practice56 extends ConsoleProgram{
 	public void run(){
 		int n = readInt();
-		int[] arr = new int[n];
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		
-		for(int i = 0; i < arr.length; i++){
-			
-			if(zrdadia(arr)){
-				println("sorted");
-			}else{
-				println("notSorted");
+		fillList(list, n);
+		println(list.toString());
+		makeCorrect(list);
+		println(list.toString());
+	}
+
+	private void makeCorrect(ArrayList<Integer> list) {
+		for(int i = 0; i < list.size(); i++){
+			for(int j = 0; j < list.size(); j++){
+				if(list.get(j) > list.get(i)){
+					swap(list, j, i);
+				}
 			}
 		}
 	}
 
-	private boolean zrdadia(int[] arr) {
-		for(int i = 0; i < arr.length -1; i++){
-			if(arr[i] < arr[i + 1]){
-				return true;
-			}
+	private void swap(ArrayList<Integer> list, int j, int i) {
+		int temp = list.get(j);
+		list.set(j, list.get(i));
+		list.set(i, temp);
+	}
+
+	private void fillList(ArrayList<Integer> list, int n) {
+		for(int i = 0; i < n; i++){
+			list.add(readInt());
 		}
-		return true;
 	}
 }
