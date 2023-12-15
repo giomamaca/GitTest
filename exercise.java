c2a05fdcc3fb4fbd5df71424c79f40f1c9905186
S <s>
2023-12-15 02:45:45 -0800

    {"types":"C","runs":1}

--- exercise.java
+++ exercise.java
@@ -5,10 +5,21 @@ import java.awt.*;
 
 public class exercise extends ConsoleProgram {	
 	public void run() {
-		int[] num = new int[5];
-		for(int i = 0; i < num.length; i++){
-			num [i] = readInt();
-		}
+		int x = readInt();
+		println(isPalindrome(x));
 		
 	}
+	 public int isPalindrome(int x) {
+	        int k = x;
+	        int a = 0;
+//	        if(k < 0)return false;
+	        while(k != 0){
+	            a = a * 10 + x % 10;
+	            k /= 10;
+	        }
+//	        if(x == a){
+//	            return true;
+//	        }
+	        return x;
+	    }
 }
