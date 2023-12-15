0b9d6a095d805bb42d111564d1462a694cf8b045
S <s>
2023-12-15 04:06:51 -0800

    {"types":"C","runs":2}

--- exercise.java
+++ exercise.java
@@ -7,16 +7,22 @@ import java.util.Arrays;
 public class exercise extends ConsoleProgram {	
 	public void run() {
 		int target = 5;
-		int[] nums = new int [5];
-		nums[3] = 5;
+		int[] nums = new int [4];
+		nums[0] = 1;
+		nums[1] = 3;
+		nums[2] = 6;
+		nums[3] = 7;
 		println(searchInsert(nums, target));
 	}
 
 	public int searchInsert(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                return i;
+            if(nums[i] < target){
+                return 0;
+            }else{
+            	return i;
             }
+            
         }
         return 0;
     }
