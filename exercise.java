e2a68e93c45edf1df0bb924879ad3ab5a5a2025d
S <s>
2023-12-15 01:43:52 -0800

    {"types":"C","runs":8}

--- exercise.java
+++ exercise.java
@@ -5,26 +5,10 @@ import java.awt.*;
 
 public class exercise extends ConsoleProgram {	
 	public void run() {
-		String a = readLine();
-		String b = readLine();
-		println(isIn(a, b));
-		
-	}
-
-	private boolean isIn(String a, String b) {
-		for(int i = 0; i < b.length(); i++){
-			if(a.contains(b.charAt(i) + "")){
-				a = cut(a, b.charAt(i));
-				b = cut(b, b.charAt(i));
-			}else return false;
+		int[] num = new int[5];
+		for(int i = 0; i < num.length; i++){
+			num [i] = readInt();
 		}
-		return true;
-	}
-
-	private String cut(String a, char c) {
-		String sub1 = a.substring(0, a.indexOf(c));
-		String sub2 = a.substring(a.indexOf(c) + 1);
-		
-		return sub1 + sub2;
+		println(num);
 	}
 }
