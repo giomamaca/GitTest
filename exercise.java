a3de24804364e8a0fc34b7978b99cf0b4b08c673
S <s>
2023-12-15 04:47:44 -0800

    {"types":"C","runs":7}

--- exercise.java
+++ exercise.java
@@ -4,21 +4,29 @@ import acm.program.*;
 import java.awt.*;
 import java.util.Arrays;
 
-public class exercise extends ConsoleProgram {	
+public class exercise extends ConsoleProgram {
 	public void run() {
-		int target = 0;
-		int[] nums = {4,5,6,7,0,1,2};
-		println(search(nums, target));
+		int target = 3;
+		int[] nums = { 3, 3, 2, 2 };
+		println(removeElement(nums, target));
 	}
 
-	  public int search(int[] nums, int target) {
-	        for(int i = 0; i < nums.length; i++){
-	            if(nums[i] == target){
-	                 return i;
-	            }else{
-	                return -1;
-	            }
-	        }
-	        return -1;
-	    }
+	public int removeElement(int[] nums, int val) {
+		int count = 0;
+		int[] answer = new int[nums.length];
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != val) {
+				count++;
+				for (int j = 0; i < answer.length; j++) {
+					answer[j] += nums[i];
+				}
+			}
+		}
+		for(int k = 0; k < answer.length; k++){
+			if(answer[k] == 0){
+				
+			}
+		}
+		return count;
+	}
 }
