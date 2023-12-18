3a251fc48676b8f234a0d2f95b43190fd8b42303
S <s>
2023-12-18 07:50:28 -0800

    {"types":"C","runs":9}

--- Practice58.java
+++ Practice58.java
@@ -1,20 +1,33 @@
+import java.util.ArrayList;
+
 import acm.program.ConsoleProgram;
 
 public class Practice58 extends ConsoleProgram{
 	public void run(){
-		int[][] a = new int[5][5];
-		
-		println(matrix(a));
+		int [][] matrix = {{1, 3}, {2, 4}};
+		println(isMagic(matrix));
 	}
-	private boolean matrix(int[][] a){
-		for(int i = 0; i < a.length; i++){
-			for(int j = 0; j < a[i].length; j++){
-				int[] k = new int[i * (a.length) + j];
-				
-				
-				
+
+	private boolean isMagic(int[][] matrix) {
+		if(matrix.length != matrix[0].length){
+			return false;
+		}
+		ArrayList<Integer> arr = getArrayList(matrix.length);
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				if(arr.contains(matrix[i][j])){
+					arr.remove((Integer)matrix[i][j]);
+				}
 			}
 		}
-		return false;
+		return arr.isEmpty();
+	}
+
+	private ArrayList<Integer> getArrayList(int length) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = 1; i <= length*length; i++){
+			arr.add(i);
+		}
+		return arr;
 	}
 }
