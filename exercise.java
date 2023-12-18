223d949a6ad511a8fc4b039a0ba7c921a473c87c
S <s>
2023-12-18 07:14:01 -0800

    {"types":"C","runs":5}

--- exercise.java
+++ exercise.java
@@ -9,7 +9,7 @@ public class exercise extends ConsoleProgram {
 		int[][] num = new int[5][5];
 		for(int i = 0; i < num.length; i++){
 			for(int j = 0; j < num[i].length; j++){
-				num[i][j] = i + j;
+				num[i][j] = 1;
 			}
 		}
 		println(Arrays.toString(num));
