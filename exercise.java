3101f2be589a91630f221cc3eed551f9ac064070
S <s>
2023-12-18 07:13:01 -0800

    {"types":"C","runs":1}

--- exercise.java
+++ exercise.java
@@ -6,27 +6,12 @@ import java.util.Arrays;
 
 public class exercise extends ConsoleProgram {
 	public void run() {
-		int target = 3;
-		int[] nums = { 3, 3, 2, 2 };
-		println(removeElement(nums, target));
-	}
-
-	public int removeElement(int[] nums, int val) {
-		int count = 0;
-		int[] answer = new int[nums.length];
-		for (int i = 0; i < nums.length; i++) {
-			if (nums[i] != val) {
-				count++;
-				for (int j = 0; i < answer.length; j++) {
-					
-				}
+		int[][] num = new int[5][5];
+		for(int i = 0; i < num.length; i++){
+			for(int j = 0; j < num[i].length; j++){
+				num[i][j] = i + j;
 			}
 		}
-		for(int k = 0; k < answer.length; k++){
-			if(answer[k] == 0){
-				
-			}
-		}
-		return count;
+		println(Arrays.toString(num));
 	}
 }
