afc518a8d09fff7470856b991eae4c3d59ce4a89
S <s>
2023-12-18 07:50:28 -0800

    {"types":"C","runs":9}

--- exercise.java
+++ exercise.java
@@ -2,36 +2,16 @@ import acm.graphics.*;
 import acm.program.ConsoleProgram;
 import acm.program.*;
 import java.awt.*;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class exercise extends ConsoleProgram {
 	public void run() {
-		int[][] matrix = { { 1, 3 }, { 2, 4 } };
-		int[][] ma2 = new int[2][2];
-		println(isMagic(matrix));
-	}
-
-	private boolean isMagic(int[][] matrix) {
-		if (matrix.length != matrix[0].length) {
-			return false;
-		}
-		ArrayList<Integer> arr = getArrayList(matrix.length);
-		for (int i = 0; i < matrix.length; i++) {
-			for (int j = 0; j < matrix[0].length; j++) {
-				if (arr.contains(matrix[i][j])) {
-					arr.remove((Integer) matrix[i][j]);
-				}
+		int[][] num = new int[5][5];
+		for(int i = 0; i < num.length; i++){
+			for(int j = 0; j < num[i].length; j++){
+				num[i][j] = 1;
 			}
 		}
-		return arr.isEmpty();
-	}
-
-	private ArrayList<Integer> getArrayList(int length) {
-		ArrayList<Integer> arr = new ArrayList<Integer>();
-		for (int i = 1; i <= length * length; i++) {
-			arr.add(i);
-		}
-		return arr;
+		println(Arrays.toString(num));
 	}
 }
