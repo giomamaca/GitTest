8a2677dc87c916db78bd277f939db29c676b7937
S <s>
2023-12-18 23:41:52 -0800

    {"types":"C","runs":1}

--- Practice62.java
+++ Practice62.java
@@ -1,10 +1,24 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import acm.program.ConsoleProgram;
 
 public class Practice62 extends ConsoleProgram{
 	public void run(){
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
 		
+		while(true){
+			String name = readLine();
+			if(name.isEmpty()){
+				break;
+			}
+			
+			map.put(name, map.getOrDefault(name, 0) + 1);
+		}
+		println(map);
+	}
+		
+	private void run1(){
 		ArrayList<String> list = new ArrayList<String>();
 		String s = readLine();
 		list.add(s);
@@ -13,7 +27,6 @@ public class Practice62 extends ConsoleProgram{
 			s = readLine();
 			list.add(s);
 		}
-		
 		for(int i = 0; i < list.size(); i++){
 			String a = list.get(i);
 			for(int j = i + 1; j < list.size(); j++){
