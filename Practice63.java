fada91a5b1585313b86a091910f83a832af5b587
S <s>
2023-12-19 00:01:02 -0800

    {"types":"C","runs":6}

--- Practice63.java
+++ Practice63.java
@@ -21,6 +21,17 @@ public class Practice63 extends ConsoleProgram {
 			map.putIfAbsent(b, new HashSet<String>());
 			map.get(b).add(a);
 		}
+		String name = null;
+		println(map);
+		int ans = 0;
+		for(String friend : map.keySet()){
+			int amount = map.get(friend).size();
+			if(amount > ans){
+				ans = amount;
+				name = friend;
+			}
+		}
+		println(name);
 		println(map);
 	}
 	
