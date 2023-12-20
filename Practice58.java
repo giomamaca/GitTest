d49fa5a743c1846305226e345ae19be011d04fe9
S <s>
2023-12-20 10:24:25 -0800

    {"types":"C","runs":28}

--- Practice58.java
+++ Practice58.java
@@ -4,28 +4,31 @@ import acm.program.ConsoleProgram;
 
 public class Practice58 extends ConsoleProgram{
 	public void run(){
-		int[][] m = {{1, 3}, {2, 4}};
-		println(isMagic(m));
-	}
-	
-	private boolean isMagic(int[][] m){
-		int n = m.length;
-		for(int i = 1; i <= n; i++){
-			if(!containsMatrix(m, i)){
-				return false;
-			}
-		}
-		return true;
+		int [][] matrix = {{1, 3}, {2, 4}};
+		int [][] ma2 = new int[2][2];
+		println(isMagic(matrix));
 	}
 
-	private boolean containsMatrix(int[][] m, int num) {
-		for(int i = 0; i < m.length; i++){
-			for(int j = 0; j < m[0].length; j++){
-				if(m[i][j] == num){
-					return true;
+	private boolean isMagic(int[][] matrix) {
+		if(matrix.length != matrix[0].length){
+			return false;
+		}
+		ArrayList<Integer> arr = getArrayList(matrix.length);
+		for(int i = 0; i < matrix.length; i++){
+			for(int j = 0; j < matrix[0].length; j++){
+				if(arr.contains(matrix[i][j])){
+					arr.remove((Integer)matrix[i][j]);
 				}
 			}
 		}
-		return false;
+		return arr.isEmpty();
+	}
+
+	private ArrayList<Integer> getArrayList(int length) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		for(int i = 1; i <= length*length; i++){
+			arr.add(i);
+		}
+		return arr;
 	}
 }
