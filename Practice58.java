06295a1d262f24c2b7ae59b35b0cf0e4805c25af
S <s>
2023-12-20 10:22:03 -0800

    {"types":"C","runs":23}

--- Practice58.java
+++ Practice58.java
@@ -7,11 +7,8 @@ public class Practice58 extends ConsoleProgram{
 		int[][] m = {{1, 3}, {2, 4}};
 		println(isMagic(m));
 	}
-	
-	
 	private boolean isMagic(int[][] m){
-		int n = m.length;
-		for(int i = 1; i <= n; i++){
+		for(int i = 1; i <= m.length; i++){
 			if(!containsMatrix(m, i)){
 				return false;
 			}
