5284a9862ce88b4f51f3575a41bdc2bc1341a761
S <s>
2023-12-20 10:23:39 -0800

    {"types":"C","runs":24}

--- Practice58.java
+++ Practice58.java
@@ -3,12 +3,14 @@ import java.util.ArrayList;
 import acm.program.ConsoleProgram;
 
 public class Practice58 extends ConsoleProgram{
-	public void run1(){
+	public void run(){
 		int[][] m = {{1, 3}, {2, 4}};
 		println(isMagic(m));
 	}
+	
 	private boolean isMagic(int[][] m){
-		for(int i = 1; i <= m.length; i++){
+		int n = m.length;
+		for(int i = 1; i <= n; i++){
 			if(!containsMatrix(m, i)){
 				return false;
 			}
