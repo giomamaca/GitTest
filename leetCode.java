59067216162815f1c64b85e2761ae751e21afd56
S <s>
2023-12-22 02:37:41 -0800

    {"types":"C","runs":15}

--- leetCode.java
+++ leetCode.java
@@ -1,4 +1,31 @@
+import acm.program.ConsoleProgram;
 
-public class leetCode {
+public class leetCode extends ConsoleProgram {
+	public void run() {
 
+	}
+
+	private int maxScore(String s) {
+		int ans = 0;
+		int zeros = 0;
+		int ones = 0;
+		for (int i = 1; i < s.length(); i++) {
+			String sub1 = s.substring(i, s.length() + 1);
+			String sub2 = s.substring(0, i);
+			for (int j = 0; j < sub1.length(); j++) {
+				if (sub1.charAt(j) == 1) {
+					ones++;
+				}
+			}
+			for (int k = 0; k < sub2.length(); k++) {
+				if (sub2.charAt(k) == 0) {
+					zeros++;
+				}
+			}
+			if ((zeros + ones) > ans) {
+				ans = zeros + ones;
+			}
+		}
+		return ans;
+	}
 }
