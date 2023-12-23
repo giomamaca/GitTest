84e7a057956fe8b7e0fe95b078952fa76bfea9ee
S <s>
2023-12-23 09:08:04 -0800

    {"types":"C","runs":1}

--- exercise.java
+++ exercise.java
@@ -6,52 +6,18 @@ import java.util.Arrays;
 
 public class exercise extends ConsoleProgram {
 	public void run() {
-
+		int n = readInt();
+		println(hammingWeight(n));
 	}
 
-	public String addBinary(String a, String b) {
-		int ans = 0;
-		int n1 = 0;
-		int n2 = 0;
-		for (int i = a.length(); i > 0; i--) {
-			char ch = a.charAt(i);
-			int b1 = gainte(ch);
-			n1 = n1 + b1 * xarisxi(2);
-		}
-		for (int j = b.length(); j > 0; j--) {
-			char chr = b.charAt(j);
-			int b2 = gainte(chr);
-			n2 = n2 + b2 * xarisxi(2);
-		}
-		ans = n1 + n2;
-		return binary(ans);
-	}
-
-	private int xarisxi(int x) {
-		int count = 1;
-		int k = x;
-		while (count != x) {
-			k *= x;
-			count++;
-		}
-	}
-
-	private int gainte(char b1) {
-		int a = b1 - '0';
-		return a;
-	}
-
-	private String binary(int ans) {
-		String s = "";
-		int count = 0;
-		int a = 2;
-
-		while (a <= ans) {
-			a *= 2;
-			count++;
-		}
-		for (int i = 0; i < count; i++) {
-
-		}
-	}
+    private int hammingWeight(int n) {
+        int ans = 0;
+        while(n > 0){
+            if(n % 10 == 1){
+                ans++;
+            }
+            n /= 10;
+        }
+        return ans;
+    }
 }
