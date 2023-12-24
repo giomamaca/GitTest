cc5cc2d5f3c1ae3ffc643bc6801f0d6c43396681
S <s>
2023-12-24 05:23:40 -0800

    {"types":"C","runs":8}

--- exercise.java
+++ exercise.java
@@ -7,15 +7,44 @@ import java.util.Arrays;
 public class exercise extends ConsoleProgram {
 	public void run() {
 		String s = "aad";
-		metodi(s);
+		println(metodi(s));
 	}
 
-	private void metodi(String s) {
-		for(int i = 0; i < s.length() - 1; i++){
-			for(int j = i + 1; j < s.length() + 1; j++){
-				String sub = s.substring(i, j);
-				println(sub);
-			}
-		}
+	private int metodi(String s) {
+		   if(s.length() == 0) return 0;
+	        if(same(s)) return 1;
+	        if(s.length() == 2 && !same(s)) return 2;
+	        if(s.length() > 2){
+	            String ans = "";
+	            for(int i = 0; i < s.length() - 2; i++){
+	                for(int j = i + 2; j < s.length() + 1; j++){
+	                    String sub = s.substring(i, j);
+	                    if(different(sub) && ans.length() < sub.length()){
+	                        ans = sub;
+	                    }
+	                }
+	            }
+	            return ans.length();
+	        }
+	        return 1; 
 	}
+	   private boolean same(String s){
+           char a = s.charAt(0);
+           char x = a;
+           for(int i = 0; i < s.length(); i++){
+               if(s.charAt(i) != a) return false;
+           }
+           return true;
+       }
+
+       private boolean different(String sub){
+           for(int i = 0; i < sub.length() - 1; i++){
+               char a = sub.charAt(i);
+               for(int j = i + 1; j < sub.length(); j++){
+                   char b = sub.charAt(j);
+                   if(a == b) return false;
+               }
+           }
+           return true;
+       }
 }
