c05dcddb5f31f4024cf4f2e8c6f7d3737010224a
S <s>
2023-12-24 04:46:56 -0800

    {"types":"C","runs":1}

--- exercise.java
+++ exercise.java
@@ -6,18 +6,30 @@ import java.util.Arrays;
 
 public class exercise extends ConsoleProgram {
 	public void run() {
-		int n = readInt();
-		println(hammingWeight(n));
+		String s = "adadada";
+		println(s.substring(0, 3));
 	}
 
-    private int hammingWeight(int n) {
-        int ans = 0;
-        while(n > 0){
-            if(n % 10 == 1){
-                ans++;
-            }
-            n /= 10;
-        }
-        return ans;
-    }
+	   private int lengthOfLongestSubstring(String s) {
+	        String ans = "";
+	        for(int i = 0; i < s.length() - 1; i++){
+	            for(int j = i + 1; j < s.length(); j++){
+	                String sub = s.substring(i, j);
+	                if(different(sub) && ans.length() < sub.length()){
+	                    ans = sub;
+	                }
+	            }
+	        }
+	        return ans.length();
+	    }
+	        private boolean different(String sub){
+	            for(int i = 0; i < sub.length() - 1; i++){
+	                char a = sub.charAt(i);
+	                for(int j = i + 1; j < sub.length(); j++){
+	                    char b = sub.charAt(j);
+	                    if(a == b) return false;
+	                }
+	            }
+	            return true;
+	        }
 }
