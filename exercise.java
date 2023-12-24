cadf614ca6bce43b498a84d6e82d9352df3baeec
S <s>
2023-12-24 07:02:55 -0800

    {"types":"C","runs":17}

--- exercise.java
+++ exercise.java
@@ -16,28 +16,31 @@ public class exercise extends ConsoleProgram {
 	        return sum(1, n - 1);
 	    }
 	    private int sum(int a, int b){
-	        int ans = 1;
-	        while(b >= a){
-	        	int up = fact(b);
-	        	System.out.println(up);
-	        	int down1 = fact(a);
-//	        System.out.println(down1);
-	        	int down2 = fact(b - a);
-//	            System.out.println(down2);
-	            ans = ans + (up / (down1 * down2));
-//	            System.out.println(ans);
-	            b -= 1;
-//	            System.out.println(b);
-	            a += 1;
-//	            System.out.println(a);
-	        }
-	        return ans;
+//	        int ans = 1;
+//	        while(b >= a){
+//	        	int up = fact(b);
+//	        	System.out.println(up);
+//	        	int down1 = fact(a);
+////	        System.out.println(down1);
+//	        	int down2 = fact(b - a);
+////	            System.out.println(down2);
+//	            ans = ans + (up / (down1 * down2));
+////	            System.out.println(ans);
+//	            b -= 1;
+////	            System.out.println(b);
+//	            a += 1;
+////	            System.out.println(a);
+//	        }
+//	        return ans;
 	    }
 	    private int fact(int a){
+	    	a = 5;
 	        int ans = 1;
+	        int count = 0;
 	        while(a != 0){
 	            ans *= a;
-	            a = a - 1;
+	            System.out.println(ans);
+	            a -= 1;
 	        }
 	        return ans;
 	    }
