3b49b091b897a62830895335e454540e7a68652b
S <s>
2023-12-25 08:32:09 -0800

    {"types":"C","runs":6}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -6,13 +6,20 @@ import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-		HashMap<String, String> map = new HashMap<String, String>();
-		while(true){
-			if(map.isEmpty()){
-				break;
-			}
-			map.put("gio", "sofo");
-		}
-		println(map);
+	      HashMap<Integer, String> numbers = new HashMap<>();
+
+	        // insert entries to the HashMap
+	        numbers.put(1, "Java");
+	        numbers.put(2, "Python");
+	        numbers.put(3, "JavaScript");
+	        System.out.println("HashMap: " + numbers);
+
+	        // mapping for the key is present in HashMap
+	        String value1 = numbers.getOrDefault(1, "Not Found");
+	        System.out.println("Value for key 1:  " + value1);
+
+	        // mapping for the key is not present in HashMap
+	        String value2 = numbers.getOrDefault(4, "Not Found");
+	        System.out.println("Value for key 4: " + value2);
 	}
 }
