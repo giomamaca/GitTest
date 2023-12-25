adf366e546073e93b7fb90cfb8132ba42a19558c
S <s>
2023-12-25 07:57:56 -0800

    {"types":"C","runs":1}

--- Final141.java
+++ Final141.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class Final141 {
+public class Final141 extends ConsoleProgram{
+	public void run() {
+		int arr[] = new int[100];
+		
+		for (int i=0; i<10; ) {
+			for (int j=0; j<8; j++) {
+				j++;
+				arr[i+j] = i+2*j;
+				i++;
+			}
+		}
 
+		for (int i=0; i<10; i++) {
+			print(arr[i] + " ");
+		}
+	}
 }
