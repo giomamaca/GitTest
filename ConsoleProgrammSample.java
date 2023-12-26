0ec9188beed2e5e24e656613ea7f440b8e0f4d72
S <s>
2023-12-26 10:33:20 -0800

    {"types":"C","runs":7}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -7,7 +7,7 @@ import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-		int[] prices = {1,4,6,7,84,3,2,4,79,5,3,2,2,345};
+		int[] prices = {50,4,6,7,84,3,2,4,79,5,3,2,2,345};
 		println(maxProfit(prices));
 	}
 
