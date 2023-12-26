f8b421ee1f6123e12db8e8b31c98d7c83dfc2151
S <s>
2023-12-26 10:30:56 -0800

    {"types":"C","runs":4}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -8,15 +8,8 @@ import acm.program.*;
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
 		int[] prices = {1,4,6,7,84,3,2,4,79,5,3,2,2,345};
-		println(maxProfit(prices));
-	}
-
-	public int maxProfit(int[] prices) {
-		int ans = 0, mi = prices[0];
 		for (int v : prices) {
-			ans = Math.max(ans, v - mi);
-			mi = Math.min(mi, v);
+			println(v);
 		}
-		return ans;
 	}
 }
