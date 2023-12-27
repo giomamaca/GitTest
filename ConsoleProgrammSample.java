fade28f0f4c87545f8906df0dcf822db5e660821
S <s>
2023-12-27 04:24:22 -0800

    {"types":"C","runs":1}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -2,21 +2,19 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
+import javax.swing.JButton;
+import javax.swing.JTextField;
+
 import acm.graphics.GOval;
 import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
+	
+	private JTextField text;
+	private JButton but;
+	
 	public void run() {
-		int[] prices = {50,4,6,7,84,3,2,4,79,5,3,2,2,345};
-		println(maxProfit(prices));
-	}
-
-	public int maxProfit(int[] prices) {
-		int ans = 0, mi = prices[0];
-		for (int v : prices) {
-			ans = Math.max(ans, v - mi);
-			mi = Math.min(mi, v);
-		}
-		return ans;
+		text = new JTextField(10);
+		add(text, SOUTH);
 	}
 }
