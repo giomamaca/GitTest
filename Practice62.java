39cadb44cb48cbb06b6bda8deb252e38d0a43eac
S <s>
2023-12-27 06:45:32 -0800

    {"types":"C","runs":23}

--- Practice62.java
+++ Practice62.java
@@ -12,8 +12,29 @@ public class Practice62 extends ConsoleProgram{
 			if(name.isEmpty()){
 				break;
 			}
+			
 			map.put(name, map.getOrDefault(name, 0) + 1);
 		}
 		println(map);
+	}
+		
+	private void run1(){
+		ArrayList<String> list = new ArrayList<String>();
+		String s = readLine();
+		list.add(s);
+		
+		while(!s.equals("")){
+			s = readLine();
+			list.add(s);
+		}
+		for(int i = 0; i < list.size(); i++){
+			String a = list.get(i);
+			for(int j = i + 1; j < list.size(); j++){
+				String b = list.get(j);
+				if(a.equals(b)){
+					println(i + " and " + j);
+				}
+			}
+		}
 	}
 }
