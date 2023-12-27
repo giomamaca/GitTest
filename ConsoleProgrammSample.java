546731c93158ef6f08ccac80cdf5205b482bce17
S <s>
2023-12-27 04:28:15 -0800

    {"types":"C","runs":3}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -1,8 +1,10 @@
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.StringTokenizer;
 
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 import acm.graphics.GOval;
@@ -14,7 +16,16 @@ public class ConsoleProgrammSample extends ConsoleProgram {
 	private JButton but;
 	
 	public void run() {
-		text = new JTextField(100);
+		JLabel lab = new JLabel("text: ");
+		add(lab, SOUTH);
+		text = new JTextField(10);
 		add(text, SOUTH);
+		but = new JButton("ender");
+		add(but, SOUTH);
+		
+	}
+
+	private void actionPerformed(ActionEvent e) {
+		
 	}
 }
