0f91653ceb30f980bfd6b9989e95f70b61e91101
S <s>
2023-12-27 06:44:41 -0800

    {"types":"C","runs":19}

--- Practice64.java
+++ Practice64.java
@@ -1,7 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
 public class Practice64 extends ConsoleProgram{
 	public void run(){
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
 		
+		while(true){
+			String s = readLine();
+			
+			StringTokenizer tk = new StringTokenizer(s, " ");
+			String str = tk.nextToken();
+		}
 	}
 }
