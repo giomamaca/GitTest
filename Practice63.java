b0261ac3cbeb8646d08cbc3c6031fce57dfa571c
S <s>
2023-12-27 06:14:25 -0800

    {"types":"C","runs":15}

--- Practice63.java
+++ Practice63.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.StringTokenizer;
 
@@ -8,31 +9,20 @@ import acm.program.ConsoleProgram;
 
 public class Practice63 extends ConsoleProgram {
 	public void run() {
-		HashMap <String, Set<String>> map = new HashMap <String, Set<String>>();
-		
-		while(true){
-			String s = readLine();
-			if(map.isEmpty()){
+		Map<String, Integer> map = new HashMap<>();
+		while (true) {
+			String name = readLine("Enter name: ");
+			if (name.equals("")) {
 				break;
 			}
-			StringTokenizer tok = new StringTokenizer(s);
-			String a = tok.nextToken();
-			String b = tok.nextToken();
-			map.putIfAbsent(b, new HashSet<String>());
-			map.get(b).add(a);
-		}
-		String name = null;
-		println(map);
-		int ans = 0;
-		for(String friend : map.keySet()){
-			int amount = map.get(friend).size();
-			if(amount > ans){
-				ans = amount;
-				name = friend;
+			if (!map.containsKey(name)) {
+				map.put(name, 0);
 			}
+			map.put(name, map.get(name) + 1);
 		}
-		println(name);
-		println(map);
+		for(String name : map.keySet()){
+			println(name + " - " + map.get(name));
+		}
 	}
 	
 	
