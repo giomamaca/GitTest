23fac1936feea8bc8a85f3b115b053192a65fc59
S <s>
2023-12-29 23:32:19 -0800

    {"types":"C","runs":0}

--- Final1415.java
+++ Final1415.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class Final1415 {
-
+public class Final1415 extends ConsoleProgram{
+	public void run(){
+		
+	}
+	private int[][] solveSudoku (int [][] sudoku){
+		return sudoku;
+		
+	}
 }
