09713cfc9e2297bf837967a56aa5b33ef3a616f4
S <s>
2023-12-31 02:35:20 -0800

    {"types":"C","runs":1}

--- ConsoleProgrammSample.java
+++ ConsoleProgrammSample.java
@@ -9,23 +9,25 @@ import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
-
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.*;
 
 public class ConsoleProgrammSample extends ConsoleProgram {
 	public void run() {
-		int [] a = new int[10];
-    }
+		double a = Math.sqrt(12);
+		print(a);
+	}
 
-    // Helper method to print a matrix of strings
-    private static void printStringMatrix(int[][] s) {
-        for (int i = 0; i < s.length; i++) {
-            for (int j = 0; j < s[0].length; j++) {
-                System.out.print(j + " ");
-            }
-            System.out.println();
-        }
-    }
+	// Helper method to print a matrix of strings
+	public int mySqrt(int x) {
+		int n = 0;
+		for (int i = 0; true; i++) {
+			if ((n * n) < x) {
+				n++;
+			}else{
+				return n;
+			}
+		}
+	}
 }
