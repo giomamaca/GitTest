fc48e2336115c02610516e983e273de59b02a3d5
S <s>
2024-01-01 14:15:10 +0400

    {"types":"C","runs":37}

--- Practice65.java
+++ Practice65.java
@@ -1,4 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
-public class Practice65 {
+import acm.program.ConsoleProgram;
 
+public class Practice65 extends ConsoleProgram{
+	private String save;
+	public void run(){
+		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+
+		while (true) {
+			ArrayList<String> list = new ArrayList<String>();
+			String s = readLine();
+
+			StringTokenizer tk = new StringTokenizer(s);
+			int c = 0;
+			while (tk.hasMoreTokens()) {
+				String str = tk.nextToken();
+				if(c % 2 == 0){
+					list.add(str);
+					save = str;
+				}else{					
+					if(map.containsKey(str)){
+						map.get(str).add(save);
+					}else{						
+						map.put(str, list);
+					}
+				}
+				c++;
+			}
+			println(map);
+		}
+	}
 }
