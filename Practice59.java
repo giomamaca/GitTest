e648bd18766145418bf8606a9cc208cb367681b5
S <s>
2024-01-01 15:33:05 +0400

    {"types":"C","runs":11}

--- Practice59.java
+++ Practice59.java
@@ -4,28 +4,29 @@ import acm.util.RandomGenerator;
 public class Practice59 extends ConsoleProgram {
 	RandomGenerator rgen = new RandomGenerator();
 	public void run() {
-		int[][] mat = new int[4][4];
-		fillMat(mat);
-		printMatrix(mat);
+		int[][] m = new int[4][4];
+		fillRandomMatrixWithOnesAndZeroes(m);
+		printMatrix(m);
 		println();
-		int full = 0;
-		for(int i = 0; i < mat.length; i++){
-			for(int j = 0; j < mat[i].length; j++){
-				int right = countRight(mat, i, j);
-				int down = countDown(mat, i, j);
-				int max = checkMatrix(mat, i, j, i + down - 1, j + right - 1);
-				if(max > full){
-					full = max;
+		int fullMax = 0;
+		for(int i = 0; i < m.length; i++){
+			for(int j = 0; j < m[i].length; j++){
+				int right = countRight(m, i, j);
+				int down = countDown(m, i, j);
+				int max = checkMatrix(m, i, j, i + down - 1, j + right - 1);
+				if(max > fullMax){
+					fullMax = max;
 				}
 			}
 		}
-		println("max is: " + full);
+		println("max is: " + fullMax);
 	}
-	private int checkMatrix(int[][]mat, int i, int j, int k, int l) {
+
+	private int checkMatrix(int[][]m, int i, int j, int k, int l) {
 		int max = -1;
 		for(int r = i; r <= k; r++){
 			for(int c = j; c <= l; c++){
-				if(isFilled(mat, i, j, r, c)){
+				if(isFilled(m, i, j, r, c)){
 					int size = (r - i + 1) * (c - j + 1);
 					if(size > max){
 						max = size;
@@ -35,7 +36,7 @@ public class Practice59 extends ConsoleProgram {
 		}
 		return max;
 	}
-	
+
 	private boolean isFilled(int[][] m, int i, int j, int r, int c) {
 		for(int a = i; a <= r; a++){
 			for(int b = j; b <= c; b++){
@@ -46,36 +47,43 @@ public class Practice59 extends ConsoleProgram {
 		}
 		return true;
 	}
+
+	private int countDown(int[][] m, int i, int j) {
+		int count = 0;
+		for(int k = i; k < m.length; k++){
+			if(m[k][j] == 1){
+				count++;
+			}else{
+				break;
+			}
+			
+		}
+		return count;
+	}
+
+	private int countRight(int[][] m, int i, int j) {
+		int count = 0;
+		for(int k = j; k < m[i].length; k++){
+			if(m[i][k] == 1){
+				count++;
+			}else{
+				break;
+			}
+			
+		}
+		return count;
+	}
+
+	private void fillRandomMatrixWithOnesAndZeroes(int[][] m) {
+		for(int i = 0; i < m.length; i++){
+			for(int j = 0; j < m[i].length; j++){
+				m[i][j] = rgen.nextInt(0, 1);
+			}
+		}
+	}
 	
-	private int countDown(int[][] mat, int i, int j) {
-		int count = 0;
-		for(int k = i; k < mat.length; k++){
-			if(mat[i][k] == 1){
-				count++;
-			}else{
-				break;
-			}
-		}
-		return count;
-	}
-	private int countRight(int[][] mat, int i, int j) {
-		int count = 0;
-		for(int k = j; k < mat[i].length; k++){
-			if(mat[i][k] == 1){
-				count++;
-			}else{
-				break;
-			}
-		}
-		return count;
-	}
-	private void fillMat(int[][] mat) {
-		for(int i = 0; i < mat.length; i++){
-			for(int j = 0; j < mat.length; j++){
-				mat[i][j] = rgen.nextInt(0, 1);
-			}
-		}
-	}
+
+	
 	private void printMatrix(int[][] m){
 		for(int i = 0; i < m.length; i++){
 			for(int j = 0; j < m[i].length; j++){
