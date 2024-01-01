349d2af67b00a4aca958f3bf1fa939f4400e1166
S <s>
2024-01-01 13:51:00 +0400

    {"types":"C","runs":14}

--- Practice64.java
+++ Practice64.java
@@ -5,24 +5,25 @@ import java.util.StringTokenizer;
 import acm.program.ConsoleProgram;
 
 public class Practice64 extends ConsoleProgram{
-	private String save = "";
+	private String save;
 	public void run(){
 		HashMap<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
 		
 		while(true){
-			int c = 1;
+			int c = 0;
 			ArrayList<String> list = new ArrayList<String>();
 			String s = readLine();
 			
 			StringTokenizer tk = new StringTokenizer(s, " ");
 			String str = tk.nextToken();
-			if(c % 2 == 0){
-				save = str;
-			}
 			if(c % 2 == 1){
-				list.add(str);
+				save = str;
+				c++;
 			}
-			c++;
+			if(c % 2 == 0){
+				list.add(str);
+				c++;
+			}
 			System.out.println(c);
 			map.put(save, list);
 			System.out.println(map);
