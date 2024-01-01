9b65e7176d55dd3dfee298ae14bf41cea044bc97
S <s>
2024-01-01 16:19:00 +0400

    {"types":"C","runs":1}

--- Practice68.java
+++ Practice68.java
@@ -1,4 +1,25 @@
+import java.awt.event.ActionEvent;
 
-public class Practice68 {
+import javax.swing.JButton;
+import javax.swing.JTextField;
 
+import acm.program.ConsoleProgram;
+
+public class Practice68 extends ConsoleProgram{
+	private JButton enter;
+	private JTextField text;
+	public void run(){
+		enter = new JButton("enter");
+		add(enter, SOUTH);
+		text = new JTextField(10);
+		add(text, SOUTH);
+		text.addActionListener(this);
+		addActionListeners();
+	}
+	public void ActionListener(ActionEvent e){
+		if(text.getText().length() > 0){
+			println(text.getText());
+			text.setText("");
+		}
+	}
 }
